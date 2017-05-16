83f97dd5558c35b266e9a252077ca55b5ebe3414
Andre Abrantes <andredossantosabrantes@gmail.com>
2017-05-16 00:08:07 -0300

    Fix main layout

--- app/controllers.js
+++ app/controllers.js
@@ -29,51 +29,17 @@
   app.controller("HomeController", function HomeController(InstitutionService, $mdDialog) {
     var homeCtrl = this;
 
-    homeCtrl.institutions = [];
-    
-    InstitutionService.get(function(info) {
-      homeCtrl.institutions = info.data;
-    });
-
-    homeCtrl.delete = function deleteInst(inst, index) {
-     InstitutionService.delete(inst.iid, function(info) {
-      homeCtrl.institutions.splice(index, 1);
-     });
-    };
-
-    homeCtrl.showAdvanced = function(ev, inst) {
-      $mdDialog.show({
-        controller: DialogController,
-        controllerAs: 'dialogController',
-        templateUrl: 'dialog1.tmpl.html',
-        parent: angular.element(document.body),
-        targetEvent: ev,
-        clickOutsideToClose:true,
-        bindToController: true,
-        locals: { institution: inst }
-      })
-      .then(function(answer) {
-        homeCtrl.status = 'You said the information was "' + answer + '".';
-      }, function() {
-        homeCtrl.status = 'You cancelled the dialog.';
-      });
-    };
-
-    function DialogController($mdDialog) {
-      var dialogController = this;
-      dialogController.hide = function() {
-        $mdDialog.hide();
-      };
-
-      dialogController.cancel = function() {
-        $mdDialog.cancel();
-      };
-
-      dialogController.answer = function(answer) {
-        $mdDialog.hide(answer);
-      };
-    };
-
+    homeCtrl.posts = [
+      {title: "Dolor sit amet", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent maximus id est in dapibus. Fusce lorem libero, vulputate quis purus maximus, auctor tempus enim. Sed."},
+      {title: "Consectetur adipiscing elit", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur convallis rutrum scelerisque. Aenean a ultricies sem. Quisque feugiat lorem id neque iaculis commodo. Suspendisse aliquam."},
+      {title: "Curabitur sollicitudin", text: "Curabitur sollicitudin velit massa, quis lobortis lectus iaculis in. Integer leo urna, eleifend quis ultricies a, aliquam in nisi. In varius, neque pretium faucibus luctus."},
+      {title: "Integer efficitur faucibus est", text: "Integer efficitur faucibus est, porttitor faucibus lorem sagittis a. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce quis mi."},
+      {title: "Suspendisse erat arcu", text: "Suspendisse erat arcu, pellentesque non bibendum a, viverra sed magna. Suspendisse potenti. In hac habitasse platea dictumst. Suspendisse accumsan, tellus quis sagittis tempus, sem ligula."},
+      {title: "Fusce ullamcorper elit eget", text: "Fusce ullamcorper elit eget convallis porttitor. In sit amet ultrices metus, a lacinia lacus. Maecenas velit magna, congue id tempor vitae, luctus vitae quam. Pellentesque."},
+      {title: "Duis consequat eu sem quis fermentum", text: "Duis consequat eu sem quis fermentum. Duis varius condimentum sodales. Ut id nibh sed magna semper tincidunt vitae eget justo. Suspendisse nibh risus, ornare at."},
+      {title: "Ut augue nulla", text: "Ut augue nulla, accumsan ac lobortis in, maximus ac eros. Duis in mattis lacus. Maecenas ut molestie leo, non consectetur ante. Integer elit est, convallis"},
+      {title: "Fusce interdum non magna congue dapibus", text: "Fusce interdum non magna congue dapibus. Aliquam ullamcorper orci nisl, ac ornare mi aliquam sed. Nunc sit amet elit id erat scelerisque mattis. Ut suscipit."}
+    ]
   });
 
   app.controller("NewInstitutionController", function NewInstitutionController() {

286924c25dbff81a77d78bfb0270007ba845e0b2
Andre Abrantes <andredossantosabrantes@gmail.com>
2017-05-23 16:20:28 -0300

    Validation when creating post

--- app/home/homeController.js
+++ app/home/homeController.js
@@ -1,8 +1,9 @@
 (function() {
     var app = angular.module("app");
 
-    app.controller("HomeController", function HomeController(PostService, AuthService, $interval) {
+    app.controller("HomeController", function HomeController(PostService, AuthService, $interval, $mdToast) {
         var homeCtrl = this;
+        
         homeCtrl.posts = [];
 
         Object.defineProperty(homeCtrl, 'user', {
@@ -11,34 +12,41 @@
             }
         });
 
-        homeCtrl.createdAt = function(post) {
-            if (post) {
-                post.created_at = new Date();
+        homeCtrl.createPost = function createPost(data) {
+            var post = new Post(data, homeCtrl.user.current_institution.key);
+            if (post.isValid()) {
+                PostService.createPost(post).then(function success(response) {
+                    showToast('Postado com sucesso!');
+                    homeCtrl.posts.push(response.data);
+                }, function error(response) {
+                    showToast(response.data.msg);
+                });
+            } else {
+                showToast('Post inválido!');
             }
         };
 
-        /** TODO 
-            Autor: Mayza Nunes 18/05/2016
-            Error treatment
-        **/
         var loadPosts = function() {
-            PostService.get().then(function(response) {
+            PostService.get().then(function success(response) {
                 homeCtrl.posts = response.data;
-            });
-        };
-
-        /** TODO 
-            Autor: Mayza Nunes 18/05/2016
-            Error treatment
-        **/
-        homeCtrl.post = function(post) {
-            PostService.post(post).then(function success(info) {
-                homeCtrl.posts.push(info.data)
+            }, function error(response) {
+                showToast(response.data.msg);
             });
         };
 
         loadPosts();
 
         $interval(loadPosts, 5000);
+
+        function showToast(msg) {
+            $mdToast.show(
+                $mdToast.simple()
+                    .textContent(msg)
+                    .action('FECHAR')
+                    .highlightAction(true)
+                    .hideDelay(5000)
+                    .position('bottom right')
+            );
+        }
     });
 })();

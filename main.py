effa7e848b6b18c6256d37024c0fbdd4641875c9
root <raoni.smaneoto@ccc.ufcg.edu.br>
2017-05-30 14:50:40 -0300

    Removing posts_handler file

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from handlers.login_logout_handler import LoginHandler
 from handlers.login_logout_handler import LogoutHandler
 from handlers.user_handler import UserHandler
 from handlers.post_handler import PostHandler
-from handlers.post_handler import PostsHandler
 from handlers.user_timeline_handler import UserTimelineHandler
 from handlers.erro_handler import ErroHandler
 from handlers.get_key_handler import GetKeyHandler

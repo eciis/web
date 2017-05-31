db0c3db353cdaf3e09fc9dbaecc8ae0af36729dc
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2017-05-31 16:39:50 -0300

    Add tests of operation replace

--- test/json_patch_test.py
+++ test/json_patch_test.py
@@ -45,60 +45,65 @@ class TestJsonPatch(unittest.TestCase):
     def setUp(cls):
         """Configure the objects every time a test is run."""
         cls.user = User("Luiz", 19, "I'm a computer scientist.")
-        cls.user.emails.append("luiz.silva@ccc.ucg.edu.br")
+        cls.user.emails.append("luiz.silva@ccc.ufcg.edu.br")
         cls.user.emails.append("fernan.luizsilva@gmail.com")
 
-    def test_add(self):
-        """Test operation add in JsonPatch."""
-        # Add attribute  registration in user
+
+class TestOperationAdd(TestJsonPatch):
+    """Class of test operation add."""
+
+    def test_add_simple_value(self):
+        """Add attribute  registration in user."""
         self.assertFalse(hasattr(self.user, 'registration'))
         json = create_json_patch('add', '/registration', '11112121')
         self.json_patch.load(json, self.user)
+        self.assertTrue(hasattr(self.user, 'registration'))
         self.assertEqual(self.user.registration, '11112121')
 
-        # Add attribute  user2 in user
+    def test_add_object(self):
+        """Add attribute  user2 in user."""
         self.assertFalse(hasattr(self.user, 'user2'))
         json = '[{"op": "add", "path": "/user2", "value": {"age": 23, "name": "Maiza"}}]'
         self.json_patch.load(json, self.user, User)
+        self.assertTrue(hasattr(self.user, 'user2'))
         self.assertEqual(self.user.user2.name, 'Maiza')
         self.assertEqual(self.user.user2.age, 23)
 
-        # Add new email at the end of the email list of user
+    def test_add_in_list(self):
+        """Add new email at the end of the email list of user."""
         self.assertTrue('fernan.luizsilva@hotmail.com' not in self.user.emails)
         json = create_json_patch('add', '/emails/-', 'fernan.luizsilva@hotmail.com')
         self.json_patch.load(json, self.user)
-        self.assertEqual(self.user.emails, [
-            "luiz.silva@ccc.ucg.edu.br",
+        self.assertListEqual(self.user.emails, [
+            "luiz.silva@ccc.ufcg.edu.br",
             "fernan.luizsilva@gmail.com",
             "fernan.luizsilva@hotmail.com"
         ])
 
-        # Add new email in list emails of user in a specific index
+    def test_add_in_index_list(self):
+        """Add new email in list emails of user in a specific index."""
         self.assertTrue('fernan.luizsilva@facebook.com' not in self.user.emails)
         json = create_json_patch('add', '/emails/1', 'fernan.luizsilva@facebook.com')
         self.json_patch.load(json, self.user)
-        self.assertEqual(self.user.emails, [
-            "luiz.silva@ccc.ucg.edu.br",
+        self.assertListEqual(self.user.emails, [
+            "luiz.silva@ccc.ufcg.edu.br",
             "fernan.luizsilva@facebook.com",
             "fernan.luizsilva@gmail.com",
-            "fernan.luizsilva@hotmail.com"
         ])
 
         # Add new email in list emails of user in a specific index
         self.assertTrue('fernan.luizsilva@outlook.com' not in self.user.emails)
         json = create_json_patch('add', '/emails/0', 'fernan.luizsilva@outlook.com')
         self.json_patch.load(json, self.user)
-        self.assertEqual(self.user.emails, [
+        self.assertListEqual(self.user.emails, [
             "fernan.luizsilva@outlook.com",
-            "luiz.silva@ccc.ucg.edu.br",
+            "luiz.silva@ccc.ufcg.edu.br",
             "fernan.luizsilva@facebook.com",
             "fernan.luizsilva@gmail.com",
-            "fernan.luizsilva@hotmail.com"
         ])
 
-    def test_add_err(self):
-        """Test error in operation add."""
-        # Add attribute  registration in user
+    def test_add_exists_attr(self):
+        """Add attribute  registration in user."""
         self.assertFalse(hasattr(self.user, 'registration'))
         json = create_json_patch('add', '/registration', '11112121')
         self.json_patch.load(json, self.user)
@@ -112,9 +117,58 @@ class TestJsonPatch(unittest.TestCase):
         self.assertEqual(str(ex.exception), "Attribute registration already exists")
         self.assertEqual(self.user.registration, '11112121')
 
-        #  Trying to add None
+    def test_add_none_attr(self):
+        """Trying to add None."""
         self.assertFalse(hasattr(self.user, 'user2'))
         json = '[{"op": "add", "path": "/user2"}]'
         with self.assertRaises(PatchException) as ex:
             self.json_patch.load(json, self.user)
         self.assertEqual(str(ex.exception), "Value can not be None")
+
+
+class TestOperantionReplace(TestJsonPatch):
+    """Class of test operation Replace."""
+
+    def test_replace_simple_value(self):
+        """Replece attribute name."""
+        self.assertEqual(self.user.name, "Luiz")
+        json = create_json_patch("replace", "/name", "Luiz Fernando da Silva")
+        self.json_patch.load(json, self.user)
+        self.assertNotEqual(self.user.name, "Luiz")
+        self.assertEqual(self.user.name, "Luiz Fernando da Silva")
+
+    def test_replace_object(self):
+        """Replace attribute user2 in user."""
+        json = '[{"op": "add", "path": "/user2", "value": {"age": 23, "name": "Maiza"}}]'
+        self.json_patch.load(json, self.user, User)
+
+        self.assertEqual(self.user.user2.name, "Maiza")
+        self.assertEqual(self.user.user2.age, 23)
+        json = '[{"op": "replace", "path": "/user2", "value": {"age": 19, "name": "Luiz"}}]'
+        self.json_patch.load(json, self.user, User)
+        self.assertNotEqual(self.user.user2.name, "Maiza")
+        self.assertNotEqual(self.user.user2.age, 23)
+        self.assertEqual(self.user.user2.name, "Luiz")
+        self.assertEqual(self.user.user2.age, 19)
+
+    def test_replace_in_list(self):
+        """Replace email in list of emails."""
+        self.assertListEqual(self.user.emails, [
+            "luiz.silva@ccc.ufcg.edu.br",
+            "fernan.luizsilva@gmail.com"
+        ])
+        json = create_json_patch("replace", "/emails/-", "fernan.luizsilva@hotmail.com")
+        self.json_patch.load(json, self.user)
+        self.assertListEqual(self.user.emails, [
+            "luiz.silva@ccc.ufcg.edu.br",
+            "fernan.luizsilva@hotmail.com"
+        ])
+
+    def test_replace_value_none(self):
+        """Replace attribute without passing the value."""
+        self.assertEqual(self.user.name, "Luiz")
+        json = create_json_patch("replace", "/name")
+        with self.assertRaises(PatchException) as ex:
+            self.json_patch.load(json, self.user)
+        self.assertEqual(str(ex.exception), "Value can not be None")
+        self.assertEqual(self.user.name, "Luiz")

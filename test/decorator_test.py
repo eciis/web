8d82d6f894ba9b623564df0eb5597eb2ba42ae6d
Raoni Smaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2017-06-02 11:33:03 -0300

    Some refactors

--- test/decorator_test.py
+++ test/decorator_test.py
@@ -86,59 +86,60 @@ def is_decorated(self, user, key):
 
 
 def initModels(cls):
+    """Init the models."""
     # new User Mayza
-        cls.mayza = User()
-        cls.mayza.name = 'Mayza Nunes'
-        cls.mayza.cpf = '089.675.908-90'
-        cls.mayza.email = 'mayzabeel@gmail.com'
-        cls.mayza.institutions = []
-        cls.mayza.follows = []
-        cls.mayza.institutions_admin = []
-        cls.mayza.notifications = []
-        cls.mayza.posts = []
-        cls.mayza.put()
-        # new User Raoni
-        cls.raoni = User()
-        cls.raoni.name = 'Raoni Smaneoto'
-        cls.raoni.cpf = '089.675.908-65'
-        cls.raoni.email = 'raoni.smaneoto@ccc.ufcg.edu.br'
-        cls.raoni.institutions = []
-        cls.raoni.follows = []
-        cls.raoni.institutions_admin = []
-        cls.raoni.notifications = []
-        cls.raoni.posts = []
-        cls.raoni.put()
-        # new User Ruan
-        cls.ruan = User()
-        cls.ruan.name = 'Ruan'
-        cls.ruan.cpf = '089.675.908-65'
-        cls.ruan.email = 'ruan@gmail.com'
-        cls.ruan.institutions = []
-        cls.ruan.follows = []
-        cls.ruan.institutions_admin = []
-        cls.ruan.notifications = []
-        cls.ruan.posts = []
-        cls.ruan.put()
-        # new Institution CERTBIO
-        cls.certbio = Institution()
-        cls.certbio.name = 'CERTBIO'
-        cls.certbio.cnpj = '18.104.068/0001-86'
-        cls.certbio.legal_nature = 'public'
-        cls.certbio.address = 'Universidade Federal de Campina Grande'
-        cls.certbio.occupation_area = ''
-        cls.certbio.description = 'Ensaio Químico - Determinação de Material Volátil por \
+    cls.mayza = User()
+    cls.mayza.name = 'Mayza Nunes'
+    cls.mayza.cpf = '089.675.908-90'
+    cls.mayza.email = 'mayzabeel@gmail.com'
+    cls.mayza.institutions = []
+    cls.mayza.follows = []
+    cls.mayza.institutions_admin = []
+    cls.mayza.notifications = []
+    cls.mayza.posts = []
+    cls.mayza.put()
+    # new User Raoni
+    cls.raoni = User()
+    cls.raoni.name = 'Raoni Smaneoto'
+    cls.raoni.cpf = '089.675.908-65'
+    cls.raoni.email = 'raoni.smaneoto@ccc.ufcg.edu.br'
+    cls.raoni.institutions = []
+    cls.raoni.follows = []
+    cls.raoni.institutions_admin = []
+    cls.raoni.notifications = []
+    cls.raoni.posts = []
+    cls.raoni.put()
+    # new User Ruan
+    cls.ruan = User()
+    cls.ruan.name = 'Ruan'
+    cls.ruan.cpf = '089.675.908-65'
+    cls.ruan.email = 'ruan@gmail.com'
+    cls.ruan.institutions = []
+    cls.ruan.follows = []
+    cls.ruan.institutions_admin = []
+    cls.ruan.notifications = []
+    cls.ruan.posts = []
+    cls.ruan.put()
+    # new Institution CERTBIO
+    cls.certbio = Institution()
+    cls.certbio.name = 'CERTBIO'
+    cls.certbio.cnpj = '18.104.068/0001-86'
+    cls.certbio.legal_nature = 'public'
+    cls.certbio.address = 'Universidade Federal de Campina Grande'
+    cls.certbio.occupation_area = ''
+    cls.certbio.description = 'Ensaio Químico - Determinação de Material Volátil por \
             Gravimetria e Ensaio Biológico - Ensaio de Citotoxicidade'
-        cls.certbio.email = 'certbio@ufcg.edu.br'
-        cls.certbio.phone_number = '(83) 3322 4455'
-        cls.certbio.members = [cls.mayza.key, cls.raoni.key]
-        cls.certbio.followers = [cls.mayza.key, cls.raoni.key]
-        cls.certbio.posts = []
-        cls.certbio.admin = cls.mayza.key
-        cls.certbio.put()
-        # POST of Mayza To Certbio Institution
-        cls.mayza_post = Post()
-        cls.mayza_post.title = "Novo edital do CERTBIO"
-        cls.mayza_post.text = "At vero eos et accusamus et iusto odio dignissimos \
+    cls.certbio.email = 'certbio@ufcg.edu.br'
+    cls.certbio.phone_number = '(83) 3322 4455'
+    cls.certbio.members = [cls.mayza.key, cls.raoni.key]
+    cls.certbio.followers = [cls.mayza.key, cls.raoni.key]
+    cls.certbio.posts = []
+    cls.certbio.admin = cls.mayza.key
+    cls.certbio.put()
+    # POST of Mayza To Certbio Institution
+    cls.mayza_post = Post()
+    cls.mayza_post.title = "Novo edital do CERTBIO"
+    cls.mayza_post.text = "At vero eos et accusamus et iusto odio dignissimos \
         ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti \
         quos dolores et quas molestias excepturi sint occaecati cupiditate \
         non provident, similique sunt in culpa qui officia deserunt mollitia \
@@ -151,31 +152,31 @@ def initModels(cls):
         et molestiae non recusandae. Itaque earum rerum hic tenetur sapiente \
         delectus, ut aut reiciendis voluptatibus maiores alias consequatur \
         aut perferendis doloribus asperiores repellat."
-        cls.mayza_post.author = cls.mayza.key
-        cls.mayza_post.institution = cls.certbio.key
-        cls.mayza_post.put()
-        # new Institution SPLAB
-        cls.splab = Institution()
-        cls.splab.name = 'SPLAB'
-        cls.splab.cnpj = '18.104.068/0001-56'
-        cls.splab.legal_nature = 'public'
-        cls.splab.address = 'Universidade Federal de Campina Grande'
-        cls.splab.occupation_area = ''
-        cls.splab.description = 'The mission of the Software Practices Laboratory (SPLab) \
+    cls.mayza_post.author = cls.mayza.key
+    cls.mayza_post.institution = cls.certbio.key
+    cls.mayza_post.put()
+    # new Institution SPLAB
+    cls.splab = Institution()
+    cls.splab.name = 'SPLAB'
+    cls.splab.cnpj = '18.104.068/0001-56'
+    cls.splab.legal_nature = 'public'
+    cls.splab.address = 'Universidade Federal de Campina Grande'
+    cls.splab.occupation_area = ''
+    cls.splab.description = 'The mission of the Software Practices Laboratory (SPLab) \
             is to promote the development of the state-of-the-art in the \
             theory and practice of Software Engineering.'
-        cls.splab.image_url = 'http://amaurymedeiros.com/images/splab.png'
-        cls.splab.email = 'splab@ufcg.edu.br'
-        cls.splab.phone_number = '(83) 3322 7865'
-        cls.splab.members = [cls.mayza.key, cls.ruan.key]
-        cls.splab.followers = [cls.mayza.key, cls.ruan.key]
-        cls.splab.posts = []
-        cls.splab.admin = cls.mayza.key
-        cls.splab.put()
-        # POST of Raoni To Certbio Institution
-        cls.raoni_post = Post()
-        cls.raoni_post.title = "Novwdfssdo edital do CERTBIO"
-        cls.raoni_post.text = "At vero eos et accusamus et iusto odio dignissimos \
+    cls.splab.image_url = 'http://amaurymedeiros.com/images/splab.png'
+    cls.splab.email = 'splab@ufcg.edu.br'
+    cls.splab.phone_number = '(83) 3322 7865'
+    cls.splab.members = [cls.mayza.key, cls.ruan.key]
+    cls.splab.followers = [cls.mayza.key, cls.ruan.key]
+    cls.splab.posts = []
+    cls.splab.admin = cls.mayza.key
+    cls.splab.put()
+    # POST of Raoni To Certbio Institution
+    cls.raoni_post = Post()
+    cls.raoni_post.title = "Novwdfssdo edital do CERTBIO"
+    cls.raoni_post.text = "At vero eos et accusamus et iusto odio dignissimos \
         ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti \
         quos dolores et quas molestias excepturi sint occaecati cupiditate \
         non provident, similique sunt in dfsfdsfsdculpa qui officia deseritia \
@@ -188,18 +189,18 @@ def initModels(cls):
         et molestiae non recusandae. Itaque earum rerum hic tenetur sapiente \
         delectus, ut aut reiciendis voluptatibus maiores alias consequatur \
         aut perferendis doloribus asperiores repellat."
-        cls.raoni_post.author = cls.raoni.key
-        cls.raoni_post.institution = cls.splab.key
-        cls.raoni_post.put()
-        # POST of Raoni To Certbio Institution
-        cls.ruan_post = Post()
-        cls.ruan_post.title = "Novwdfssdo edital do CERTBIO"
-        cls.ruan_post.text = "At vero eos et accusamus et iusto odio dignissimos \
+    cls.raoni_post.author = cls.raoni.key
+    cls.raoni_post.institution = cls.splab.key
+    cls.raoni_post.put()
+    # POST of Raoni To Certbio Institution
+    cls.ruan_post = Post()
+    cls.ruan_post.title = "Novwdfssdo edital do CERTBIO"
+    cls.ruan_post.text = "At vero eos et accusamus et iusto odio dignissimos \
         emporibus autem quibusdam et aut officiis debitis aut rerum \
         necessitatibus saepe eveniet ut et voluptates repudiandae sint \
         et molestiae non recusandae. Itaque earum rerum hic tenetur sapiente \
         delectus, ut aut reiciendis voluptatibus maiores alias consequatur \
         aut perferendis doloribus asperiores repellat."
-        cls.ruan_post.author = cls.ruan.key
-        cls.ruan_post.institution = cls.certbio.key
-        cls.ruan_post.put()
+    cls.ruan_post.author = cls.ruan.key
+    cls.ruan_post.institution = cls.certbio.key
+    cls.ruan_post.put()

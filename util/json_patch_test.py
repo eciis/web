8bc01963ab5b6b9b2cbf426d49aec6217e8da3b8
André L. Abrantes <andredossantosabrantes@gmail.com>
2017-06-05 10:06:29 -0300

    Insert app engine sdk on test

--- util/json_patch_test.py
+++ util/json_patch_test.py
@@ -3,6 +3,11 @@
 
 import unittest
 
+import sys
+sys.path.append("../")
+sys.path.insert(1, '../google_appengine')
+sys.path.insert(1, '../google_appengine/lib/yaml/lib')
+
 from json_patch import JsonPatch
 from json_patch import PatchException
 

b70fd649f7aa04f0b21042218017c760191e4278
Raoni Smaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2017-06-12 11:18:00 -0300

    Creating a new super class

--- test/post_collection_handler_test.py
+++ test/post_collection_handler_test.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Post Collection handler test."""
 
-from test_base import TestBase
+from test_base_handler import TestBaseHandler
 from models.user import User
 from models.institution import Institution
 from handlers.post_collection_handler import PostCollectionHandler
@@ -9,19 +9,13 @@ from google.appengine.ext import ndb
 import json
 
 
-class PostCollectionHandlerTest(TestBase):
+class PostCollectionHandlerTest(TestBaseHandler):
     """Post Collection handler test."""
 
     @classmethod
     def setUp(cls):
         """Provide the base for the tests."""
-        cls.test = cls.testbed.Testbed()
-        cls.test.activate()
-        cls.policy = cls.datastore.PseudoRandomHRConsistencyPolicy(
-            probability=1)
-        cls.test.init_datastore_v3_stub(consistency_policy=cls.policy)
-        cls.test.init_memcache_stub()
-        cls.ndb.get_context().set_cache_policy(False)
+        super(PostCollectionHandlerTest, cls).setUp()
         app = cls.webapp2.WSGIApplication(
             [("/api/post", PostCollectionHandler),
              ], debug=True)
---
+++ test/test_base_handler.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Imports."""
+from test_base import TestBase
+
+
+class TestBaseHandler(TestBase):
+    """SuperClass of the handler's tests."""
+
+    @classmethod
+    def setUp(cls):
+        """Provide the base for the tests."""
+        cls.test = cls.testbed.Testbed()
+        cls.test.activate()
+        cls.policy = cls.datastore.PseudoRandomHRConsistencyPolicy(
+            probability=1)
+        cls.test.init_datastore_v3_stub(consistency_policy=cls.policy)
+        cls.test.init_memcache_stub()
+        cls.ndb.get_context().set_cache_policy(False)

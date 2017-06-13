ab978685714d58a4b1b050e6cfbdba004bbe60e4
MaianaB <maiana.brito@ccc.ufcg.edu.br>
2017-06-13 08:51:08 -0300

    Test case: user member unfollow in institution

--- test/followers_institution_test.py
+++ test/followers_institution_test.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 """Institution follower handler test."""
-import unittest
 from test_base import TestBase
 from models.user import User
 from models.institution import Institution
@@ -61,19 +60,6 @@ class InstitutionFollowersHandlerTest(TestBase):
         # Pretend an authentication
         self.os.environ['REMOTE_USER'] = 'mayzabeel@gmail.com'
         self.os.environ['USER_EMAIL'] = 'mayzabeel@gmail.com'
-        # Verified objects, are empty
-        self.assertTrue(len(self.certbio.followers) == 0, "The number of followers expected was 0")
-        self.assertTrue(len(self.mayza.follows) == 0, "The number of follows expected was 0")
-        # Call the delete method
-        self.testapp.delete("/api/institution/%s/followers" % self.certbio.key.urlsafe())
-
-        # Update the objects
-        self.mayza = self.mayza.key.get()
-        self.certbio = self.certbio.key.get()
-
-        # Don't changed
-        self.assertTrue(len(self.mayza.follows) == 0, "The number of follows expected was 0")
-        self.assertTrue(len(self.certbio.followers) == 0, "The number of followers expected was 0")
 
         # Call the post method
         self.testapp.post("/api/institution/%s/followers" % self.certbio.key.urlsafe())
@@ -91,9 +77,29 @@ class InstitutionFollowersHandlerTest(TestBase):
         self.mayza = self.mayza.key.get()
         self.certbio = self.certbio.key.get()
 
-        # Remove one follower
+        # Remove one follower. Admin can unfollow
         self.assertTrue(len(self.mayza.follows) == 0, "The number of follows expected was 0")
         self.assertTrue(len(self.certbio.followers) == 0, "Number of followers expected was 0")
+
+    def teste_delete_usermember(self):
+        """Test that user member try unfollow the institution."""
+        # Pretend an authentication
+        self.os.environ['REMOTE_USER'] = 'maiana.brito@ccc.ufcg.edu.br'
+        self.os.environ['USER_EMAIL'] = 'maiana.brito@ccc.ufcg.edu.br'
+
+        # Call the delete method
+        self.testapp.post("/api/institution/%s/followers" % self.certbio.key.urlsafe())
+
+        # Call the delete method
+        self.testapp.delete("/api/institution/%s/followers" % self.certbio.key.urlsafe())
+
+        # Update the objects
+        self.maiana = self.maiana.key.get()
+        self.certbio = self.certbio.key.get()
+
+        # Don't remove users are members of institution
+        self.assertTrue(len(self.maiana.follows) == 1, "The number of follows expected was 1")
+        self.assertTrue(len(self.certbio.followers) == 1, "Number of followers expected was 1")
 
     def tearDown(cls):
         """Deactivate the test."""
@@ -113,7 +119,18 @@ def initModels(cls):
     cls.mayza.notifications = []
     cls.mayza.posts = []
     cls.mayza.put()
-    # new Institution CERTBIO
+    # new User Maiana
+    cls.maiana = User()
+    cls.maiana.name = 'Maiana Brito'
+    cls.maiana.cpf = '089.675.908-91'
+    cls.maiana.email = 'maiana.brito@ccc.ufcg.edu.br'
+    cls.maiana.institutions = []
+    cls.maiana.follows = []
+    cls.maiana.institutions_admin = []
+    cls.maiana.notifications = []
+    cls.maiana.posts = []
+    cls.maiana.put()
+    # new Institution CERTBIO, user Maiana is follower
     cls.certbio = Institution()
     cls.certbio.name = 'CERTBIO'
     cls.certbio.cnpj = '18.104.068/0001-86'
@@ -124,8 +141,12 @@ def initModels(cls):
             Gravimetria e Ensaio Biológico - Ensaio de Citotoxicidade'
     cls.certbio.email = 'certbio@ufcg.edu.br'
     cls.certbio.phone_number = '(83) 3322 4455'
-    cls.certbio.members = []
+    cls.certbio.members = [cls.maiana.key]
     cls.certbio.followers = []
     cls.certbio.posts = []
     cls.certbio.admin = cls.mayza.key
     cls.certbio.put()
+
+    # Update user
+    cls.maiana.institutions = [cls.certbio.key]
+    cls.maiana.put()

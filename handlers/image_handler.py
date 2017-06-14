cd8707d42c6ee9c938ea23d5c68b4ea224846983
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2017-06-14 14:36:10 -0300

    Restricting type of images to be saved

--- handlers/image_handler.py
+++ handlers/image_handler.py
@@ -2,12 +2,15 @@
 """Image Handler. """
 
 import cloudstorage
+import mimetypes
 from google.appengine.api import app_identity
 from google.appengine.ext import blobstore
 from handlers.base_handler import BaseHandler
+from google.appengine.api import images
 
 from utils import login_required
 from utils import json_response
+from utils import Utils
 
 
 class ImageUploadHandler(BaseHandler):
@@ -17,20 +20,33 @@ class ImageUploadHandler(BaseHandler):
     @json_response
     def post(self, user):
         """Save image in data store."""
-        file_upload = self.request.POST.get("file", None)
-        file_name = file_upload.filename
-        file = file_upload.file.read()
+        file_uploaded = self.request.POST.get("file", None)
+        image_name = file_uploaded.filename
+        image = file_uploaded.file.read()
+
+        # TODO modify exception
+        Utils._assert(
+            not image_name.endswith(".jpg") and not image_name.endswith(".png"),
+            "Image type should be jpg or png", Exception)
+
+        # TODO Define image size
+        image = images.resize(image, 720, 480)
+        image_type = mimetypes.guess_type(image_name)[0]
 
         # Get the default Cloud Storage Bucket name and create a file name for
         # the object in Cloud Storage.
         bucket = app_identity.get_default_gcs_bucket_name()
 
         # Cloud Storage file names are in the format /bucket/object.
-        filename = '/{}/{}'.format(bucket, file_name)
+        filename = '/{}/{}'.format(bucket, image_name)
 
         # Create a file in Google Cloud Storage and write something to it.
-        with cloudstorage.open(filename, 'w') as filehandle:
-            filehandle.write(file)
+        with cloudstorage.open(
+            filename,
+            'w',
+            content_type=image_type
+        ) as filehandle:
+            filehandle.write(image)
 
         # In order to read the contents of the file using the Blobstore API,
         # you must create a blob_key from the Cloud Storage file name.
@@ -47,6 +63,7 @@ class ViewImageHandler(BaseHandler):
     def get(self, image_key):
         """Get image."""
         if blobstore.get(image_key):
+            blob_info = blobstore.get(image_key)
             blob_key = image_key
             # Instantiate a BlobReader for a given Blobstore blob_key.
             blob_reader = blobstore.BlobReader(blob_key)
@@ -65,7 +82,7 @@ class ViewImageHandler(BaseHandler):
             blob_reader_data = blob_reader.read()
 
             # Write the contents to the response.
-            self.response.headers['Content-Type'] = 'image/jpg'
+            self.response.headers['Content-Type'] = blob_info.content_type.encode('utf-8')
             self.response.write(blob_reader_data)
         else:
             self.response.status(404)

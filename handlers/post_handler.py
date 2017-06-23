8acecefc16c828626af9cb424585cd99e378b524
André L. Abrantes <andredossantosabrantes@gmail.com>
2017-06-23 10:18:12 -0300

    Add TODO comment

--- handlers/post_handler.py
+++ handlers/post_handler.py
@@ -30,6 +30,11 @@ class PostHandler(BaseHandler):
         """Update the post, the user and the institution in datastore."""
         post.put()
 
+
+    """
+    TODO: Test is_authorized
+    @author: Andre L Abrantes - 23-06-2017
+    """
     @json_response
     @login_required
     @is_authorized

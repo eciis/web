74d3a40115a2730b97e1460238bf1dd7f3b69762
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2017-06-23 09:27:16 -0300

    Resolving problems

--- app/imageUpload/uploadDirective.js
+++ app/imageUpload/uploadDirective.js
@@ -1,24 +1,44 @@
 "use strict";
-(function() {
-var app = angular.module('app');
 
-    app.controller('UpladImageController', ['Upload', function (Upload) {
-        var vm = this;
+(function() {
+    var app = angular.module('app');
+
+    app.controller('UpladImageController', function (Upload, $mdToast) {
+        var uploadImgCtrl = this;
+        var URL_UPLOAD_IMAGE = '/api/images';
 
         // upload on file select or drop
-        vm.upload = function (file, model) {
-            console.log(file);
-            Upload.upload({
-                url: '/api/images',
-                data: {image: file}
-            }).then(function (resp) {
-                model.photo_url = resp.data.file_url;
-                delete vm.file;
-            }, function (resp) {
-                console.log('Error status: ' + resp.status);
-            });
+        uploadImgCtrl.upload = function (file, model) {
+            var jpgType = "image/jpeg";
+            var pngType = "image/png";
+            var maximumSize = 5242880; // 5Mb in bytes
+
+            if ((file.type === jpgType || file.type === pngType) && file.size <= maximumSize) {
+                Upload.upload({
+                    url: URL_UPLOAD_IMAGE,
+                    data: {image: file}
+                }).then(function (response) {
+                    model.photo_url = response.data.file_url;
+                    uploadImgCtrl.file = null;
+                }, function (response) {
+                    showToast("Problemas encontrados ao fazer upload da imagem" + response.config.data.image.name);
+                });
+            } else {
+                showToast("Image deve ser do tipo png ou jpeg e deve ter tamanho menor ou igual a 5 mega bytes");
+            }
         };
-    }]);
+
+        function showToast(msg) {
+            $mdToast.show(
+                $mdToast.simple()
+                    .textContent(msg)
+                    .action('FECHAR')
+                    .highlightAction(true)
+                    .hideDelay(5000)
+                    .position('bottom right')
+            );
+        }
+    });
 
     app.directive("uploadImage", function() {
         return {

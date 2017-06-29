e485b70f7ff816da15fe03d0db797b198056a5e5
ruanres <ruan.silveira@ccc.ufcg.edu.br>
2017-06-29 15:16:30 -0300

    unnecessary spies removed

--- app/spec/post.spec.js
+++ app/spec/post.spec.js
@@ -16,40 +16,34 @@ describe('Test Post Model:', function() {
     it('should be invalid because of empty title', function() {
         data.title = "";
         post = new Post(data, {});
-        spyOn(post, 'isValid').and.returnValue(false);
         expect(post.isValid()).toBeFalsy();
     });
 
     it('should be invalid because of undefined title', function() {
         data.title = undefined;
         post = new Post(data, {});
-        spyOn(post, 'isValid').and.returnValue(false);
         expect(post.isValid()).toBeFalsy();
     });
 
      it('should be invalid because of empty text', function() {
         data.text = "";
         post = new Post(data, {});
-        spyOn(post, 'isValid').and.returnValue(false);
         expect(post.isValid()).toBeFalsy();
     });
 
     it('should be invalid because of undefined text', function() {
         data.text = undefined;
         post = new Post(data, {});
-        spyOn(post, 'isValid').and.returnValue(false);
         expect(post.isValid()).toBeFalsy();
     });
 
     it('should be invalid because of undefined institution', function() {
         post = new Post(data, undefined);
-        spyOn(post, 'isValid').and.returnValue(false);
         expect(post.isValid()).toBeFalsy();
     });
 
     it('should be valid', function() {
         post = new Post(data, {});
-        spyOn(post, 'isValid').and.returnValue(true);
         expect(post.isValid()).toBeTruthy();
     });
 });

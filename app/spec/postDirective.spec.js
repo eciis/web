7a0b26b153911db6df3bc4eb596070ecb563a475
ruanres <ruan.silveira@ccc.ufcg.edu.br>
2017-06-29 09:53:55 -0300

    created tests to postDirective

--- app/spec/postDirective.spec.js
+++ app/spec/postDirective.spec.js
@@ -3,64 +3,99 @@
 describe('Unit: postDirective', function() {
     beforeEach(module('app'));
 
-    var ctrl, post, $mdDialog, PostService;
+    var postCtrl, post, user, httpBackend, scope, deffered, mdDialog, rootScope, postService;
 
-    beforeEach(inject(function($controller, $mdDialog, PostService) {
-        ctrl = $controller('PostController');
-        $mdDialog = $mdDialog;
-        PostService = PostService;
+    beforeEach(inject(function($controller, $httpBackend, $q, $mdDialog, PostService, AuthService, $mdToast, $rootScope) {
+        postCtrl = $controller('PostController');
+        httpBackend = $httpBackend;
+        rootScope = $rootScope;
+        scope = $rootScope.$new();
+        deffered = $q.defer();
+        mdDialog = $mdDialog;
+        postService = PostService;
+        user = {
+            name: 'name',
+            current_institution: {key: "abc"}
+        }
         post = {
             title: 'title',
             text: 'text',
             institution: {}
         };
-        ctrl.user = {
-            current_institution: {key: "abc"}
-        };
+        postCtrl.user = user;
+        httpBackend.when('GET', '/api/user').respond(user);
+        httpBackend.when('POST', '/api/posts').respond(post);
+        httpBackend.when('GET', 'main/main.html').respond(200);
+        httpBackend.when('GET', 'home/home.html').respond(200);
+        httpBackend.flush();   
     }));
 
+    afterEach(function() {
+        httpBackend.verifyNoOutstandingExpectation();
+        httpBackend.verifyNoOutstandingRequest();
+    });
+
     it('post should not to have properties', function() {
-        expect(ctrl.post).toEqual({});
+        expect(postCtrl.post).toEqual({});
     });
 
     it('post should be invalid', function() {
         post.title = undefined;
-        ctrl.post = new Post(post, {});
-        spyOn(ctrl, 'isPostValid').and.returnValue(false);
-        expect(ctrl.isPostValid()).toBeFalsy();
-        expect(ctrl.isPostValid).toHaveBeenCalled();
+        postCtrl.post = new Post(post, {});
+        spyOn(postCtrl, 'isPostValid').and.returnValue(false);
+        expect(postCtrl.isPostValid()).toBeFalsy();
+        expect(postCtrl.isPostValid).toHaveBeenCalled();
     });
 
     it('post should be valid', function() {
-        ctrl.post = new Post(post, {});
-        spyOn(ctrl, 'isPostValid').and.returnValue(true);
-        expect(ctrl.isPostValid()).toBeTruthy();
-        expect(ctrl.isPostValid).toHaveBeenCalled();
+        postCtrl.post = new Post(post, {});
+        spyOn(postCtrl, 'isPostValid').and.returnValue(true);
+        expect(postCtrl.isPostValid()).toBeTruthy();
+        expect(postCtrl.isPostValid).toHaveBeenCalled();
     });
 
     it('should change the current post instance to an empty object', function() {
-        ctrl.post = new Post(post, {});
-        spyOn(ctrl, 'clearPost').and.callThrough();
-        ctrl.clearPost();
-        expect(ctrl.post).toEqual({});
-        expect(ctrl.clearPost).toHaveBeenCalled();
+        postCtrl.post = new Post(post, {});
+        spyOn(postCtrl, 'clearPost').and.callThrough();
+        postCtrl.clearPost();
+        expect(postCtrl.post).toEqual({});
+        expect(postCtrl.clearPost).toHaveBeenCalled();
     });
 
     it('should cancel dialog', function() {
-        spyOn(ctrl, 'cancelDialog');
-        ctrl.cancelDialog();
-        expect(ctrl.cancelDialog).toHaveBeenCalled();
+        spyOn(postCtrl, 'cancelDialog');
+        postCtrl.cancelDialog();
+        expect(postCtrl.cancelDialog).toHaveBeenCalled();
     });
 
-    it('should create a post', function() {
-        ctrl.post = post;
-        spyOn(ctrl, 'createPost').and.callThrough();
-        spyOn(new Post(), 'isValid').and.returnValue(true);
-        // spyOn(PostService, 'createPost').and.callThrough();
-        spyOn(ctrl, 'clearPost').and.callThrough();
-        ctrl.createPost();
-        expect(ctrl.createPost).toHaveBeenCalled();
-        // expect(PostService.createPost).toHaveBeenCalled();
+    it('should create a post (successfull case)', function() {
+        postCtrl.post = post;
+        var newPost = new Post(postCtrl.post, postCtrl.user.current_institution.key);
+        spyOn(postService, 'createPost').and.returnValue(deffered.promise);
+        spyOn(postCtrl, 'clearPost').and.callThrough();
+        spyOn(mdDialog, 'hide');
+        deffered.resolve(post);
+        postCtrl.createPost();  
+        rootScope.$apply();
+        expect(postService.createPost).toHaveBeenCalledWith(newPost);
+        expect(postCtrl.clearPost).toHaveBeenCalled();
+        expect(mdDialog.hide).toHaveBeenCalled();
+    });
 
+    it('should not create a post (Fail case)', function() {
+        postCtrl.post = post;
+        spyOn(postService, 'createPost').and.returnValue(deffered.promise);
+        spyOn(mdDialog, 'hide');
+        deffered.reject({status: 400, data: {msg: 'Erro'}});
+        postCtrl.createPost();  
+        rootScope.$apply();
+        expect(postService.createPost).toHaveBeenCalled();
+        expect(mdDialog.hide).toHaveBeenCalled();
+    });
+
+    it('should not create a post (post invalid)', function() {
+        postCtrl.post = {};
+        postCtrl.createPost();  
+        rootScope.$apply();
     });
 });

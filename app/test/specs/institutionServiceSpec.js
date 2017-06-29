2e6150ef3b23b8592b1d90489b0883fcc04fb99a
Raoni Smaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2017-06-29 16:26:02 -0300

    Renaming the tests, removing the failure tests and changing the respond/expect method

--- app/test/specs/institutionServiceSpec.js
+++ app/test/specs/institutionServiceSpec.js
@@ -16,33 +16,24 @@
             $q = _$q_;
             $http = _$http_;
             service = InstitutionService;
+            httpBackend.when('GET', 'main/main.html').respond(200);
+            httpBackend.when('GET', 'home/home.html').respond(200);
+            httpBackend.when('GET', 'error/error.html').respond(200);
         }));
 
-        it('Spy getInstitutions in success case', function() {
+        it('Test getInstitutions in success case', function() {
             spyOn($http, 'get').and.callThrough();
-            httpBackend.when('GET', INSTITUTIONS_URI).respond($q.when(institutions));
+            httpBackend.when('GET', INSTITUTIONS_URI).respond(institutions);
             var result;
             service.getInstitutions().then(function(data){
                 result = data;
             });
             httpBackend.flush();
             expect($http.get).toHaveBeenCalled();
-            expect(result.data.$$state.value).toEqual(institutions);
+            expect(result.data).toEqual(institutions);
         });
 
-        it('Spy getInstitutions in fail case', function() {
-            spyOn($http, 'get').and.callThrough();
-            httpBackend.when('GET', INSTITUTIONS_URI).respond(404, $q.when());
-            var result;
-            service.getInstitutions().catch(function(data){
-                result = data;
-            });
-            httpBackend.flush();
-            expect($http.get).toHaveBeenCalled();
-            expect(result.status).toEqual(404);
-        });
-
-        it('Spy follow', function() {
+        it('Test follow', function() {
             spyOn($http, 'post').and.callThrough();
             httpBackend.when('POST', INSTITUTIONS_URI+ "/" + institutions[0].key + "/followers").respond({});
             service.follow(institutions[0].key);
@@ -50,7 +41,7 @@
             expect($http.post).toHaveBeenCalled();
         });
 
-        it('Spy unfollow', function() {
+        it('Test unfollow', function() {
             spyOn($http, 'delete').and.callThrough();
             httpBackend.when('DELETE', INSTITUTIONS_URI+ "/" + institutions[0].key + "/followers").respond({});
             service.unfollow(institutions[0].key);
@@ -58,101 +49,52 @@
             expect($http.delete).toHaveBeenCalled();
         });
 
-        it('Spy getTimeline in success case', function() {
+        it('Test getTimeline in success case', function() {
             spyOn($http, 'get').and.callThrough();
-            httpBackend.when('GET', INSTITUTIONS_URI + "/" + institutions[0].key + "/timeline").respond($q.when(post));
+            httpBackend.when('GET', INSTITUTIONS_URI + "/" + institutions[0].key + "/timeline").respond(post);
             var result;
             service.getTimeline(institutions[0].key).then(function(data){
                 result = data;
             });
             httpBackend.flush();
             expect($http.get).toHaveBeenCalled();
-            expect(result.data.$$state.value).toEqual(post);
+            expect(result.data).toEqual(post);
         });
 
-        it('Spy getTimeline in fail case', function() {
+        it('Test getMembers in success case', function() {
             spyOn($http, 'get').and.callThrough();
-            httpBackend.when('GET', INSTITUTIONS_URI + "/" + institutions[0].key + "/timeline").respond(404, $q.when());
-            var result;
-            service.getTimeline(institutions[0].key).catch(function(data){
-                result = data;
-            });
-            httpBackend.flush();
-            expect($http.get).toHaveBeenCalled();
-            expect(result.status).toEqual(404);
-        });
-
-        it('Spy getMembers in success case', function() {
-            spyOn($http, 'get').and.callThrough();
-            httpBackend.when('GET', INSTITUTIONS_URI + "/" + institutions[0].key + "/members").respond($q.when(institutions[0].members[0]));
+            httpBackend.when('GET', INSTITUTIONS_URI + "/" + institutions[0].key + "/members").respond(institutions[0].members[0]);
             var result;
             service.getMembers(institutions[0].key).then(function(data){
                 result = data;
             });
             httpBackend.flush();
             expect($http.get).toHaveBeenCalled();
-            expect(result.data.$$state.value).toEqual(raoni);
+            expect(result.data).toEqual(raoni);
         });
 
-        it('Spy getMembers in fail case', function() {
+        it('Test getFollowers in success case', function() {
             spyOn($http, 'get').and.callThrough();
-            httpBackend.when('GET', INSTITUTIONS_URI + "/" + institutions[0].key + "/members").respond(500, $q.when());
-            var result;
-            service.getMembers(institutions[0].key).catch(function(data){
-                result = data;
-            });
-            httpBackend.flush();
-            expect($http.get).toHaveBeenCalled();
-            expect(result.data.$$state.value).toEqual(undefined);
-        });
-
-        it('Spy getFollowers in success case', function() {
-            spyOn($http, 'get').and.callThrough();
-            httpBackend.when('GET', INSTITUTIONS_URI + "/" + institutions[0].key + "/followers").respond($q.when(institutions[0].followers[0]));
+            httpBackend.when('GET', INSTITUTIONS_URI + "/" + institutions[0].key + "/followers").respond(institutions[0].followers[0]);
             var result;
             service.getFollowers(institutions[0].key).then(function(data){
                 result = data;
             });
             httpBackend.flush();
             expect($http.get).toHaveBeenCalled();
-            expect(result.data.$$state.value).toEqual(raoni);
+            expect(result.data).toEqual(raoni);
         });
 
-        it('Spy getFollowers in fail case', function() {
+        it('Test getInstitution in success case', function() {
             spyOn($http, 'get').and.callThrough();
-            httpBackend.when('GET', INSTITUTIONS_URI + "/" + institutions[0].key + "/followers").respond(404, $q.when());
-            var result;
-            service.getFollowers(institutions[0].key).catch(function(data){
-                result = data;
-            });
-            httpBackend.flush();
-            console.log(result);
-            expect($http.get).toHaveBeenCalled();
-            expect(result.data.$$state.value).toEqual(undefined);
-        });
-
-        it('Spy getInstitution in success case', function() {
-            spyOn($http, 'get').and.callThrough();
-            httpBackend.when('GET', INSTITUTIONS_URI + "/" + institutions[0].key ).respond($q.when(institutions[0]));
+            httpBackend.when('GET', INSTITUTIONS_URI + "/" + institutions[0].key ).respond(institutions[0]);
             var result;
             service.getInstitution(institutions[0].key).then(function(data){
                 result = data;
             });
             httpBackend.flush();
             expect($http.get).toHaveBeenCalled();
-            expect(result.data.$$state.value).toEqual(institutions[0]);
-        });
-
-        it('Spy getInstitution in fail case', function() {
-            spyOn($http, 'get').and.callThrough();
-            httpBackend.when('GET', INSTITUTIONS_URI + "/" + institutions[0].key ).respond(404, $q.when());
-            var result;
-            service.getInstitution(institutions[0].key).catch(function(data){
-                result = data;
-            });
-            httpBackend.flush();
-            expect($http.get).toHaveBeenCalled();
-            expect(result.data.$$state.value).toEqual(undefined);
+            expect(result.data).toEqual(institutions[0]);
         });
 
 }));

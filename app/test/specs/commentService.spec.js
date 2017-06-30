fdaee136ba829000a9de70f803787e5941e3d271
ruanres <ruan.silveira@ccc.ufcg.edu.br>
2017-06-30 14:13:41 -0300

    CommentService createComment method passing on tests

--- app/test/specs/commentService.spec.js
+++ app/test/specs/commentService.spec.js
@@ -1,8 +1,8 @@
 'use strict';
 
 describe('Test CommentService', function() {
-    var http, httpBackend, deferred, scope, commentService, comments, data, error;
-    var commentsUri = '/api/post/post-key/comments';
+    var http, httpBackend, deferred, scope, commentService, comments, resp, error;
+    var postCommentsUri = '/api/posts/post-key/comments';
     
     beforeEach(module('app'));
 
@@ -17,44 +17,87 @@ describe('Test CommentService', function() {
             post_key: 'post_key',
             id: 'comment_id'
         }];
-        data = undefined;
-        error = undefined;
         httpBackend.when('GET', 'main/main.html').respond(200);        
         httpBackend.when('GET', 'home/home.html').respond(200);
         httpBackend.when('GET', 'error/error.html').respond(200);        
     }));
 
     afterEach(function() {
-        
+        resp = undefined;
+        error = undefined;
     });
 
-    it('Test getComments - Success Case', function() {
-        httpBackend.expect('GET', commentsUri).respond(comments);        
-        spyOn(http, 'get').and.returnValue(deferred.promise);
-        deferred.resolve(comments);
-        commentService.getComments(commentsUri).then(function success(response) {
-            data = response;
-        }, function err(response) {
-            error = response;
+    describe('Test getComments', function() { 
+        beforeEach(function() {
+            httpBackend.expectGET(postCommentsUri).respond(comments);        
+            spyOn(http, 'get').and.returnValue(deferred.promise);
         });
-        scope.$apply();
-        expect(http.get).toHaveBeenCalledWith(commentsUri);
-        expect(data).toEqual(comments);
-        expect(error).toBeUndefined();
+
+        it('Success case', function() {
+            deferred.resolve(comments);
+            commentService.getComments(postCommentsUri).then(function success(response) {
+                resp = response;
+            }, function err(response) {
+                error = response;
+            });
+            scope.$apply();
+            expect(http.get).toHaveBeenCalledWith(postCommentsUri);
+            expect(resp).toEqual(comments);
+            expect(error).toBeUndefined();
+        });
+
+        it('Fail case', function() {
+            deferred.reject({status: 400, data: {msg: 'Erro'}});
+            commentService.getComments(postCommentsUri).then(function success(response) {
+                resp = response;
+            }, function err(response) {
+                error = response;
+            });
+            scope.$apply();
+            expect(http.get).toHaveBeenCalledWith(postCommentsUri);
+            expect(resp).toBeUndefined();
+            expect(error.status).toEqual(400);
+        });
     });
 
-    it('Test getComments - Fail Case', function() {
-        httpBackend.expect('GET', commentsUri).respond(comments);        
-        spyOn(http, 'get').and.returnValue(deferred.promise);
-        deferred.reject({status: 400, data: {msg: 'Erro'}});
-        commentService.getComments(commentsUri).then(function success(response) {
-            data = response;
-        }, function err(response) {
-            error = response;
+    describe('Test creatComment', function() {
+        var text = 'new_text';
+        var institutionKey = 'institutionKey';
+        var postKey = 'post-key';
+        var data = {text: text, institution_key: institutionKey};
+        var newComment = {text: text, post_key: postKey, id: 'new_comment_id'};
+
+        beforeEach(function() {
+            httpBackend.expectPOST(postCommentsUri, data).respond(newComment);
+            spyOn(http, 'post').and.returnValue(deferred.promise);
         });
-        scope.$apply();
-        expect(http.get).toHaveBeenCalledWith(commentsUri);
-        expect(data).toBeUndefined();
-        expect(error.status).toEqual(400);
+
+        it('Sucess case', function() {
+            deferred.resolve(newComment);
+            commentService.createComment(postKey, text, institutionKey)
+            .then(function success(response) {
+                resp = response;
+            }, function err(response) {
+                error = response;
+            });
+            scope.$apply();
+            expect(http.post).toHaveBeenCalledWith(postCommentsUri, data);
+            expect(resp).toEqual(newComment);
+            expect(error).toBeUndefined();
+        });
+
+        it('Fail case', function() {
+            deferred.reject({status: 400, data: {msg: 'Erro'}});
+            commentService.createComment(postKey, text, institutionKey)
+            .then(function success(response) {
+                resp = response;
+            }, function err(response) {
+                error = response;
+            });
+            scope.$apply();
+            expect(http.post).toHaveBeenCalledWith(postCommentsUri, data);
+            expect(resp).toBeUndefined();
+            expect(error.status).toEqual(400);
+        });
     });
 });

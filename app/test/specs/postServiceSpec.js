78aaf143caf776987786316198e29459fd01864f
Raoni Smaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2017-07-03 08:50:35 -0300

    Changing some variables' names, getPosts method and removing a not necessary expect

--- app/test/specs/postServiceSpec.js
+++ app/test/specs/postServiceSpec.js
@@ -3,26 +3,36 @@
 (describe('Test PostService', function () {
         var httpBackend, service, $http;
         var POSTS_URI = "/api/posts";
+
         var user = {name: 'Raoni', key: 12345};
+
         var institutions = [{name: 'Splab',key: '098745', followers: [user], members: [user]},
         {name: 'e-CIS', key: '456879', followers: [user], members: [user]}];
+
         user.follows = [institutions[0], institutions[1]];
         user.institutions = [institutions[0]];
-        var post = {title: 'teste', text: 'post de teste', institution: institutions[0].key};
-        var post2 = {title: 'teste2', text: 'post de teste2', institution: institutions[0].key};
-        var post3 = {title: 'teste3', text: 'post de teste3', institution: institutions[1].key};
-        institutions[0].posts = [post, post2];
-        institutions[1].posts= [post3];
+
+        var mainPostTest = {title: 'teste', text: 'post de teste principal', institution: institutions[0].key};
+
+        var secondaryPostTest = {title: 'teste2', text: 'post de teste secundário', institution: institutions[0].key};
+
+        var helperPostTest = {title: 'teste3', text: 'post de teste auxiliar', institution: institutions[1].key};
+
+        institutions[0].posts = [mainPostTest, secondaryPostTest];
+
+        institutions[1].posts= [helperPostTest];
+
         var like = {author: user.key, id: '012320'};
-        post.likes = [like];
+
+        mainPostTest.likes = [like];
 
         function getPosts(){
             var posts = [];
-            for(var i = 0; i < user.follows.length; i++){
-                for(var j = 0; j < user.follows[i].posts.length; j++){
-                    posts.push(user.follows[i].posts[j]);
-                }
-            }
+            _.forEach(user.follows, function(institution) {
+                _.forEach(institution.posts, function(post) {
+                    posts.push(post);
+                });
+            });
             return posts;
         }
 
@@ -47,51 +57,50 @@
             });
             httpBackend.flush();
             expect($http.get).toHaveBeenCalled();
-            expect(result.data).toEqual([post, post2, post3]);
+            expect(result.data).toEqual([mainPostTest, secondaryPostTest, helperPostTest]);
         });
 
         it('Test createPost in success case', function() {
             spyOn($http, 'post').and.callThrough();
-            httpBackend.expect('POST', POSTS_URI).respond(post);
+            httpBackend.expect('POST', POSTS_URI).respond(mainPostTest);
             var result;
-            service.createPost(post).then(function(data){
+            service.createPost(mainPostTest).then(function(data){
                 result = data;
             });
             httpBackend.flush();
-            expect($http.post).toHaveBeenCalled();
-            expect($http.post).toHaveBeenCalledWith(POSTS_URI, post);
-            expect(result.data).toEqual(post);
+            expect($http.post).toHaveBeenCalledWith(POSTS_URI, mainPostTest);
+            expect(result.data).toEqual(mainPostTest);
         });
 
         it('Test likePost in success case', function() {
             spyOn($http, 'post').and.callThrough();
-            httpBackend.expect('POST', POSTS_URI + '/' + post.key + '/likes').respond();
-            service.likePost(post);
+            httpBackend.expect('POST', POSTS_URI + '/' + mainPostTest.key + '/likes').respond();
+            service.likePost(mainPostTest);
             httpBackend.flush();
             expect($http.post).toHaveBeenCalled();
         });
 
         it('Test dislikePost in success case', function() {
             spyOn($http, 'delete').and.callThrough();
-            httpBackend.expect('DELETE', POSTS_URI + '/' + post.key + '/likes').respond();
-            service.dislikePost(post);
+            httpBackend.expect('DELETE', POSTS_URI + '/' + mainPostTest.key + '/likes').respond();
+            service.dislikePost(mainPostTest);
             httpBackend.flush();
             expect($http.delete).toHaveBeenCalled();
         });
 
         it('Test deletePost in success case', function() {
             spyOn($http, 'delete').and.callThrough();
-            httpBackend.expect('DELETE', POSTS_URI + '/' + post.key).respond();
-            service.deletePost(post);
+            httpBackend.expect('DELETE', POSTS_URI + '/' + mainPostTest.key).respond();
+            service.deletePost(mainPostTest);
             httpBackend.flush();
             expect($http.delete).toHaveBeenCalled();
         });
 
         it('Test getLikes in success case', function() {
             spyOn($http, 'get').and.callThrough();
-            httpBackend.expect('GET', POSTS_URI + '/' + post.key + '/likes').respond(post.likes);
+            httpBackend.expect('GET', POSTS_URI + '/' + mainPostTest.key + '/likes').respond(mainPostTest.likes);
             var result;
-            service.getLikes(POSTS_URI + '/' + post.key + '/likes').then(function(data){
+            service.getLikes(POSTS_URI + '/' + mainPostTest.key + '/likes').then(function(data){
                 result = data;
             });
             httpBackend.flush();
@@ -101,27 +110,27 @@
 
         it('Test save in success case', function() {
             spyOn($http, 'patch').and.callThrough();
-            httpBackend.expect('PATCH', POSTS_URI + '/' + post.key).respond();
+            httpBackend.expect('PATCH', POSTS_URI + '/' + mainPostTest.key).respond();
             var newPost = {title: 'test', text: 'post de teste', institution: institutions[0].key};
-            var patch = jsonpatch.compare(post, newPost);
-            service.save(post, newPost);
+            var patch = jsonpatch.compare(mainPostTest, newPost);
+            service.save(mainPostTest, newPost);
             httpBackend.flush();
             expect($http.patch).toHaveBeenCalled();
-            expect($http.patch).toHaveBeenCalledWith(POSTS_URI + '/' + post.key, patch);
+            expect($http.patch).toHaveBeenCalledWith(POSTS_URI + '/' + mainPostTest.key, patch);
         });
 
         it('Test save in fail case', function() {
             spyOn($http, 'patch').and.callThrough();
-            httpBackend.expect('PATCH', POSTS_URI + '/' + post.key).respond(400, {status: 400, msg: "Operation invalid"});
+            httpBackend.expect('PATCH', POSTS_URI + '/' + mainPostTest.key).respond(400, {status: 400, msg: "Operation invalid"});
             var newPost = {title: 'test', institution: institutions[0].key};
             var result;
-            var patch = jsonpatch.compare(post, newPost);
-            service.save(post, newPost).catch(function(data) {
+            var patch = jsonpatch.compare(mainPostTest, newPost);
+            service.save(mainPostTest, newPost).catch(function(data) {
                 result = data;
             });
             httpBackend.flush();
             expect($http.patch).toHaveBeenCalled();
-            expect($http.patch).toHaveBeenCalledWith(POSTS_URI + '/' + post.key, patch);
+            expect($http.patch).toHaveBeenCalledWith(POSTS_URI + '/' + mainPostTest.key, patch);
             expect(result.status).toEqual(400);
             expect(result.data.msg).toEqual("Operation invalid");
         });

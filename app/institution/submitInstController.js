1a7dcc49c5a87fb33bb85c0c570f1a99e9794f81
ruanres <ruan.silveira@ccc.ufcg.edu.br>
2017-07-06 14:35:50 -0300

    institution form done

--- app/institution/submitInstController.js
+++ app/institution/submitInstController.js
@@ -6,33 +6,50 @@
     app.controller("SubmitInstController", function SubmitInstController(AuthService) {
         var submitInstCtrl = this;
 
-        submitInstCtrl.cnpjRegex = "[0-9]{2}[\.][0-9]{3}[\.][0-9]{3}[\/][0-9]{4}[-][0-9]{2}";
-
-        submitInstCtrl.invite = {
-            invitee: "reponsavel",
-             suggestion_institution_name: "Nome sugerido"
-        };
-
         Object.defineProperty(submitInstCtrl, 'user', {
             get: function() {
                 return AuthService.user;
             }
         });
 
+        submitInstCtrl.invite = {
+            invitee: "reponsavel@email.com",
+            suggestion_institution_name: "Nome sugerido"
+        };
         submitInstCtrl.institution = {
             name: submitInstCtrl.invite.suggestion_institution_name,
+            adminName: "nome do reponsavel",
             sigla: "",
             cnpj: "",
             legalNature: "",
             address: "",
             occupationArea: "",
+            otherArea: "",
             description: "",
             imageUrl: "",
-            email: "",
+            email: submitInstCtrl.invite.invitee,
             phoneNumber: "",
             admin: "",
-            parentInstitution: "",
             state: "active"
+        };
+        submitInstCtrl.natures = [
+            {value:"public", name:"Pública"}, 
+            {value:"private", name:"Privada"},
+            {value:"philanthropic", name:"Filantrópica"}
+        ];
+        submitInstCtrl.areas = [
+            {value:"official laboratories", name:"Laboratórios Oficiais"}, 
+            {value:"government agencies", name:"Ministérios e outros Órgãos do Governo"}, 
+            {value:"funding agencies", name:"Agências de Fomento"}, 
+            {value:"research institutes", name:"Institutos de Pesquisa"}, 
+            {value:"colleges", name:"Universidades"},
+            {value:"other", name:"Outra"}
+        ];
+        submitInstCtrl.cnpjRegex = "[0-9]{2}[\.][0-9]{3}[\.][0-9]{3}[\/][0-9]{4}[-][0-9]{2}";
+        submitInstCtrl.phoneRegex = "([0-9]{2}[\\s][0-9]{8})";
+
+        submitInstCtrl.submit = function submit() {
+            
         };
     });
 })();

2dc96ff82905b1b1ddb34bad412474d772c67e07
Mayza Nunes <mayzabeel@gmail.com>
2017-07-07 16:16:48 -0300

    move decorator

--- handlers/invite_collection_handler.py
+++ handlers/invite_collection_handler.py
@@ -12,21 +12,24 @@ from handlers.base_handler import BaseHandler
 from models.invite import Invite
 
 
-class InviteCollectionHandler(BaseHandler):
-    """Get user's invite."""
-
-    def is_admin(method):
+def is_admin(method):
         """Check if the user is admin of the institution."""
         def check_authorization(self, user, *args):
             data = json.loads(self.request.body)
             institution_key = ndb.Key(urlsafe=data['institution_key'])
             institution = institution_key.get()
 
-            Utils._assert(institution.key not in user.institutions_admin or institution.admin != user.key,
+            Utils._assert(institution.key not in user.institutions_admin,
+                          'User is not administrator', NotAuthorizedException)
+            Utils._assert(institution.admin != user.key,
                           'User is not administrator', NotAuthorizedException)
 
             method(self, user, *args)
         return check_authorization
+
+
+class InviteCollectionHandler(BaseHandler):
+    """Get user's invite."""
 
     @json_response
     @login_required

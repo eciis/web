04ae623ecbc4ac22a0d8de41cf8ac7f5f6908dfb
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2017-07-10 09:11:52 -0300

    Refactoring constants of CRUDService

--- app/imageUpload/CRUDService.js
+++ app/imageUpload/CRUDService.js
@@ -5,13 +5,18 @@
 
     app.service("CRUDService", function CRUDService( $q, $firebaseStorage) {
         var service = this;
+        var folderImages = 'images/';
 
         service.saveImage = function(file) {
+            var INDEX_FILENAME = 0;
+            var INDEX_TYPE_FILE = 1;
+            var fileProperties = file.name.split(".");
+            var filename = fileProperties[INDEX_FILENAME]  + "-" + (new Date()).getTime();
+            var image = firebase.storage().ref(folderImages + filename);
             var deferred = $q.defer();
-            var image = firebase.storage().ref("images/" + file.name.split(".")[0]);
 
             var metadata = {
-                contentType: 'image/jpeg'
+                contentType: 'image/' + fileProperties[INDEX_TYPE_FILE]
             };
 
             var promise = $firebaseStorage(image).$put(file, metadata);

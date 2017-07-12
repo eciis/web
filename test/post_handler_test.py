8b9eb99be8140f5e282c882fe2b255bd265d6df0
ruanres <ruan.silveira@ccc.ufcg.edu.br>
2017-07-12 08:49:14 -0300

    tests corrected

--- test/post_handler_test.py
+++ test/post_handler_test.py
@@ -83,6 +83,7 @@ def initModels(cls):
     # new Institution CERTBIO
     cls.certbio = Institution()
     cls.certbio.name = 'CERTBIO'
+    cls.certbio.acronym = 'CERTBIO'
     cls.certbio.cnpj = '18.104.068/0001-86'
     cls.certbio.legal_nature = 'public'
     cls.certbio.address = 'Universidade Federal de Campina Grande'

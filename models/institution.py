9833cab7d0a2a5bcff4997b1ce4b1cdd827e2c2f
Mayza Nunes <mayzabeel@gmail.com>
2017-07-26 09:30:30 -0300

    set status of user when create a new institution

--- models/institution.py
+++ models/institution.py
@@ -153,6 +153,7 @@ class Institution(ndb.Model):
 
         user.institutions.append(institution.key)
         user.institutions_admin.append(institution.key)
+        user.state = "active"
         user.follows.append(institution.key)
         user.put()
 

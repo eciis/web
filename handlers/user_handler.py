757ce1310f2a61dfae1f6d2dcb0d3f8a61afc679
ruanres <ruan.silveira@ccc.ufcg.edu.br>
2017-07-27 08:32:28 -0300

    renamed variable institution to institution_key on user_handler

--- handlers/user_handler.py
+++ handlers/user_handler.py
@@ -39,8 +39,8 @@ def makeUser(user, request):
         user_json['institutions'].append(
             Utils.toJson(institution.get())
         )
-    user_json['follows'] = [institution.get().make(
-        ['acronym', 'photo_url', 'key']) for institution in user.follows]
+    user_json['follows'] = [institution_key.get().make(
+        ['acronym', 'photo_url', 'key']) for institution_key in user.follows]
     return user_json
 
 

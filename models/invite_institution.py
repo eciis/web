08f55a6c1234ee7e26a5f024be9105ec89f7cbaa
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2017-07-31 17:23:17 -0300

    Merge

---
+++ models/invite_institution.py
@@ -0,0 +1,8 @@
+"""Invite institution model."""
+from invite import Invite
+
+
+class InviteInstitution(Invite):
+    """Model of invite institution."""
+
+    pass
---
+++ models/invite_user.py
@@ -0,0 +1,8 @@
+"""Invite user model."""
+from invite import Invite
+
+
+class InviteUser(Invite):
+    """Model of invite user."""
+
+    pass

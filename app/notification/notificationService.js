29401fee0faab67afc2634985181f82ca9d51b7a
tiagolimpe <tiago.pereira@ccc.ufcg.edu.br>
2017-08-15 11:14:44 -0300

    fixing the notifications order

--- app/notification/notificationService.js
+++ app/notification/notificationService.js
@@ -28,7 +28,7 @@
             setupNotifications(userKey, function() {
                 _.forEach(firebaseArrayNotifications, function each(notification) {
                     if (isNew(notification)) {
-                        notificationsList.push(notification);
+                        notificationsList.splice(0, 0, notification);
                     }
                 });
 

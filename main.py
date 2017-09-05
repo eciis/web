e6fc2e71c72d2b988a02168fbc8258c1e3033fd4
Mayza Nunes <mayzabeel@gmail.com>
2017-09-05 17:58:00 -0300

    fix import

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ from handlers.post_comment_handler import PostCommentHandler
 from handlers.invite_collection_handler import InviteCollectionHandler
 from handlers.search_handler import SearchHandler
 from handlers.invite_handler import InviteHandler
-from handlers.event import EventHandler
+from handlers.event_handler import EventHandler
 from handlers.event_collection_handler import EventCollectionHandler
 from handlers.redirect_handler import AppRedirectHandler
 from handlers.institution_hierarchy_handler import InstitutionHierarchyHandler

28227512dffb817ed0ec6667815dd60730f38b9c
André L. Abrantes <andredossantosabrantes@gmail.com>
2017-09-21 15:32:01 -0300

    Fix intercept exceptions routes after login

--- app/auth/loginController.js
+++ app/auth/loginController.js
@@ -24,16 +24,8 @@
 
         loginCtrl.loginWithEmailPassword = function loginWithEmailPassword() {
             AuthService.loginWithEmailAndPassword(loginCtrl.user.email, loginCtrl.user.password).then(
-                function success(user) {
-                    var pendingInvite = user.getPendingInvitation();
-                    if (pendingInvite) {
-                        var inviteKey = pendingInvite.key;
-                        $state.go("new_invite", {key: inviteKey});
-                    } else if (user.isInactive()) {
-                        $state.go("user_inactive");
-                    } else {
-                        $state.go("app.home");
-                    }
+                function success() {
+                    $state.go("app.home");
                 }
             );
         };

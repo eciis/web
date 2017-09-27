caa189d4c8e4394cdfeb1559a688bdcd71b64345
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2017-09-27 08:22:06 -0300

    Rename editInstDirective for configInstDirective

--- app/institution/configInstDirective.js
+++ app/institution/configInstDirective.js
@@ -1,56 +1,56 @@
 'use strict';
 (function() {
     var app = angular.module("app");
-    app.controller("EditInstController", function EditInstController(AuthService, InstitutionService, CropImageService,$state,
+    app.controller("ConfigInstController", function ConfigInstController(AuthService, InstitutionService, CropImageService,$state,
             $mdToast, $mdDialog, $http, InviteService, ImageService, $rootScope, MessageService, PdfService, $q, RequestInvitationService) {
 
-        var editInstCtrl = this;
+        var configInstCtrl = this;
         var institutionKey = $state.params.institutionKey;
         var currentPortfoliourl = null;
         var observer;
 
-        editInstCtrl.loading = false;
-        editInstCtrl.user = AuthService.getCurrentUser();
-        editInstCtrl.cnpjRegex = "[0-9]{2}[\.][0-9]{3}[\.][0-9]{3}[\/][0-9]{4}[-][0-9]{2}";
-        editInstCtrl.phoneRegex = "[0-9]{2}[\\s][0-9]{4,5}[-][0-9]{4,5}";
-        editInstCtrl.cepRegex = "([0-9]{5}[-][0-9]{3})";
-        editInstCtrl.newInstitution = {
+        configInstCtrl.loading = false;
+        configInstCtrl.user = AuthService.getCurrentUser();
+        configInstCtrl.cnpjRegex = "[0-9]{2}[\.][0-9]{3}[\.][0-9]{3}[\/][0-9]{4}[-][0-9]{2}";
+        configInstCtrl.phoneRegex = "[0-9]{2}[\\s][0-9]{4,5}[-][0-9]{4,5}";
+        configInstCtrl.cepRegex = "([0-9]{5}[-][0-9]{3})";
+        configInstCtrl.newInstitution = {
             photo_url: "/images/institution.jpg",
-            email: editInstCtrl.user.email[0]
+            email: configInstCtrl.user.email[0]
         };
 
         getLegalNatures();
         getOccupationAreas();
 
-        editInstCtrl.addImage = function addImage(image) {
+        configInstCtrl.addImage = function addImage(image) {
             var newSize = 800;
             var promise = ImageService.compress(image, newSize);
             promise.then(function success(data) {
-                editInstCtrl.photo_instituicao = data;
+                configInstCtrl.photo_instituicao = data;
                 ImageService.readFile(data, setImage);
-                editInstCtrl.file = null;
+                configInstCtrl.file = null;
             }, function error(error) {
                 MessageService.showToast(error);
             });
             return promise;
         };
 
-        editInstCtrl.cropImage = function cropImage(image_file) {
+        configInstCtrl.cropImage = function cropImage(image_file) {
             CropImageService.crop(image_file).then(function success(croppedImage) {
-                editInstCtrl.addImage(croppedImage);
+                configInstCtrl.addImage(croppedImage);
             }, function error() {
-                editInstCtrl.file = null;
+                configInstCtrl.file = null;
             });
         };
 
         function setImage(image) {
             $rootScope.$apply(function() {
-                editInstCtrl.newInstitution.photo_url = image.src;
+                configInstCtrl.newInstitution.photo_url = image.src;
             });
         }
 
-        editInstCtrl.submit = function submit(event) {
-            var newInstitution = new Institution(editInstCtrl.newInstitution);
+        configInstCtrl.submit = function submit(event) {
+            var newInstitution = new Institution(configInstCtrl.newInstitution);
             var promise;
             if (newInstitution.isValid()){
                 var confirm = $mdDialog.confirm(event)
@@ -76,12 +76,12 @@
 
         function saveImage() {
             var defer = $q.defer();
-            if(editInstCtrl.photo_instituicao) {
-                editInstCtrl.loading = true;
-                ImageService.saveImage(editInstCtrl.photo_instituicao).then(
+            if(configInstCtrl.photo_instituicao) {
+                configInstCtrl.loading = true;
+                ImageService.saveImage(configInstCtrl.photo_instituicao).then(
                     function(data) {
-                        editInstCtrl.loading = false;
-                        editInstCtrl.newInstitution.photo_url = data.url;
+                        configInstCtrl.loading = false;
+                        configInstCtrl.newInstitution.photo_url = data.url;
                         defer.resolve();
                     }, function error(response) {
                         MessageService.showToast(response.data.msg);
@@ -95,10 +95,10 @@
 
         function savePortfolio() {
             var defer = $q.defer();
-            if(editInstCtrl.file) {
-                PdfService.save(editInstCtrl.file, currentPortfoliourl).then(
+            if(configInstCtrl.file) {
+                PdfService.save(configInstCtrl.file, currentPortfoliourl).then(
                     function success(data) {
-                        editInstCtrl.newInstitution.portfolio_url = data.url;
+                        configInstCtrl.newInstitution.portfolio_url = data.url;
                         currentPortfoliourl = data.url;
                         defer.resolve();
                     }, function error(response) {
@@ -115,7 +115,7 @@
             var savePromises = [savePortfolio(), saveImage()];
             var promise = $q.defer();
             $q.all(savePromises).then(function success() {
-                if(editInstCtrl.isSubmission) {
+                if(configInstCtrl.isSubmission) {
                     saveRequestInst();
                 } else {
                     patchIntitution();
@@ -132,7 +132,7 @@
             var patch = jsonpatch.generate(observer);
             InstitutionService.update(institutionKey, patch).then(
                 function success() {
-                    updateUserInstitutions(editInstCtrl.newInstitution);
+                    updateUserInstitutions(configInstCtrl.newInstitution);
                 },
                 function error(response) {
                     MessageService.showToast(response.data.msg);
@@ -140,7 +140,7 @@
         }
 
         function saveRequestInst() {
-            RequestInvitationService.sendRequestInst(editInstCtrl.newInstitution).then(
+            RequestInvitationService.sendRequestInst(configInstCtrl.newInstitution).then(
                 function success() {
                     MessageService.showToast("Pedido enviado com sucesso!");
                     $state.go('user_inactive');
@@ -150,41 +150,41 @@
         }
 
         function updateUserInstitutions(institution) {
-            editInstCtrl.user.updateInstitutions(institution);
+            configInstCtrl.user.updateInstitutions(institution);
             AuthService.save();
             changeInstitution(institution);
             MessageService.showToast('Edição de instituição realizado com sucesso');
             $state.go('app.institution', {institutionKey: institutionKey});
         }
 
-        editInstCtrl.showButton = function() {
-            return !editInstCtrl.loading;
+        configInstCtrl.showButton = function() {
+            return !configInstCtrl.loading;
         };
 
         function changeInstitution(institution) {
-            if(editInstCtrl.newInstitution &&
-                editInstCtrl.user.current_institution.key === editInstCtrl.newInstitution.key) {
-                editInstCtrl.user.changeInstitution(institution);
+            if(configInstCtrl.newInstitution &&
+                configInstCtrl.user.current_institution.key === configInstCtrl.newInstitution.key) {
+                configInstCtrl.user.changeInstitution(institution);
             }
         }
 
         function getLegalNatures() {
             $http.get('institution/legal_nature.json').then(function success(response) {
-                editInstCtrl.legalNatures = response.data;
+                configInstCtrl.legalNatures = response.data;
             });
         }
 
         function getOccupationAreas() {
             $http.get('institution/occupation_area.json').then(function success(response) {
-                editInstCtrl.occupationAreas = response.data;
+                configInstCtrl.occupationAreas = response.data;
             });
         }
 
         function loadInstitution() {
             InstitutionService.getInstitution(institutionKey).then(function success(response) {
-                editInstCtrl.newInstitution = response.data;
-                currentPortfoliourl = editInstCtrl.newInstitution.portfolio_url;
-                observer = jsonpatch.observe(editInstCtrl.newInstitution);
+                configInstCtrl.newInstitution = response.data;
+                currentPortfoliourl = configInstCtrl.newInstitution.portfolio_url;
+                observer = jsonpatch.observe(configInstCtrl.newInstitution);
             }, function error(response) {
                 MessageService.showToast(response.data.msg);
             });
@@ -197,12 +197,12 @@
         })();
     });
 
-    app.directive("editInstitution", function() {
+    app.directive("configInstitution", function() {
         return {
             restrict: 'E',
             templateUrl: "institution/submit_form.html",
-            controller: "EditInstController",
-            controllerAs: "editInstCtrl",
+            controller: "ConfigInstController",
+            controllerAs: "configInstCtrl",
             scope: {},
             bindToController: {
                 isSubmission: '='

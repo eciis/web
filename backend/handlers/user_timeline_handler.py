b0408a25a7d9b384b9b08d96e23448345a89769f
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2017-10-17 16:50:47 -0300

    Add cursor for get posts in datastore

--- backend/handlers/user_timeline_handler.py
+++ backend/handlers/user_timeline_handler.py
@@ -2,6 +2,7 @@
 """User Timeline Handler."""
 
 import json
+from google.appengine.ext import ndb
 
 from utils import login_required
 from utils import json_response
@@ -13,6 +14,9 @@ from models.post import Post
 class UserTimelineHandler(BaseHandler):
     """Get posts of all institutions that the user follow."""
 
+    cursor = None
+    number_fetchs = 12
+
     @json_response
     @login_required
     def get(self, user):
@@ -22,12 +26,24 @@ class UserTimelineHandler(BaseHandler):
         """
         array = []
         visible_posts = []
+
+        cursor = None
+        if UserTimelineHandler.cursor:
+            cursor = ndb.Cursor.from_websafe_string(UserTimelineHandler.cursor)
+
         if len(user.follows) > 0:
             queryPosts = Post.query(Post.institution.IN(
-                user.follows)).order(Post.last_modified_date)
+                user.follows)).order(-Post.last_modified_date, Post.key)
+
+            queryPosts, next_cursor, more = queryPosts.fetch_page(
+                UserTimelineHandler.number_fetchs,
+                start_cursor=cursor)
 
             array = [Post.make(post, self.request.host) for post in queryPosts]
             visible_posts = [post for post in array
                              if not Post.is_hidden(post)]
+
+        if more:
+            UserTimelineHandler.cursor = next_cursor.to_websafe_string()
 
         self.response.write(json.dumps(visible_posts))

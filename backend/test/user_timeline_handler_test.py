9bdc709140755d6306ebb1f9d5d2c13017dad50f
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2017-10-18 12:11:45 -0300

    fix tests in backend

--- backend/test/user_timeline_handler_test.py
+++ backend/test/user_timeline_handler_test.py
@@ -38,10 +38,10 @@ class UserTimelineHandlerTest(TestBaseHandler):
         posts = self.testapp.get("/api/user/timeline")
 
         # Update the objects
-        post_top = (posts.json)[1]
+        post_top = (posts.json['posts'])[0]
         key_post_top = ndb.Key(urlsafe=post_top['key'])
         post_top_obj = key_post_top.get()
-        post_last = (posts.json)[0]
+        post_last = (posts.json['posts'])[1]
         key_post_last = ndb.Key(urlsafe=post_last['key'])
         post_last_obj = key_post_last.get()
 
@@ -67,8 +67,8 @@ class UserTimelineHandlerTest(TestBaseHandler):
         posts = self.testapp.get("/api/user/timeline")
 
         # Update the objects
-        post_top = (posts.json)[1]
-        post_last = (posts.json)[0]
+        post_top = (posts.json['posts'])[0]
+        post_last = (posts.json['posts'])[1]
 
         # Verify if the post was deleted and your informations
         self.assertEqual(post_top["title"], None,

18c0e8b34e8894b8628b779c620f3eeb58ef9c2a
MaianaB <maiana.brito@ccc.ufcg.edu.br>
2017-11-07 10:06:35 -0300

    Create SurveyService

--- frontend/survey/surveyDetailsDirective.js
+++ frontend/survey/surveyDetailsDirective.js
@@ -3,7 +3,7 @@
 
     var app = angular.module('app');
 
-    app.controller('SurveyDetailsController', function($scope) {
+    app.controller('SurveyDetailsController', function($scope, SurveyService) {
 
         var surveyCtrl = this;
         surveyCtrl.post = $scope.post;
@@ -12,19 +12,29 @@
             return surveyCtrl.post.state === 'deletd';
         };
 
-        surveyCtrl.vote = function(option, bo){
-            if(bo){
-                option.number_votes += 1;
-            } else {
-                option.number_votes -= 1;
-            }
-            surveyCtrl.calculatePercentage(option);
+        function getOptionsSelected(){
+            return _.takeWhile(surveyCtrl.post.options, function(option) { return option.selected; });
+        }
+
+        function addVote(options){
+            _.forEach(options, function(option) {
+              option.number_votes += 1;
+            });
+        }
+
+        surveyCtrl.vote = function(){
+            var optionsSelected = getOptionsSelected();
+            SurveyService.vote(surveyCtrl.post, optionsSelected).then(function sucess(data){
+                addVote(optionsSelected);
+            });
+            surveyCtrl.calculatePercentage();
         };
 
-        surveyCtrl.calculatePercentage = function(option){
-            var percentage = option.number_votes / surveyCtrl.post.number_votes;
-            option.percentage = surveyCtrl.post.number_votes === 0 ? 0 : percentage;
-            console.log(option.percentage);
+        surveyCtrl.calculatePercentage = function(){
+            _.forEach(surveyCtrl.post.options, function(option) {
+                var percentage = option.number_votes / surveyCtrl.post.number_votes;
+                option.percentage = surveyCtrl.post.number_votes === 0 ? 0 : percentage;
+            });
         };
     });
     

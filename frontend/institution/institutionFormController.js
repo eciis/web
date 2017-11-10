191dfd214717bca8dd8e92195d6ebc367e3240fa
Raoni Smaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2017-11-10 13:17:28 -0300

    Starting the second step

--- frontend/institution/institutionFormController.js
+++ frontend/institution/institutionFormController.js
@@ -3,8 +3,29 @@
 (function() {
    var app = angular.module('app');
 
-   app.controller('InstitutionFormController', function InstitutionFormController($stateParams) {
+   app.controller('InstitutionFormController', function InstitutionFormController($stateParams, $http) {
         var instFormCtrl = this;
         instFormCtrl.institutionKey = $stateParams.institutionKey;
+
+        instFormCtrl.firstStep = false;
+        instFormCtrl.secondStep = true;
+        instFormCtrl.thirdStep = false;
+
+        getLegalNatures();
+        getActuationAreas();
+
+        function getLegalNatures() {
+            $http.get('app/institution/legal_nature.json').then(function success(response) {
+                instFormCtrl.legalNatures = response.data;
+            });
+        }
+
+        function getActuationAreas() {
+            $http.get('app/institution/actuation_area.json').then(function success(response) {
+                instFormCtrl.actuationArea = response.data;
+            });
+        }
+
+
    });
 })();

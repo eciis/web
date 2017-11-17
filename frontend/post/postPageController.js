e015bb3f9cc746308a8e33bf7bc6af3168e7cf29
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2017-11-17 11:15:50 -0300

    Fix verification of hiden post

--- frontend/post/postPageController.js
+++ frontend/post/postPageController.js
@@ -8,8 +8,13 @@
 
         postCtrl.post = null;
 
-        postCtrl.isDeletedPost = function isDeletedPost() {
-            return postCtrl.post.state == 'deleted' && postCtrl.post.number_of_comments === 0;
+        postCtrl.isHiden = function isHiden() {
+            var isDeleted = postCtrl.post.state == 'deleted';
+            var hasNoComments = postCtrl.post.number_of_comments === 0;
+            var hasNoLikes = postCtrl.post.number_of_likes === 0;
+            var hasNoActivity = hasNoComments && hasNoLikes;
+
+            return postCtrl.post.state == 'deleted' && hasNoActivity;
         };
 
         function loadPost(postKey) {

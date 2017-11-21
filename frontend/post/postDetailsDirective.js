3069e089d34fd2ba33869d1571d6631f66fd56a5
MaianaB <maiana.brito@ccc.ufcg.edu.br>
2017-11-21 08:43:55 -0300

    Fix date and user that delete post

--- frontend/post/postDetailsDirective.js
+++ frontend/post/postDetailsDirective.js
@@ -32,6 +32,9 @@
                 postDetailsCtrl.post = new Post(postDetailsCtrl.post);
                 PostService.deletePost(postDetailsCtrl.post).then(function success() {
                     postDetailsCtrl.post.remove(postDetailsCtrl.user.name);
+                    postDetailsCtrl.post.last_modified_date = new Date().toISOString();
+                    postDetailsCtrl.post.last_modified_by = postDetailsCtrl.user.name;
+                    console.log(postDetailsCtrl.post);
                     MessageService.showToast('Post excluído com sucesso');
                 }, function error(response) {
                     MessageService.showToast(response.data.msg);

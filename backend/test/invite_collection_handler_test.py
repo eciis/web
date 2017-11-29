27191ae55862b16a36b72de06a062c8b07e46711
Raoni Smaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2017-11-29 08:57:39 -0300

    Removing the TODO

--- backend/test/invite_collection_handler_test.py
+++ backend/test/invite_collection_handler_test.py
@@ -14,9 +14,6 @@ from mock import patch
 
 class InviteCollectionHandlerTest(TestBaseHandler):
     """Invite Collection handler test."""
-    # TODO:
-    # Fix the post method, to check specific exceptions and error messages
-    # @author Mayza Nunes 07-07-2017
 
     @classmethod
     def setUp(cls):

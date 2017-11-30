7f05525d8d5ed13fe002c3c0e0c4a438fc0fe086
tiagolimpe <tiago.pereira@ccc.ufcg.edu.br>
2017-11-30 08:23:01 -0300

    adding shared post in posts of surveyDetailsDirective test

--- frontend/test/specs/surveyDetailsDirectiveSpec.js
+++ frontend/test/specs/surveyDetailsDirectiveSpec.js
@@ -53,7 +53,7 @@
 
         surveyCtrl.user = user;
         surveyCtrl.post = survey;
-        surveyCtrl.posts = [survey,];
+        surveyCtrl.posts = [survey,shared_post];
 
         httpBackend.when('GET', 'main/main.html').respond(200);
         httpBackend.when('GET', 'home/home.html').respond(200);

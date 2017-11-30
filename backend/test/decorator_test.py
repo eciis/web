0279d44236cf0f6183be5ebc11f58b539fd68483
Raoni Smaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2017-11-30 11:27:06 -0300

    Removing init_models and creating create_post in mocks.py

--- backend/test/decorator_test.py
+++ backend/test/decorator_test.py
@@ -3,7 +3,6 @@
 
 
 from test_base import TestBase
-from models.post import Post
 from custom_exceptions.notAuthorizedException import NotAuthorizedException
 from utils import is_authorized
 from handlers.post_handler import is_post_author
@@ -24,12 +23,21 @@ class TestIsAuthorized(TestBase):
         cls.ndb.get_context().set_cache_policy(False)
         cls.test.init_search_stub()
 
-        initModels(cls)
-
     def test_not_allowed(self):
         """Test if the user is really not allowed."""
         """Make sure that an exception is raised because the user
         is not authorized."""
+        self.first_user = mocks.create_user()
+        self.second_user = mocks.create_user()
+        self.third_user = mocks.create_user()
+        self.first_inst = mocks.create_institution()
+        self.second_inst = mocks.create_institution()
+        self.first_user_post = mocks.create_post(
+            self.first_user.key, self.first_inst.key)
+        self.second_user_post = mocks.create_post(
+            self.second_user.key, self.second_inst.key)
+        self.third_user_post = mocks.create_post(
+            self.third_user.key, self.first_inst.key)
         with self.assertRaises(NotAuthorizedException) as Aex:
             is_decorated(self, self.second_user,
                          self.first_user_post.key.urlsafe())
@@ -60,6 +68,21 @@ class TestIsAuthorized(TestBase):
         """Test if everything goes ok."""
         """ Make sure if the return is None, once when everything goes ok
         the method returns nothing."""
+        self.first_user = mocks.create_user()
+        self.second_user = mocks.create_user()
+        self.third_user = mocks.create_user()
+        self.first_inst = mocks.create_institution()
+        self.first_inst.admin = self.first_user.key
+        self.first_inst.put()
+        self.second_inst = mocks.create_institution()
+        self.second_inst.admin = self.first_user.key
+        self.second_inst.put()
+        self.first_user_post = mocks.create_post(
+            self.first_user.key, self.first_inst.key)
+        self.second_user_post = mocks.create_post(
+            self.second_user.key, self.second_inst.key)
+        self.third_user_post = mocks.create_post(
+            self.third_user.key, self.first_inst.key)
         self.assertIsNone(is_decorated(self, self.first_user,
                                        self.first_user_post.key.urlsafe()),
                           "Something went wrong during the execution")
@@ -78,6 +101,14 @@ class TestIsAuthorized(TestBase):
         """Test is_post_author decorator."""
         """Make sure that an exception is raised because the user
         is not the post's author."""
+        self.second_user = mocks.create_user()
+        self.third_user = mocks.create_user()
+        self.first_inst = mocks.create_institution()
+        self.second_inst = mocks.create_institution()
+        self.second_user_post = mocks.create_post(
+            self.second_user.key, self.second_inst.key)
+        self.third_user_post = mocks.create_post(
+            self.third_user.key, self.first_inst.key)
         with self.assertRaises(NotAuthorizedException) as Naex:
             is_decorated_by_post_author(
                 self, self.second_user, self.third_user_post.key.urlsafe())
@@ -93,6 +124,10 @@ class TestIsAuthorized(TestBase):
         """Test is_post_author decorator."""
         """ Make sure if the return is None, once when everything goes ok
         the method returns nothing."""
+        self.first_user = mocks.create_user()
+        self.first_inst = mocks.create_institution()
+        self.first_user_post = mocks.create_post(
+            self.first_user.key, self.first_inst.key)
         self.assertIsNone(is_decorated_by_post_author(self, self.first_user,
                                                       self.first_user_post.key.urlsafe()),
                           "Something went wrong during the execution")
@@ -112,81 +147,3 @@ def is_decorated(self, user, key):
 def is_decorated_by_post_author(self, user, url_string):
     """Allow the system test the decorator."""
     pass
-
-
-def initModels(cls):
-    """Init the models."""
-    # new First User
-    cls.first_user = mocks.create_user()
-    # new Second User
-    cls.second_user = mocks.create_user()
-    # new Third User
-    cls.third_user = mocks.create_user()
-    # new First Institution
-    cls.first_inst = mocks.create_institution()
-    cls.first_inst.cnpj = '18.104.068/0001-86'
-    cls.first_inst.legal_nature = 'public'
-    cls.first_inst.actuation_area = ''
-    cls.first_inst.description = 'Ensaio Químico - Determinação de Material Volátil por \
-            Gravimetria e Ensaio Biológico - Ensaio de Citotoxicidade'
-    cls.first_inst.phone_number = '(83) 3322 4455'
-    cls.first_inst.members = [cls.first_user.key, cls.second_user.key]
-    cls.first_inst.followers = [cls.first_user.key, cls.second_user.key]
-    cls.first_inst.admin = cls.first_user.key
-    cls.first_inst.put()
-    # POST of First User To First Institution
-    cls.first_user_post = Post()
-    cls.first_user_post.title = "Novo edital do CERTBIO"
-    cls.first_user_post.text = "At vero eos et accusamus et iusto odio dignissimos \
-        ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti \
-        quos dolores et quas molestias excepturi sint occaecati cupiditate \
-        aut perferendis doloribus asperiores repellat."
-    cls.first_user_post.author = cls.first_user.key
-    cls.first_user_post.institution = cls.first_inst.key
-    cls.first_user_post.put()
-    # new Second Institution
-    cls.second_inst = mocks.create_institution()
-    cls.second_inst.cnpj = '18.104.068/0001-56'
-    cls.second_inst.legal_nature = 'public'
-    cls.second_inst.actuation_area = ''
-    cls.second_inst.description = 'The mission of the Software Practices Laboratory (SPLab) \
-            is to promote the development of the state-of-the-art in the \
-            theory and practice of Software Engineering.'
-    cls.second_inst.photo_url = 'http://amaurymedeiros.com/images/second_inst.png'
-    cls.second_inst.phone_number = '(83) 3322 7865'
-    cls.second_inst.members = [cls.first_user.key, cls.third_user.key]
-    cls.second_inst.followers = [cls.first_user.key, cls.third_user.key]
-    cls.second_inst.admin = cls.first_user.key
-    cls.second_inst.put()
-    # Second User's post
-    cls.second_user_post = Post()
-    cls.second_user_post.title = "Novwdfssdo edital do CERTBIO"
-    cls.second_user_post.text = "At vero eos et accusamus et iusto odio dignissimos \
-        ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti \
-        delectus, ut aut reiciendis voluptatibus maiores alias consequatur \
-        aut perferendis doloribus asperiores repellat."
-    cls.second_user_post.author = cls.second_user.key
-    cls.second_user_post.institution = cls.second_inst.key
-    cls.second_user_post.put()
-    # Another second user's post
-    cls.second_user_post2 = Post()
-    cls.second_user_post2.title = "Novwdfsadsssdo edital do CERTBIO"
-    cls.second_user_post2.text = "At vero eos et accusamus et iusto odio dignissimos \
-        ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti \
-        delectus, ut aut reiciendis voluptatibus maiores alias consequatur \
-        aut perferendis doloribus asperiores repellat."
-    cls.second_user_post2.author = cls.second_user.key
-    cls.second_user_post2.institution = cls.first_inst.key
-    cls.second_user_post2.put()
-    # third user's post
-    cls.third_user_post = Post()
-    cls.third_user_post.title = "Novwdfssdo edital do CERTBIO"
-    cls.third_user_post.text = "At vero eos et accusamus et iusto odio dignissimos \
-        emporibus autem quibusdam et aut officiis debitis aut rerum \
-        necessitatibus saepe eveniet ut et voluptates repudiandae sint \
-        et molestiae non recusandae. Itaque earum rerum hic tenetur sapiente \
-        delectus, ut aut reiciendis voluptatibus maiores alias consequatur \
-        aut perferendis doloribus asperiores repellat."
-    cls.third_user_post.author = cls.third_user.key
-    cls.third_user_post.institution = cls.first_inst.key
-    cls.third_user_post.put()
--- backend/test/mocks.py
+++ backend/test/mocks.py
@@ -2,6 +2,7 @@
 
 from models.user import User
 from models.institution import Institution
+from models.post import Post
 import datetime
 import sys
 
@@ -22,11 +23,6 @@ def create_user():
     """Create user function."""
     user = User()
     user_hash = getHash(user)
-    user.institutions = []
-    user.follows = []
-    user.institutions_admin = []
-    user.notifications = []
-    user.posts = []
     user.name = "User %s" % user_hash
     user.email = ["user%s@email.com" % user_hash]
     user.put()
@@ -39,8 +35,23 @@ def create_institution():
     inst_hash = getHash(institution)
     institution.name = "Inst %s" % inst_hash
     institution.email = "inst%s@email.com" % inst_hash
-    institution.members = []
-    institution.followers = []
-    institution.posts = []
-    institution.acronym = inst_hash
+    institution.acronym = "acronym %s" % inst_hash
+    institution.cnpj = "cnpj %s" % inst_hash
+    institution.phone_number = "phone %s" % inst_hash
+    institution.photo_url = "photo %s" % inst_hash
+    institution.actuation_area = "area %s" % inst_hash
+    institution.description = "description %s" % inst_hash
+    institution.put()
     return institution
+
+
+def create_post(author_key, institution_key):
+    """Create post."""
+    post = Post()
+    post.author = author_key
+    post.institution = institution_key
+    post_hash = getHash(post)
+    post.title = "title %s" % post_hash
+    post.text = "text %s" % post_hash
+    post.put()
+    return post

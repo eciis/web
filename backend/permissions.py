7363ec439bacf99c20447b34b87735b20e00c034
tiagolimpe <tiago.pereira@ccc.ufcg.edu.br>
2017-12-01 10:56:21 -0300

    changing is_admin_of_requested_inst verification to verify if the user has permission to accept or reject links between instituitions

--- backend/permissions.py
+++ backend/permissions.py
@@ -6,7 +6,8 @@ DEFAULT_ADMIN_PERMISSIONS  = [
     "remove_inst", 
     "update_inst",
     "remove_posts", 
-    "remove_insts"
+    "remove_insts",
+    "link_insts"
     ]
 
 DEFAULT_SUPER_USER_PERMISSIONS = [

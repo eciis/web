7b4ce21d658ac3f7f276bd219620428ba42aaedb
Raoni Smaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2017-12-01 14:31:39 -0300

    Fixing timeline's tests

--- backend/test/institution_timeline_handler_test.py
+++ backend/test/institution_timeline_handler_test.py
@@ -8,6 +8,7 @@ from handlers.post_handler import PostHandler
 from handlers.post_collection_handler import PostCollectionHandler
 from handlers.institution_timeline_handler import InstitutionTimelineHandler
 from google.appengine.ext import ndb
+import mocks
 
 from mock import patch
 
@@ -25,21 +26,33 @@ class InstitutionTimelineHandlerTest(TestBaseHandler):
              ("/api/posts", PostCollectionHandler)
              ], debug=True)
         cls.testapp = cls.webtest.TestApp(app)
-        initModels(cls)
 
-    @patch('utils.verify_token', return_value={'email': 'user1@email.com'})
+    @patch('utils.verify_token', return_value={'email': 'user@email.com'})
     def test_get(self, verify_token):
         """Test the institution_timeline_handler get method."""
-        # Pretend an authentication
-        self.os.environ['REMOTE_USER'] = 'user1@email.com'
-        self.os.environ['USER_EMAIL'] = 'user1@email.com'
-        # Added the posts in datastore
-        self.testapp.post_json("/api/posts", self.post_user1)
-        self.testapp.post_json("/api/posts", self.post_aux)
+        user = mocks.create_user()
+        institution = mocks.create_institution()
+        user.email = ['user@email.com']
+        user.add_institution(institution.key)
+        institution.members = [user.key]
+        user.put()
+        institution.put()
+        post = {
+            'title': "Novo edital da instituição",
+            'text': "At vero eos et accusamus et iusto",
+            'institution': institution.key.urlsafe()
+        }
+        post_aux = {
+            'title': "Post Auxiliar",
+            'text': "At vero eos et accusamus et iusto",
+            'institution': institution.key.urlsafe()
+        }
+        self.testapp.post_json("/api/posts", post)
+        self.testapp.post_json("/api/posts", post_aux)
 
         # Call the get method
         posts = self.testapp.get("/api/institutions/%s/timeline?page=0&&limit=2" %
-                                 self.inst1.key.urlsafe())
+                                 institution.key.urlsafe())
         # Update the objects
         post_top = (posts.json['posts'])[0]
         key_post_top = ndb.Key(urlsafe=post_top['key'])
@@ -50,25 +63,25 @@ class InstitutionTimelineHandlerTest(TestBaseHandler):
 
         # Verify if the posts was published and your informations
         self.assertEqual(post_top_obj.title, 'Post Auxiliar',
-                         "The title expected was new post")
+                         "The title is not the expected one")
         self.assertEqual(post_top_obj.text, "At vero eos et accusamus et iusto",
-                         "The text expected was new post")
+                         "The text is not the expected one")
         self.assertEqual(post_top_obj.state, 'published',
                          "The state of post should be published")
-        self.assertEqual(post_last_obj.title, 'Novo edital do Inst 1',
-                         "The title expected was new post")
+        self.assertEqual(post_last_obj.title, 'Novo edital da instituição',
+                         "The title is not the expected one")
         self.assertEqual(post_last_obj.text, "At vero eos et accusamus et iusto",
-                         "The text expected was new post")
+                         "The text is not the expected one")
         self.assertEqual(post_last_obj.state, 'published',
                          "The state of post should be published")
 
         # Call the delete method for a post that has activity
-        post_last_obj.like(self.user1.key)
+        post_last_obj.like(user.key)
         self.testapp.delete("/api/posts/%s" % post_last_obj.key.urlsafe())
 
         # Call the get method
         posts = self.testapp.get("/api/institutions/%s/timeline?page=0&&limit=2" %
-                                 self.inst1.key.urlsafe())
+                                 institution.key.urlsafe())
 
         # Update the objects
         post_top = (posts.json['posts'])[0]
@@ -82,53 +95,8 @@ class InstitutionTimelineHandlerTest(TestBaseHandler):
         self.assertEqual(post_top["state"], 'deleted',
                          "The state of post should be deleted")
         self.assertEqual(post_last["title"], "Post Auxiliar",
-                         "The title expected was of post_aux")
+                         "The title is not the expected one")
         self.assertEqual(post_last["text"], "At vero eos et accusamus et iusto",
-                         "The text expected was of post_aux")
+                         "The text is not the expected one")
         self.assertEqual(post_last["state"], 'published',
                          "The state of post should be published")
-
-
-def initModels(cls):
-    """Init the models."""
-    # new User1
-    cls.user1 = User()
-    cls.user1.name = 'User 1'
-    cls.user1.cpf = '089.675.908-90'
-    cls.user1.email = ['user1@email.com']
-    cls.user1.institutions = []
-    cls.user1.follows = []
-    cls.user1.institutions_admin = []
-    cls.user1.notifications = []
-    cls.user1.posts = []
-    cls.user1.put()
-    # new Inst1
-    cls.inst1 = Institution()
-    cls.inst1.name = 'Inst 1'
-    cls.inst1.acronym = 'Inst 1'
-    cls.inst1.cnpj = '18.104.068/0001-86'
-    cls.inst1.legal_nature = 'public'
-    cls.inst1.actuation_area = ''
-    cls.inst1.description = 'Ensaio Químico'
-    cls.inst1.email = 'inst1@email.com'
-    cls.inst1.phone_number = '(83) 3322 4455'
-    cls.inst1.members = [cls.user1.key]
-    cls.inst1.followers = [cls.user1.key]
-    cls.inst1.posts = []
-    cls.inst1.admin = cls.user1.key
-    cls.inst1.put()
-    # POST of Mayza To Certbio Institution
-    cls.post_user1 = {
-        'title': "Novo edital do Inst 1",
-        'text': "At vero eos et accusamus et iusto",
-        'institution': cls.inst1.key.urlsafe()
-    }
-    cls.post_aux = {
-        'title': "Post Auxiliar",
-        'text': "At vero eos et accusamus et iusto",
-        'institution': cls.inst1.key.urlsafe()
-    }
-    cls.user1.follows = [cls.inst1.key]
-    cls.user1.put()
-
-    cls.user1.add_institution(cls.inst1.key)

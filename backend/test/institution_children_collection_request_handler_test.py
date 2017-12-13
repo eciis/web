d96283315983d36473e1ad8197748a3b804bd0d4
Mayza Nunes <mayzabeel@gmail.com>
2017-12-13 17:21:49 -0300

    update test to use mocks

--- backend/test/institution_children_collection_request_handler_test.py
+++ backend/test/institution_children_collection_request_handler_test.py
@@ -7,9 +7,12 @@ from models.user import User
 from models.institution import Institution
 from models.institution import Address
 from handlers.institution_children_request_collection_handler import InstitutionChildrenRequestCollectionHandler
+import mocks
 
 from mock import patch
 
+ADMIN = {'email': 'user1@gmail.com'}
+USER = {'email': 'otheruser@ccc.ufcg.edu.br'}
 
 class InstitutionChildrenRequestCollectionHandlerTest(TestBaseHandler):
     """Test the handler InstitutionParentRequestCollectionHandler."""
@@ -24,58 +27,84 @@ class InstitutionChildrenRequestCollectionHandlerTest(TestBaseHandler):
             [(InstitutionChildrenRequestCollectionHandlerTest.REQUEST_URI, InstitutionChildrenRequestCollectionHandler),
              ], debug=True)
         cls.testapp = cls.webtest.TestApp(app)
-        initModels(cls)
+        
 
-    @patch('utils.verify_token', return_value={'email': 'useradmin@test.com'})
+    @patch('utils.verify_token', return_value=ADMIN)
     def test_post(self, verify_token):
         """Test method post of InstitutionParentRequestCollectionHandler."""
-        data = {
-            'sender_key': self.other_user.key.urlsafe(),
-            'is_request': True,
-            'admin_key': self.user_admin.key.urlsafe(),
-            'institution_key': self.inst_test.key.urlsafe(),
-            'institution_requested_key': self.inst_requested.key.urlsafe(),
-            'type_of_invite': 'REQUEST_INSTITUTION_CHILDREN'
-        }
+        admin = mocks.create_user(ADMIN['email'])
+        institution = mocks.create_institution()		 
+        admin.institutions_admin = [institution.key]
+        institution.admin = admin.key
+        admin.add_permission("send_link_inst_request", institution.key.urlsafe())
+        admin.put()
+        institution.put()
+        other_user = mocks.create_user(USER['email'])
+        inst_requested = mocks.create_institution()
+        admin_requested = mocks.create_user(ADMIN['email'])
+        admin.institutions_admin = [inst_requested.key]
+        admin_requested.put()
+        inst_requested.admin = admin_requested.key 
+        inst_requested.put()
 
-        request = self.testapp.post_json(
-            "/api/institutions/" + self.inst_test.key.urlsafe() + "/requests/institution_children",
-            data)
+        data = {
+            'sender_key': admin.key.urlsafe(),
+            'is_request': True,
+            'admin_key': admin_requested.key.urlsafe(),
+            'institution_key': institution.key.urlsafe(),
+            'institution_requested_key': inst_requested.key.urlsafe(),
+            'type_of_invite': 'REQUEST_INSTITUTION_CHILDREN'
+        } 
+
+        request = self.testapp.post_json("/api/institutions/" + institution.key.urlsafe() + "/requests/institution_children", data)
 
         request = json.loads(request._app_iter[0])
 
-        institution = self.inst_test.key.get()
+        institution = institution.key.get()
 
         self.assertEqual(
             request['sender'],
-            self.other_user.email,
-            'Expected sender email is other_user@test.com')
+            admin.email,
+            'Expected sender email is user1@gmail.com')
         self.assertEqual(
             request['admin_name'],
-            self.user_admin.name,
-            'Expected sender admin_name is User Admin')
+            admin_requested.name,
+            'Expected admin is admin_requested is User Admin')
         self.assertEqual(
             request['type_of_invite'],
             'REQUEST_INSTITUTION_CHILDREN',
             'Expected sender type_of_invite is REQUEST_INSTITUTION_CHILDREN')
         
 
-    @patch('utils.verify_token', return_value={'email': 'otheruser@test.com'})
+    @patch('utils.verify_token', return_value=USER)
     def test_post_user_not_admin(self, verify_token):
         """Test post request with user is not admin."""
-
+        admin = mocks.create_user(ADMIN['email'])
+        institution = mocks.create_institution()		 
+        admin.institutions_admin = [institution.key]
+        institution.admin = admin.key
+        admin.add_permission("send_link_inst_request", institution.key.urlsafe())
+        admin.put()
+        institution.put()
+        other_user = mocks.create_user(USER['email'])
+        inst_requested = mocks.create_institution()
+        admin_requested = mocks.create_user(ADMIN['email'])
+        admin.institutions_admin = [inst_requested.key]
+        admin_requested.put()
+        inst_requested.admin = admin_requested.key 
+        inst_requested.put()
         data = {
-            'sender_key': self.other_user.key.urlsafe(),
+            'sender_key': other_user.key.urlsafe(),
             'is_request': True,
-            'admin_key': self.user_admin.key.urlsafe(),
-            'institution_key': self.inst_test.key.urlsafe(),
-            'institution_requested_key': self.inst_requested.key.urlsafe(),
+            'admin_key': admin.key.urlsafe(),
+            'institution_key': institution.key.urlsafe(),
+            'institution_requested_key': inst_requested.key.urlsafe(),
             'type_of_invite': 'REQUEST_INSTITUTION_CHILDREN'
         }
 
         with self.assertRaises(Exception) as ex:
             self.testapp.post_json(
-                "/api/institutions/" + self.inst_test.key.urlsafe() + "/requests/institution_children",
+                "/api/institutions/" + institution.key.urlsafe() + "/requests/institution_children",
                 data)
 
         exception_message = self.get_message_exception(ex.exception.message)
@@ -83,41 +112,3 @@ class InstitutionChildrenRequestCollectionHandlerTest(TestBaseHandler):
             "Error! User is not allowed to send request",
             exception_message,
             "Expected error message is Error! User is not allowed to send request")
-
-
-def initModels(cls):
-    """Init the models."""
-    # new User
-    cls.user_admin = User()
-    cls.user_admin.name = 'User Admin'
-    cls.user_admin.email = ['useradmin@test.com']
-    cls.user_admin.put()
-    # Other user
-    cls.other_user = User()
-    cls.other_user.name = 'Other User'
-    cls.other_user.email = ['otheruser@test.com']
-    cls.other_user.put()
-    # new institution address
-    cls.address = Address()
-    cls.address.street = "street"
-    # new Institution inst test
-    cls.inst_test = Institution()
-    cls.inst_test.name = 'inst test'
-    cls.inst_test.members = [cls.user_admin.key]
-    cls.inst_test.followers = [cls.user_admin.key]
-    cls.inst_test.admin = cls.user_admin.key
-    cls.inst_test.address = cls.address
-    cls.inst_test.put()
-    # Update institutions admin from User admin
-    cls.user_admin.institutions_admin = [cls.inst_test.key]
-    cls.user_admin.put()
-    cls.user_admin.add_permission("send_link_inst_request", cls.inst_test.key.urlsafe())
-    cls.user_admin.put()
-    # new Institution inst requested to be parent of inst test
-    cls.inst_requested = Institution()
-    cls.inst_requested.name = 'inst requested'
-    cls.inst_requested.members = [cls.user_admin.key]
-    cls.inst_requested.followers = [cls.user_admin.key]
-    cls.inst_requested.admin = cls.user_admin.key
-    cls.inst_requested.address = cls.address
-    cls.inst_requested.put()

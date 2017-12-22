1d962316b8fc747440a53d8ff427573fbf368e98
tiagolimpe <tiago.pereira@ccc.ufcg.edu.br>
2017-12-22 13:26:16 -0300

    Improving tests to search_handler and adding the test in case of search by description

--- backend/test/search_handler_test.py
+++ backend/test/search_handler_test.py
@@ -2,12 +2,11 @@
 """Search handler test."""
 
 from test_base_handler import TestBaseHandler
-from models.user import User
-from models.institution import Institution
 from handlers.search_handler import SearchHandler
-
 from mock import patch
+import mocks
 
+USER = {'email': 'test@example.com'}
 
 class SearchHandlerTest(TestBaseHandler):
     """Test the SearchHandler class."""
@@ -20,16 +19,27 @@ class SearchHandlerTest(TestBaseHandler):
             [("/api/search/institution", SearchHandler),
              ], debug=True)
         cls.testapp = cls.webtest.TestApp(app)
-        initModels(cls)
 
-    @patch('utils.verify_token', return_value={'email': 'test@example.com'})
+    @patch('utils.verify_token', return_value=USER)
     def test_get_institution(self, verify_token):
         """Test the search_handler's get method."""
+        splab = mocks.create_institution()
+        splab.name = 'Software Practice Laboratory'
+        splab.acronym = 'SPLAB'
+        splab.state = 'pending'
+        splab.actuation_area = 'Universidades'
+        splab.put()
+        certbio = mocks.create_institution()
+        certbio.name = 'Laboratório de Avaliação e Desenvolvimento de Biomateriais do Nordeste'
+        certbio.acronym = 'CERTBIO'
+        certbio.state = 'active'
+        certbio.actuation_area = 'Universidades'
+        certbio.description = 'Ensaio Químico - Determinação de Material Volátil por Gravimetria...'
+        certbio.put()
         # Search for the institution by its full name
-        certbio_name = 'Lab. de Desenvolvimento de Biomateriais do Nordeste'
         institutions = self.testapp.get(
             "/api/search/institution?value=%s&state=%s&type=institution"
-            % (certbio_name, 'active'))
+            % (certbio.name, 'active'))
         self.assertTrue('CERTBIO' in institutions)
         # Search for the institution by part of its name
         institutions = self.testapp.get(
@@ -59,59 +69,25 @@ class SearchHandlerTest(TestBaseHandler):
             "/api/search/institution?value=%s&state=%s&type=institution"
             % ('Universidades', 'active, pending'))
         self.assertTrue('CERTBIO' and 'SPLAB' in institutions)
+        # Search for institutions by description
+        institutions = self.testapp.get(
+            "/api/search/institution?value=%s&state=%s&type=institution"
+            % ('Ensaio quimico', 'active'))
+        self.assertTrue('CERTBIO' in institutions)
 
-    @patch('utils.verify_token', return_value={'email': 'test@example.com'})
+    @patch('utils.verify_token', return_value=USER)
     def test_get_user(self, verify_token):
         """Test the search_handler's get method with type=user."""
+        user = mocks.create_user(USER['email'])
+        user.name = "User Example"
+        user.put()
         # Call the get method with the user's full name
         users = self.testapp.get(
             "/api/search/institution?value=%s&state=%s&type=user"
-            % (self.user.name, self.user.state))
+            % (user.name, user.state))
         self.assertTrue("User" in users)
         # Call the get method with part of the user's name
-        users = self.testapp.get(
+        users = testapp.get(
             "/api/search/institution?value=%s&state=%s&type=user"
-            % ("User", self.user.state))
+            % ("User", user.state))
         self.assertTrue("User" in users)
-
-    def tearDown(cls):
-        """Deactivate the test."""
-        cls.test.deactivate()
-
-
-def initModels(cls):
-    """Init the models."""
-    # new User
-    cls.user = User()
-    cls.user.name = 'User'
-    cls.user.cpf = '089.675.908-90'
-    cls.user.email = ['test@example.com']
-    cls.user.institutions = []
-    cls.user.follows = []
-    cls.user.institutions_admin = []
-    cls.user.notifications = []
-    cls.user.posts = []
-    cls.user.put()
-    # new Institution CERTBIO
-    cls.certbio = Institution()
-    cls.certbio.name = 'Lab. de Desenvolvimento de Biomateriais do Nordeste'
-    cls.certbio.acronym = 'CERTBIO'
-    cls.certbio.state = 'active'
-    cls.certbio.actuation_area = 'Universidades'
-    cls.certbio.members = [cls.user.key]
-    cls.certbio.followers = [cls.user.key]
-    cls.certbio.admin = cls.user.key
-    cls.certbio.put()
-    # new Institution SPLAB
-    cls.splab = Institution()
-    cls.splab.name = 'Software Practice Laboratory'
-    cls.splab.acronym = 'SPLAB'
-    cls.splab.state = 'pending'
-    cls.splab.actuation_area = 'Universidades'
-    cls.splab.members = [cls.user.key]
-    cls.splab.followers = [cls.user.key]
-    cls.splab.admin = cls.user.key
-    cls.splab.put()
-    # updating user institutions admin
-    cls.user.institutions_admin = [cls.certbio.key, cls.splab.key]
-    cls.user.put()

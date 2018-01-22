9935d0cf8ca00e4897b255d4067c5e94aa2d141e
Raoni Smaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2018-01-22 10:02:47 -0300

    Adding docstring in create_field_values_string method

--- backend/search_module/search_institution.py
+++ backend/search_module/search_institution.py
@@ -116,13 +116,18 @@ class SearchInstitution(SearchDocument):
         return state_string
 
     def create_field_values_string(self, value):
-        """Create a string formed by fields and values."""
+        """Create a string formed by fields and values.
+        
+        If value is empty the method will return an empty string
+        which means that the query will be only by the state
+        and the fields won't be considered.
+        """
         # add a new field here
         fields = ['name', 'acronym', 'actuation_area', 'legal_nature', 'federal_state', 'description']
         fields_values = []
 
-        for field in fields:
-            if value:
+        if value:
+            for field in fields:
                 field_value = '%s: "%s"' % (field, value)
                 fields_values.append(field_value)
 

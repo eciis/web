ddd87f2955a3cff36b270222081412881c4da5ce
ruanres <ruan.silveira@ccc.ufcg.edu.br>
2018-01-30 15:31:05 -0300

    removed prints and updated the test

--- backend/test/institution_parent_request_handler_test.py
+++ backend/test/institution_parent_request_handler_test.py
@@ -2,17 +2,15 @@
 """Institution Parent request handler test."""
 
 import json
+import mocks
+
 from test_base_handler import TestBaseHandler
-from models.user import User
-from models.institution import Institution
+from models.invite import Invite
 from models.institution import Address
 from models.request_institution_parent import RequestInstitutionParent
 from handlers.institution_parent_request_handler import InstitutionParentRequestHandler
 from worker import AddAdminPermissionsInInstitutionHierarchy
-
-import mocks
 from mock import patch
-import mocks
 
 CURRENT_INSTITUTION = {'name': 'currentInstitution'}
 CURRENT_INST_STRING = json.dumps(CURRENT_INSTITUTION)
@@ -31,16 +29,45 @@ class InstitutionParentRequestHandlerTest(TestBaseHandler):
             ('/api/queue/add-admin-permissions', AddAdminPermissionsInInstitutionHierarchy)
              ], debug=True)
         cls.testapp = cls.webtest.TestApp(app)
-        initModels(cls)
+        
+        # create models
+        # new User
+        cls.user_admin = mocks.create_user('useradmin@test.com')
+        # Other user
+        cls.other_user = mocks.create_user('otheruser@test.com')
+        # new institution address
+        cls.address = Address()
+        cls.address.street = "street"
+        # new Institution inst test
+        cls.inst_test = mocks.create_institution()
+        cls.inst_test.admin = cls.user_admin.key
+        cls.inst_test.put()
+        # new Institution inst requested to be parent of inst test
+        cls.inst_requested = mocks.create_institution()
+        cls.inst_requested.admin = cls.other_user.key
+        cls.inst_requested.put()
+        # Update Institutions admin by other user
+        cls.other_user.add_permission("answer_link_inst_request", cls.inst_requested.key.urlsafe())
+        cls.other_user.put()
+        # new Request
+        cls.request = RequestInstitutionParent()
+        cls.request.sender_key = cls.user_admin.key
+        cls.request.is_request = True
+        cls.request.admin_key = cls.user_admin.key
+        cls.request.institution_key = cls.inst_test.key
+        cls.request.institution_requested_key = cls.inst_requested.key
+        cls.request.type_of_invite = 'REQUEST_INSTITUTION_PARENT'
+        cls.request.put()
+
     
     def enqueue_task(self, handler_selector, params):
         """Method of mock enqueue tasks."""
         if handler_selector == 'add-admin-permissions':
             self.testapp.post('/api/queue/' + handler_selector, params=params)
 
-    @patch('service_messages.send_message_notification')
+    @patch.object(Invite, 'send_notification')
     @patch('utils.verify_token', return_value={'email': 'otheruser@test.com'})
-    def test_put(self, verify_token, mock_method):
+    def test_put(self, verify_token, send_notification):
         """Test method post of InstitutionParentRequestHandler."""
         request = self.testapp.put_json(
             "/api/requests/%s/institution_parent?currentInstitution=%s"
@@ -58,8 +85,14 @@ class InstitutionParentRequestHandlerTest(TestBaseHandler):
         self.assertEqual(
             institution.children_institutions[0], self.inst_test.key,
             "The children institution of inst test must be update to inst_requested")
-        self.assertTrue(mock_method.assert_called,
-                        "Should call the send_message_notification")
+        
+        # Assert the notification was sent
+        send_notification.assert_called_with(
+            current_institution=CURRENT_INSTITUTION, 
+            sender_key=self.other_user.key, 
+            receiver_key=self.user_admin.key,
+            entity_type='ACCEPT_INSTITUTION_LINK'
+        )
 
     @patch('utils.verify_token', return_value={'email': 'useradmin@test.com'})
     def test_put_user_not_admin(self, verify_token):
@@ -76,13 +109,10 @@ class InstitutionParentRequestHandlerTest(TestBaseHandler):
             exception_message,
             "Expected error message is Error! User is not allowed to accept link between institutions")
 
-    @patch('service_messages.send_message_notification')
+    @patch.object(Invite, 'send_notification')
     @patch('utils.verify_token', return_value={'email': 'otheruser@test.com'})
-    def test_delete(self, verify_token, send_message_notification):
+    def test_delete(self, verify_token, send_notification):
         """Test method post of InstitutionChildrenRequestHandler."""
-        print "under test"
-        print self.request.key
-
         self.testapp.delete(
             "/api/requests/%s/institution_parent?currentInstitution=%s"
              % (self.request.key.urlsafe(), CURRENT_INST_STRING)
@@ -100,8 +130,13 @@ class InstitutionParentRequestHandlerTest(TestBaseHandler):
             institution.children_institutions, [],
             "The list of children institution of inst requested is empty")
 
-        self.assertTrue(send_message_notification.assert_called,
-                        "Should call the send_message_notification")
+        # assert notfication was sent
+        send_notification.assert_called_with(
+            current_institution=CURRENT_INSTITUTION, 
+            sender_key=self.other_user.key, 
+            receiver_key=self.user_admin.key,
+            entity_type='REJECT_INSTITUTION_LINK'
+        )
 
     @patch('handlers.institution_parent_request_handler.enqueue_task')
     @patch('utils.verify_token', return_value={'email': 'user@example.com'})
@@ -164,34 +199,3 @@ class InstitutionParentRequestHandlerTest(TestBaseHandler):
         self.assertTrue(third_inst.key.urlsafe() in first_user.permissions['publish_post'])
         self.assertTrue(third_inst.key.urlsafe() in second_user.permissions['publish_post'])
         self.assertTrue(third_inst.key.urlsafe() in third_user.permissions['publish_post'])
-
-
-def initModels(cls):
-    """Init the models."""
-    # new User
-    cls.user_admin = mocks.create_user('useradmin@test.com')
-    # Other user
-    cls.other_user = mocks.create_user('otheruser@test.com')
-    # new institution address
-    cls.address = Address()
-    cls.address.street = "street"
-    # new Institution inst test
-    cls.inst_test = mocks.create_institution()
-    cls.inst_test.admin = cls.user_admin.key
-    cls.inst_test.put()
-    # new Institution inst requested to be parent of inst test
-    cls.inst_requested = mocks.create_institution()
-    cls.inst_requested.admin = cls.other_user.key
-    cls.inst_requested.put()
-    # Update Institutions admin by other user
-    cls.other_user.add_permission("answer_link_inst_request", cls.inst_requested.key.urlsafe())
-    cls.other_user.put()
-    # new Request
-    cls.request = RequestInstitutionParent()
-    cls.request.sender_key = cls.other_user.key
-    cls.request.is_request = True
-    cls.request.admin_key = cls.user_admin.key
-    cls.request.institution_key = cls.inst_test.key
-    cls.request.institution_requested_key = cls.inst_requested.key
-    cls.request.type_of_invite = 'REQUEST_INSTITUTION_PARENT'
-    cls.request.put()

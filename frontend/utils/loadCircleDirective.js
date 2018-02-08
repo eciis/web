827d89dfcc65371cf18c1dd02d3c7f4c329513da
MaianaB <maiana.brito@ccc.ufcg.edu.br>
2018-02-08 09:22:01 -0300

    fix layout-fill in directive load-circle

--- frontend/utils/loadCircleDirective.js
+++ frontend/utils/loadCircleDirective.js
@@ -3,11 +3,22 @@
 (function() {
     var app = angular.module("app");
 
+    app.controller("LoadController", function($scope){
+        console.log($scope.addLayoutFill);
+        $scope.addLayoutFill ? 
+            document.getElementById("element").classList.add("layout-fill") : "";
+    });
+
     app.directive("loadCircle", function() {
         return {
             restrict: 'E',
+            scope: {
+                addLayoutFill: '='
+            },
+            controller: "loadController",
+            controlerAs: "ctrl",
             template: '<div flex>'
-                    + '<div layout="column" layout-align="center center" layout-fill>'
+                    + '<div layout="column" id="element" layout-align="center center">'
                     + '<md-progress-circular md-mode="indeterminate"></md-progress-circular>'
                     + '</div>'
                     + '</div>'

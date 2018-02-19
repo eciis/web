84491540a8d98f3eda580bce382231f808ba378c
André L. Abrantes <andredossantosabrantes@gmail.com>
2018-02-19 16:17:10 -0300

    Add a dialog to show the report content

--- support/home/reportHomeController.js
+++ support/home/reportHomeController.js
@@ -1,13 +1,25 @@
-(function() {
+(function () {
     'use strict';
-    
+
     var support = angular.module("support");
 
-    support.controller("ReportHomeController", function ReportHomeController($state, FirebaseService) {
+    support.controller("ReportHomeController", function ReportHomeController($state, FirebaseService, $mdDialog) {
         var controller = this;
 
         controller.reports = [];
-        
+
+        controller.selectedReport = null;
+
+        controller.showReport = function showReport(report, event) {
+            controller.selectedReport = report;
+            $mdDialog.show({
+                contentElement: '#showReport',
+                parent: angular.element(document.body),
+                targetEvent: event,
+                clickOutsideToClose: true
+            });
+        };
+
         (function main() {
             FirebaseService.getReports().then(function (reports) {
                 controller.reports = reports;

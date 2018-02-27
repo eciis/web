941cb00921d317f3338d8f627866d0ed291d1c29
Mayza Nunes <mayzabeel@gmail.com>
2018-02-27 17:54:43 -0300

    create coontroller and update directive instution_card

--- frontend/institution/allInstitutionsController.js
+++ frontend/institution/allInstitutionsController.js
@@ -20,8 +20,76 @@
         loadInstitutions();
     });
 
-    app.controller("InstitutionCardController", function InstitutionCardController(){
-        var instituinsCardCtrl = this;
+    app.controller("InstitutionCardController", function InstitutionCardController(
+        $state, AuthService, InstitutionService, MessageService){
+        var institutionCardCtrl = this;
+
+        institutionCardCtrl.user = AuthService.getCurrentUser();
+
+        institutionCardCtrl.goToInstitution = function goToInstitution(institutionKey) {
+            $state.go('app.institution.timeline', {institutionKey: institutionKey});
+        };
+
+        institutionCardCtrl.showFollowButton = function showFollowButton() {
+            return institutionCardCtrl.institution && !institutionCardCtrl.isMember && 
+                   institutionCardCtrl.institution.name !== "Ministério da Saúde" &&
+                   institutionCardCtrl.institution.name !== "Departamento do Complexo Industrial e Inovação em Saúde";
+         };
+
+        institutionCardCtrl.follow = function follow(){
+            var promise = InstitutionService.follow(institutionCardCtrl.institution.key);
+            promise.then(function success(){
+                MessageService.showToast("Seguindo "+ institutionCardCtrl.institution.name);
+                var institution =  {
+                    acronym: institutionCardCtrl.institution.acronym,
+                    key: institutionCardCtrl.institution.key,
+                    photo_url: institutionCardCtrl.institution.photo_url,
+                    legal_nature: institutionCardCtrl.institution.legal_nature,
+                    actuation_area: institutionCardCtrl.institution.actuation_area
+                };
+                institutionCardCtrl.user.follow(institution);
+                institutionCardCtrl.isUserFollower = institutionCardCtrl.user.isFollower(institutionCardCtrl.institution);
+                AuthService.save();
+            }, function error() {
+                MessageService.showToast('Erro ao seguir a instituição.');
+            });
+            return promise;
+        };
+
+        institutionCardCtrl.unfollow = function unfollow() {
+            if(institutionCardCtrl.user.isMember(institutionCardCtrl.institution.key)){
+                MessageService.showToast("Você não pode deixar de seguir " + institutionCardCtrl.institution.name);
+            }
+            else{
+                var promise = InstitutionService.unfollow(institutionCardCtrl.institution.key);
+                promise.then(function success(){
+                    MessageService.showToast("Deixou de seguir "+institutionCardCtrl.institution.name);
+                    institutionCardCtrl.user.unfollow(institutionCardCtrl.institution);
+                    institutionCardCtrl.user.isFollower(institutionCardCtrl.institution);
+                    institutionCardCtrl.isUserFollower =  institutionCardCtrl.user.isFollower(institutionCardCtrl.institution);
+                    AuthService.save();
+                }, function error() {
+                    MessageService.showToast('Erro ao deixar de seguir instituição.');
+                });
+                return promise;
+            }
+        };
+
+        institutionCardCtrl.checkIfUserIsMember = function checkIfUserIsMember() {
+            if (institutionCardCtrl.institution){
+                var institutionKey = institutionCardCtrl.institution.key;
+                institutionCardCtrl.isMember = institutionCardCtrl.user.isMember(institutionKey);
+            }
+        };
+
+        function checkIfUserIsFollower() {
+            if (institutionCardCtrl.user && institutionCardCtrl.institution){
+                institutionCardCtrl.isUserFollower = institutionCardCtrl.user.isFollower(institutionCardCtrl.institution);
+            }   
+        }
+
+        checkIfUserIsFollower();
+        institutionCardCtrl.checkIfUserIsMember();
     });
 
     app.directive("institutionCardDetails", function () {
@@ -32,7 +100,8 @@
             controller: "InstitutionCardController",
             scope: {},
             bindToController: {
-                institution: '='
+                institution: '=',
+                user: '='
             }
         };
     });

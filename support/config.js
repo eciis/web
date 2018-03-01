bacdfbe9aa89ef6b108379cbd05605f6ce104bd6
MaianaB <maiana.brito@ccc.ufcg.edu.br>
2018-03-01 13:55:43 -0300

    Reverting config.js from frontend and support

--- support/config.js
+++ support/config.js
@@ -3,5 +3,5 @@
 var Config = {
     BACKEND_URL: 'http://localhost:8082',
     LANDINGPAGE_URL: 'http://localhost:8080',
-    APP_VERSION: 'send-invites-to-several-emails'
+    APP_VERSION: 'master'
 };

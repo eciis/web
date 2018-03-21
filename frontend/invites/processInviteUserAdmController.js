da3dfeef7a5cd0d171378d9332fdef5739a1be14
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2018-03-21 09:34:07 -0300

    Add institution key of invite in list of admin institutions on invited user

--- frontend/invites/processInviteUserAdmController.js
+++ frontend/invites/processInviteUserAdmController.js
@@ -3,15 +3,18 @@
 (function() {
     var app = angular.module('app');
 
-    app.controller('ProcessInviteUserAdmController', function ProcessInviteUserAdmController(key, type_of_dialog, InviteService, MessageService, $mdDialog) {
+    app.controller('ProcessInviteUserAdmController', function ProcessInviteUserAdmController(key, type_of_dialog, InviteService, MessageService, AuthService, $mdDialog) {
         var processCtrl = this;
         processCtrl.invite = {};
         processCtrl.type_of_dialog = type_of_dialog;
         processCtrl.isAccepting = false;
+        processCtrl.current_user = AuthService.getCurrentUser();
 
         processCtrl.accept = function accept() {
             InviteService.acceptInviteUserAdm(processCtrl.invite.key).then(function success() {
                 MessageService.showToast('Convite aceito com sucesso!');
+                processCtrl.current_user.institutions_admin.push(processCtrl.invite.institution_key);
+                AuthService.save();
                 processCtrl.type_of_dialog = 'accepted';
                 processCtrl.isAccepting = true;
             });
@@ -32,8 +35,16 @@
             InviteService.getInvite(key).then(function success(response) {
                 let invite = new Invite(response.data);
 
-                if (invite.status === 'sent' || type_of_dialog !== 'accept') {
+                if (invite.status === 'sent' || type_of_dialog === 'accepted') {
                     processCtrl.invite = invite;
+
+                    if(invite.status === 'accepted') {
+                        _.remove(processCtrl.current_user.institutions_admin, function(url) {
+                            return getKey(url) === invite.institution_key;
+                        });
+                        AuthService.save();
+                    }
+
                 } else {
                     MessageService.showToast('Convite já processado!');
                     processCtrl.close();

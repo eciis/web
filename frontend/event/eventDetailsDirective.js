a7e8dc23411dbf5d0d3154d66151a43f711b434e
tiagolimpe <tiago.pereira@ccc.ufcg.edu.br>
2018-04-04 09:14:02 -0300

    Fix null event on event page

--- frontend/event/eventDetailsDirective.js
+++ frontend/event/eventDetailsDirective.js
@@ -59,7 +59,8 @@
         };
 
         eventCtrl.canChange = function canChange(event) {
-            return eventCtrl.isEventAuthor(event) || isInstitutionAdmin(event);
+            if(event)
+                return eventCtrl.isEventAuthor(event) || isInstitutionAdmin(event);
         };
 
         eventCtrl.canEdit = function canEdit(event) {
@@ -132,8 +133,9 @@
         };
 
         function isInstitutionAdmin(event) {
-            return _.includes(_.map(eventCtrl.user.institutions_admin, Utils.getKeyFromUrl),
-                Utils.getKeyFromUrl(event.institution_key));
+            if(event.institution_key)
+                return _.includes(_.map(eventCtrl.user.institutions_admin, Utils.getKeyFromUrl),
+                    Utils.getKeyFromUrl(event.institution_key));
         }
     });
     

297fffa2e269b2aac09e24caddcf1dd88cdc6d65
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2018-04-20 10:06:04 -0300

    Show message if edit post is fail

--- frontend/post/postDirective.js
+++ frontend/post/postDirective.js
@@ -303,6 +303,8 @@
                             $mdDialog.cancel();
                             MessageService.showToast(response.data.msg);
                         });
+                    }, function error() {
+                        MessageService.showToast("Esse post não pode ser editado pois já possue atividade");
                     });
                 } else {
                     MessageService.showToast('Edição inválida!');

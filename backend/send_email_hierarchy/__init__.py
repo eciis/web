9951064fdd404c1bdfd9e8984b98a4043afd41d9
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2018-05-21 11:24:52 -0300

    Add invite_user_email_sender in __init__.py

--- backend/send_email_hierarchy/__init__.py
+++ backend/send_email_hierarchy/__init__.py
@@ -3,10 +3,12 @@
 from .email_sender import *
 from .accepted_institution_email_sender import *
 from .invite_institution_email_sender import *
+from .invite_user_email_sender import *
 
 
 email_modules = [
-    email_sender, accepted_institution_email_sender, invite_institution_email_sender
+    email_sender, accepted_institution_email_sender, invite_institution_email_sender,
+    invite_user_email_sender
 ]
 
 __all__ = [prop for email_module in email_modules for prop in email_module.__all__]

8af8808ff6d11cfaa59241c8acaae66a082de33e
Raoni Smaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2018-05-22 07:31:41 -0300

    Fixing some messages and colors

--- backend/util/strings_pt_br.py
+++ backend/util/strings_pt_br.py
@@ -12,7 +12,7 @@ SUBJECT_TRANSLATION = {
     'REJECT_LINK_EMAIL': "Seu vínculo não foi aprovado.",
     'REMOVED_LINK_EMAIL': 'Vínculo removido na Plataforma Virtual CIS.',
     'INACTIVE_USER': "Você foi removido da Plataforma Virtual CIS.",
-    'TRANSFER_ADM_EMAIL': 'Você foi indicado para ser administrador de uma instituição na plataforma virtual CIS.'
+    'TRANSFER_ADM_EMAIL': 'Você foi indicado para ser administrador de uma instituição na Plataforma Virtual CIS.'
 }
 
 

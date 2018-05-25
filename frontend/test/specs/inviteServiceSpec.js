566e8003a32d39788f74d689a8efb46ac5b73b69
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2018-05-25 12:01:46 -0300

    Refact test sendInvite in inviteServiceSpec

--- frontend/test/specs/inviteServiceSpec.js
+++ frontend/test/specs/inviteServiceSpec.js
@@ -33,9 +33,9 @@
             spyOn($http, 'post').and.callThrough();
             httpBackend.expect('POST', INVITES_URI).respond(inviteUser);
             var result;
-            service.sendInvite(inviteUser).then(function(data){
+            service.sendInvite({invite_body: inviteUser}).then(function(data){
                 result = data;
-                body['data'] = inviteUser;
+                body['data'] = {invite_body: inviteUser};
                 expect($http.post).toHaveBeenCalledWith(INVITES_URI, body);
                 expect(result.data).toEqual(inviteUser);
                 done();

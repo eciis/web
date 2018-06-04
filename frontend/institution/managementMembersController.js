3f8f4fca030b9aa4cfbce0bd82c63de3fa4d2703
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2018-06-04 11:13:37 -0300

    Rename parameter from response to response_data

--- frontend/institution/managementMembersController.js
+++ frontend/institution/managementMembersController.js
@@ -113,13 +113,13 @@
             if (manageMemberCtrl.isValidAllEmails(emails) && manageMemberCtrl.isUserInviteValid(invite)) {
                 manageMemberCtrl.isLoadingInvite = true;
                 var promise = InviteService.sendInviteUser(requestBody);
-                promise.then(function success(response) {
-                    refreshSentInvitations(requestBody.emails, response.invites);
+                promise.then(function success(response_data) {
+                    refreshSentInvitations(requestBody.emails, response_data.invites);
                     manageMemberCtrl.clearInvite(); 
                     manageMemberCtrl.showInvites = true; 
                     manageMemberCtrl.showSendInvite = false;
                     manageMemberCtrl.isLoadingInvite = false;
-                    MessageService.showToast(response.msg);
+                    MessageService.showToast(response_data.msg);
                 }, function error(response) {
                     manageMemberCtrl.isLoadingInvite = false;
                     MessageService.showToast(response.msg);

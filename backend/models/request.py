fa3e54368abaed57234c3ec6c95857778d334aff
ruanres <ruan.silveira@ccc.ufcg.edu.br>
2018-06-11 10:57:26 -0300

    add verification to get the parent only if it exists

--- backend/models/request.py
+++ backend/models/request.py
@@ -51,16 +51,20 @@ class Request(Invite):
         request_inst_json['status'] = self.status
         request_inst_json['institution_key'] = self.institution_key.urlsafe()
         request_inst_json['sender_key'] = self.sender_key.urlsafe()
+        
         if self.institution_requested_key:
             requested_inst = self.institution_requested_key.get()
-            requested_inst_parent = requested_inst.parent_institution.get()
+            parent_key = requested_inst.parent_institution
             requested_inst = requested_inst.make(Request.INST_PROPS_TO_MAKE)
-            requested_inst_parent = requested_inst_parent.make(Request.INST_PROPS_TO_MAKE)
 
+            if(parent_key):
+                parent_inst = parent_key.get()
+                parent_inst = parent_inst.make(Request.INST_PROPS_TO_MAKE)
+                requested_inst['parent_institution'] = parent_inst
+            
             # TODO remove the use of requested_inst_name
             # author: Ruan Eloy - 07/08/18
-            request_inst_json['requested_inst_name'] = requested_inst['name']
             request_inst_json['requested_institution'] = requested_inst
-            request_inst_json['requested_institution']['parent_institution'] = requested_inst_parent
+            request_inst_json['requested_inst_name'] = requested_inst['name']
 
         return request_inst_json

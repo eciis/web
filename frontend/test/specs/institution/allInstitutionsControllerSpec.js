526e97bf4a6090d0eb69548b7897dc2a1f8c26ac
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2018-06-18 16:59:21 -0300

    Create tests of function getInstitutions

--- frontend/test/specs/institution/allInstitutionsControllerSpec.js
+++ frontend/test/specs/institution/allInstitutionsControllerSpec.js
@@ -1,6 +1,58 @@
 'use strict';
 
 (describe('AllInstitutionsController Test', function() {
+    beforeEach(module('app'));
 
-    
+    var user = {
+        name: 'test',
+        key: 'sahdkjsahdkj-IHAKJHAJKH8789AJHSJKHSA'
+    };
+
+    var institution = {
+        name: 'institution'
+    };
+
+    var other_institution = {
+        name: 'other_inst'
+    };
+
+    var authService, institutionService, allInstitutionsController;
+
+    beforeEach(inject(function(AuthService, InstitutionService, $controller) {
+        authService = AuthService;
+        institutionService = InstitutionService;
+
+        authService.login(user);
+        spyOn(Utils, 'setScrollListener').and.callFake(function() {});
+
+        allInstitutionsController = $controller('AllInstitutionsController', {
+            AuthService,
+            InstitutionService
+        });
+    }));
+
+
+    describe('Test getInstitutions()', function() {
+
+        it('Should return all institutions', function() {
+            allInstitutionsController.institutions = [institution, other_institution];
+            expect(allInstitutionsController.getInstitutions()).toEqual([institution, other_institution]);
+            allInstitutionsController.filterKeyword = "*";
+            expect(allInstitutionsController.getInstitutions()).toEqual([institution, other_institution]);
+        });
+
+        it('Should return institution found', function() {
+            allInstitutionsController.institutions = [institution, other_institution];
+            allInstitutionsController.filterKeyword = "institution";
+            expect(allInstitutionsController.getInstitutions()).toEqual([institution]);
+            allInstitutionsController.filterKeyword = "other_inst";
+            expect(allInstitutionsController.getInstitutions()).toEqual([other_institution]);
+        });
+
+        fit('Shuld return empty list', function() {
+            allInstitutionsController.institutions = [institution, other_institution];
+            allInstitutionsController.filterKeyword = "institution_inst";
+            expect(allInstitutionsController.getInstitutions()).toEqual([]);
+        });
+    });
 }));

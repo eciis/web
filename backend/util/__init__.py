b7796df79b280fadc43973f9217004c7623b00e7
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2018-06-21 17:30:03 -0300

    Implement method create_notification_task in NotificationsQueueManager model

--- backend/util/__init__.py
+++ backend/util/__init__.py
@@ -3,8 +3,9 @@
 from .json_patch import *
 from .strings_pt_br import *
 from .login_service import *
+from .notifications_queue_manager import *
 
 
-util_modules = [json_patch, strings_pt_br, login_service]
+util_modules = [json_patch, strings_pt_br, login_service, notifications_queue_manager]
 
 __all__ = [prop for util_module in util_modules for prop in util_module.__all__]
--- backend/util/notifications_queue_manager.py
+++ backend/util/notifications_queue_manager.py
@@ -1,73 +1,73 @@
 """Notifications Queue Manager."""
 
 import json
+import time
 from google.appengine.api import taskqueue
 from protorpc import messages
 
-__all__ = ['NotiticationsQueueManager']
+__all__ = ['NotiticationsQueueManager', 'NotificationMessage']
 
 queue = taskqueue.Queue('notifications-manage-pull')
 notification_id = {
+    "00": "ALL_NOTIFICATIONS",
     "01": "ACCEPT_INSTITUTION_LINK",
     "02": "ACCEPT_INVITE_HIERARCHY",
     "03": "ACCEPT_INVITE_USER_ADM"
 }
 
+def get_notification_id(notification_type):
+    for key, value in notification_id.items():
+        if value == notification_type:
+            return key
+    
+    return "00"
+
 class NotiticationsQueueManager:
     
     @staticmethod
-    def create_notification_task(message):
-        pass
+    def create_notification_task(notification_message):
+        message = notification_message.message
+        notification_type = notification_message.notification_type
+        notification_key = notification_message.key
+
+        task = taskqueue.Task(
+            name=notification_key,
+            payload=json.dumps(message),
+            tag=notification_type,
+            method='PULL'
+        )
+        queue.add(task)
+
+        return notification_key
+
     
     @staticmethod
     def resolve_notification_task(notification_id):
         pass
 
 
-class Message(messages.Message):
-    from_name = messages.StringFiled(1, required=True)
-    from_institution_name = messages.StringFiled(2)
-    from_photo_url = messages.StringFiled(3, required=True)
-    to_institution_name = messages.StringFiled(4)
-    current_instituion_name = messages.StringFiled(5)
-
-    @staticmethod
-    def create_message(message_dict):
-        message_from = message_dict.get('from')
-        message_to = message_dict.get('to')
-        message_current_inst = message_dict('current_instiution')
-
-        return Message(
-            from_name=message_from.get('name'),
-            from_institution_name=message_from.get('institution_name'),
-            from_photo_url=message_from.get('photo_url'),
-            to_institution_name=message_to.get('institution_name'),
-            current_instituion_name=message_current_inst('name')
-        )
+class NotificationMessage(object):
+    def __init__(self, message, entity_urlsafe, notification_type, receiver_urlsafe):
+        self.message = message
+        self.entity_urlsafe = entity_urlsafe
+        self.notification_type = notification_type
+        self.receiver_urlsafe = receiver_urlsafe
+        self.key = self._gererate_key()
     
-    @staticmethod
-    def format_message(message):
+    def format_notification(self):
         return {
-            'from': {
-                'name': message.field_by_number(1),
-                'photo_url': message.field_by_number(3),
-                'institution_name': message.field_by_number(2)
-            },
-            'to': {
-                'institution_name': message.field_by_number(4)
-            },
-            'current_institution': {
-                'name': message.field_by_number(5)
-            }
+            'receiver_key': self.receiver_urlsafe,
+            'message': self.message,
+            'notification_type': self.notification_type,
+            'entity_key': self.entity_urlsafe
         }
+    
+    def _gererate_key(self):
+        id = get_notification_id(self.notification_type)
+        entity_hash = hash(self.entity_urlsafe)
+        receiver_hash = hash(self.receiver_urlsafe)
+        timestamp = time.time()
 
-
-class NotificationMessage(messages.Message):
-    message = messages.MessageFiled(1, required=True)
-    entity_urlsafe = messages.StringFiled(2)
-    notification_type = messages.StringFiled(3, required=True),
-    receiver_urlsafe = messages.StringFiled(4, required=True)
-
-    def __init__(self, *args, **kwargs):
-        kwargs['message'] = Message.create_message(kwargs['message'])
-        super(NotificationMessage, self).__init__(*args, **kwargs)
+        key = id + '-' + str(entity_hash) + str(receiver_hash) + str(timestamp)
+        key = key.replace(".", "")
+        return key

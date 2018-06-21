27986ea34c703dc972231565fbeb5a936f657d21
ruanres <ruan.silveira@ccc.ufcg.edu.br>
2018-06-21 11:49:46 -0300

    create methods on Institution and Invite models; create a function to show a messageDialog, and add verification to check if the request is valid

--- frontend/invites/invite.js
+++ frontend/invites/invite.js
@@ -3,17 +3,18 @@
 function Invite(data) {
     data = data || {};
     _.extend(this, data);
+
+    this.institution = new Institution(this.institution);
+    this.requested_institution = this.requested_institution &&
+        new Institution(this.requested_institution);
 }
 
 Invite.prototype.isValid = function isValid() {
-    var isInviteeNecessary = this.type_of_invite != 'USER';
-    var hasNoInvitee = (_.isUndefined(this.invitee) || _.isEmpty(this.invitee)) && isInviteeNecessary;
-    var hasNoType = _.isUndefined(this.type_of_invite) || _.isEmpty(this.type_of_invite);
-
-    if (hasNoInvitee || hasNoType) {
-        return false;
-    }
-    return true;
+    const hasType = isValueValid(this.type_of_invite);
+    const hasInviteeWhenNeeded = this.type_of_invite == 'USER' || isValueValid(this.invitee);
+    const areInstitutionsValid = this.isInstitutionActive() && this.isRequestedInstActive();
+    
+    return hasType && hasInviteeWhenNeeded && areInstitutionsValid; 
 };
 
 Invite.prototype.isStatusOn = function isStatusOn(value) {
@@ -27,3 +28,19 @@ Invite.prototype.setStatus = function (status) {
 Invite.prototype.setType = function (typeOfInvite) {
     this.type_of_invite = typeOfInvite;
 };
+
+Invite.prototype.isSent = function () {
+    return this.isStatusOn('sent');
+};
+
+Invite.prototype.isInstitutionActive = function () {
+    return this.institution.isStateOn('active');
+};
+
+Invite.prototype.isRequestedInstActive = function () {
+    return this.requested_institution ? this.requested_institution.isStateOn('active') : true;
+};
+
+function isValueValid(value) {
+    return  !(_.isUndefined(value) || _.isEmpty(value));
+}

0a50d604095eab837bc66d316a301297d8852796
ruanres <ruan.silveira@ccc.ufcg.edu.br>
2018-07-05 10:28:11 -0300

    add tests to worker aux methods

--- backend/test/worker_handlers_tests/worker_aux_methods_test.py
+++ backend/test/worker_handlers_tests/worker_aux_methods_test.py
@@ -2,16 +2,171 @@
 """WorkerAuxMethodsTest."""
 
 from ..test_base import TestBase
-import permissions
 from .. import mocks
-from worker import get_all_parent_admins
+from worker import get_all_parent_admins, should_remove, is_not_admin
+from worker import is_admin_of_parent_inst, filter_permissions_to_remove
 
 
 class WorkerAuxMethodsTest(TestBase):
     """Test worker aux methods."""
     
+    def test_should_remove(self):
+        """Test the method should_remove."""
+
+        user = mocks.create_user()
+        first_inst = mocks.create_institution()
+        second_inst = mocks.create_institution()
+        third_inst = mocks.create_institution()
+
+        user.add_institution(first_inst.key)
+        user.add_institution_admin(first_inst.key)
+
+        first_inst_urlsafe = first_inst.key.urlsafe()
+        second_inst_urlsafe = second_inst.key.urlsafe()
+        third_inst_urlsafe = third_inst.key.urlsafe()
+
+        # should be false, because user is admin of fist_inst
+        self.assertFalse(should_remove(user, first_inst_urlsafe, second_inst_urlsafe))
+        # should be true, because second_inst is the institution the user is transfering
+        # to another admin
+        self.assertTrue(should_remove(user, second_inst_urlsafe, second_inst_urlsafe))
+        # should be true, because user is not admin of third_inst
+        self.assertTrue(should_remove(user, third_inst_urlsafe, second_inst_urlsafe))
+
+
+    def test_filter_permissions_to_remove(self):
+        """Test method filter_permissions_to_remove."""
+
+        def set_admin(user, inst):
+            user.add_institution(inst.key)
+            user.add_institution_admin(inst.key)
+            inst.add_member(user)
+            inst.set_admin(user.key)
+
+
+        user = mocks.create_user()
+        first_inst = mocks.create_institution()
+        second_inst = mocks.create_institution()
+        third_inst = mocks.create_institution()
+
+        set_admin(user, first_inst)
+
+        first_inst_urlsafe = first_inst.key.urlsafe()
+        second_inst_urlsafe = second_inst.key.urlsafe()
+        third_inst_urlsafe = third_inst.key.urlsafe()
+
+        PERMISSIONS = ['PERMISSION_A', 'PERMISSION_B', 'PERMISSION_C']
+
+        user.add_permissions(PERMISSIONS, first_inst_urlsafe)
+        user.add_permissions(PERMISSIONS, second_inst_urlsafe)
+        user.add_permissions(PERMISSIONS, third_inst_urlsafe)
+
+        expected_permissions = {
+            'publish_survey': [],
+            'publish_post': []
+        }
+        for permission_type in PERMISSIONS:
+            expected_permissions[permission_type] = [second_inst_urlsafe, third_inst_urlsafe]
+
+        permissions_to_remove = filter_permissions_to_remove(
+            user, user.permissions,
+            second_inst_urlsafe, should_remove
+        )
+
+        # assert filtered permissions using should_remove method
+        self.assertEquals(permissions_to_remove, expected_permissions)
+
+        # make user admin of second_inst
+        set_admin(user, second_inst)
+        
+        permissions_to_remove = filter_permissions_to_remove(
+            user, user.permissions,
+            second_inst_urlsafe, is_not_admin
+        )
+        # update expected_permissions
+        for permission_type in PERMISSIONS:
+            expected_permissions[permission_type] = [third_inst_urlsafe]
+        
+        # assert filtered permissions using is_not_admin method
+        self.assertEquals(permissions_to_remove, expected_permissions)
+
+
+    def test_is_admin_of_parent_inst(self):
+        """Test method is_admin_of_parent_inst."""
+
+        first_user = mocks.create_user()
+        second_user = mocks.create_user()
+        third_user = mocks.create_user()
+
+        first_inst = mocks.create_institution()
+        second_inst = mocks.create_institution()
+        third_inst = mocks.create_institution()
+        fourth_inst = mocks.create_institution()
+
+        first_inst.add_member(first_user)
+        second_inst.add_member(second_user)
+        third_inst.add_member(first_user)
+        fourth_inst.add_member(third_user)
+
+        first_inst.set_admin(first_user.key)
+        second_inst.set_admin(second_user.key)
+        third_inst.set_admin(first_user.key)
+        fourth_inst.set_admin(third_user.key)
+
+        first_user.add_institution(first_inst.key)
+        first_user.add_institution(third_inst.key)
+        second_user.add_institution(second_inst.key)
+        third_user.add_institution(fourth_inst.key)
+
+        first_user.add_institution_admin(first_inst.key)
+        first_user.add_institution_admin(third_inst.key)
+        second_user.add_institution_admin(second_inst.key)
+        third_user.add_institution_admin(fourth_inst.key)
+
+        # Hierarchy (top to bottom)
+        #   first_inst -> second_inst -> third_inst -> fourth_inst
+        #   (first_user)  (second_user)   (first_user)  (third_user)
+        first_inst.add_child(second_inst.key)
+        second_inst.add_child(third_inst.key)
+        third_inst.add_child(fourth_inst.key)
+
+        second_inst.set_parent(first_inst.key)
+        third_inst.set_parent(second_inst.key)
+        fourth_inst.set_parent(third_inst.key)
+
+        self.assertTrue(is_admin_of_parent_inst(first_user, second_inst.key.urlsafe()))
+        self.assertFalse(is_admin_of_parent_inst(second_user, first_inst.key.urlsafe()))
+        self.assertFalse(is_admin_of_parent_inst(third_user, third_inst.key.urlsafe()))
+
+
+    def test_is_not_admin(self):
+        """Test the method is_not_admin."""
+
+        first_user = mocks.create_user()
+        second_user = mocks.create_user()
+
+        first_inst = mocks.create_institution()
+        second_inst = mocks.create_institution()
+
+        first_user.add_institution(first_inst.key)
+        second_user.add_institution(second_inst.key)
+
+        first_user.add_institution_admin(first_inst.key)
+        second_user.add_institution_admin(second_inst.key)
+
+        first_inst_urlsafe = first_inst.key.urlsafe()
+        second_inst_urlsafe = second_inst.key.urlsafe()
+
+        self.assertFalse(is_not_admin(first_user, first_inst_urlsafe))
+        self.assertTrue(is_not_admin(first_user, second_inst_urlsafe))
+
+        self.assertFalse(is_not_admin(second_user, second_inst_urlsafe))
+        self.assertTrue(is_not_admin(second_user, first_inst_urlsafe))
+
+
     def test_get_all_parent_admins(self):
-        """Test get_all_parent_admins method."""
+        """Test the method get_all_parent_admins."""
+
         first_user = mocks.create_user()
         second_user = mocks.create_user()
         third_user = mocks.create_user()

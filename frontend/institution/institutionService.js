c1db58bafd9ec3db3b422ec92e46b0e852301ba7
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2018-07-05 11:46:05 -0300

    Fix url path of delete institution link frontend

--- frontend/institution/institutionService.js
+++ frontend/institution/institutionService.js
@@ -73,11 +73,8 @@
         };
 
         service.removeLink = function removeLink(institutionKey, institutionLink, isParent) {
-            if (!isParent) {
-                return HttpService.delete(INSTITUTIONS_URI + "/" + institutionKey + "/hierarchy/" + institutionLink + '/institution_parent');
-            } else {
-                return HttpService.delete(INSTITUTIONS_URI + "/" + institutionKey + "/hierarchy/" + institutionLink + "?isParent=" + isParent);
-            }
+            const path = (isParent) ? 'institution_children' : 'institution_parent';
+            return HttpService.delete(INSTITUTIONS_URI + "/" + institutionKey + "/hierarchy/" + institutionLink + '/' + path);
         };
     });
 })();

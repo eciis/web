34586a41575932b108ad5f1c654c8e6cdc4f2326
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2018-09-17 17:15:26 -0300

    Add frontend and support url in config.js and refact ecis script

--- frontend/main/mainController.js
+++ frontend/main/mainController.js
@@ -5,7 +5,8 @@
     app.controller("MainController", function MainController($mdSidenav, $state, AuthService,
             RequestInvitationService, $mdMenu, $window) {
         var mainCtrl = this;
-        var url_report = "http://support.plataformacis.org/report";
+        var url_report = Config.SUPPORT_URL + "/report";
+        //var url_report = "http://support.plataformacis.org/report";
         
         mainCtrl.showSearch = false;
         mainCtrl.search_keyword = "";

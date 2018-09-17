c82fd3641ff49098aad9d5ab745e6fe8af7ea5c4
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2018-09-17 17:31:29 -0300

    Change app version

--- landing/config.js
+++ landing/config.js
@@ -5,5 +5,5 @@ var Config = {
     LANDINGPAGE_URL: 'http://localhost:8080',
     SUPPORT_URL: 'http://localhost:8083',
     FRONTEND_URL: 'http://localhost:8081',
-    APP_VERSION: 'fix-script-to-run-application'
+    APP_VERSION: 'master'
 };

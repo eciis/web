b10a58946b4a7366ef94785147abb0194f070f3b
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2018-09-18 08:44:13 -0300

    Change urls in config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,9 +1,9 @@
 "use strict";
 
 var Config = {
-    BACKEND_URL: 'http://localhost:8081',
-    LANDINGPAGE_URL: 'http://localhost:8082',
-    SUPPORT_URL: 'http://localhost:8080',
-    FRONTEND_URL: 'http://localhost:8083',
+    BACKEND_URL: 'http://localhost:8082',
+    LANDINGPAGE_URL: 'http://localhost:8080',
+    SUPPORT_URL: 'http://localhost:8083',
+    FRONTEND_URL: 'http://localhost:8081',
     APP_VERSION: 'master'
 };

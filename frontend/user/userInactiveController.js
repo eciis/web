c7b531c62c77de6e3d40064e11a194220840dc70
MaianaB <maiana.brito@ccc.ufcg.edu.br>
2018-10-02 13:21:37 -0300

    remove comments

--- frontend/user/userInactiveController.js
+++ frontend/user/userInactiveController.js
@@ -20,13 +20,6 @@
         userInactiveCtrl.institutionSelect = {};
         var ACTIVE = 'active';
 
-        window.addEventListener('popstate', function(event) {
-            // The popstate event is fired each time when the current history entry changes.
-            userInactiveCtrl.logout();
-            window.removeEventListener('popstate', ()=>{})
-        
-        }, false);
-
         userInactiveCtrl.logout = function logout() {
             AuthService.logout();
         };

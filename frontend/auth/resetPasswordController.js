aec5f875494a15c0517b8c5334c7304d9d999c30
Ruan Eloy <ruan.silveira@ccc.ufcg.edu.br>
2018-11-07 09:20:56 -0300

    add jsDoc to functions and an error feedback to the user when the reset request fails

--- frontend/auth/resetPasswordController.js
+++ frontend/auth/resetPasswordController.js
@@ -3,24 +3,38 @@
 (function () {
     const app = angular.module("app");
 
-    app.controller('ResetPasswordController', function (AuthService, $state, $scope) {
+    app.controller('ResetPasswordController', function (AuthService, $state, $scope, MessageService) {
         var resetCtrl = this;
 
         resetCtrl.email = '';
         resetCtrl.emailSent = false;
 
+        /**
+         * It sends a reset password request to the server,
+         * if it is successful the flag emailSent is set to true,
+         * otherwise, an error message is showed 
+         */
         resetCtrl.resetPassword = function () {
             AuthService.resetPassword(resetCtrl.email)
             .then(_ => {
                 resetCtrl.emailSent = true;
                 $scope.$apply();
-            }).catch(err => console.error(err));
+            }).catch(_ => {
+                MessageService.showToast("Ocorreu um erro, verifique seu e-mail e tente novamente");
+            });
         };
         
+        /**
+         * It returns the user to the singin page
+         */
         resetCtrl.return = function () {
             $state.go("signin");
         };
 
+        /**
+         * It returns the appropriate message depending on whether
+         * the user has already sent the e-mail reset request or not
+         */
         resetCtrl.getMessage = function () {
             const firstMsg = "Redefinir sua senha";
             const secondMsg = `Você receberá dentro de instantes no e-mail ${resetCtrl.email} um link para redefinição da senha.`;

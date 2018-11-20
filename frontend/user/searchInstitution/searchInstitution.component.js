797d1d1bbbea504f32213b1afe7662051119c05d
Ruan Eloy <ruaneloy@pop-os.localdomain>
2018-11-20 10:47:59 -0300

    added jsdoc

--- frontend/user/searchInstitution/searchInstitution.component.js
+++ frontend/user/searchInstitution/searchInstitution.component.js
@@ -19,16 +19,24 @@
         searchInstCtrl.institutions = [];
         searchInstCtrl.selectedInst = {};
 
-
+				/**
+				 * Get the institution icon depending on whether the institution is selected
+				 */
         searchInstCtrl.getInstIcon = function (inst) {
             return searchInstCtrl.isInstSelected(inst) ? 'done' : 'account_balance';
         }
 
+				/**
+				 * Check if the institution is selected and loaded
+				 */
         searchInstCtrl.isInstSelected = function (institution){
 						return searchInstCtrl.selectedInst.key === institution.id && 
 							searchInstCtrl.isInstLoaded;
         };
 
+				/**
+				 * Search for an institution based on the user input
+				 */
         searchInstCtrl.search = function () {
             const INST_STATE = 'active';
             InstitutionService.searchInstitutions(searchInstCtrl.keyword, INST_STATE, 'institution')
@@ -39,6 +47,9 @@
                 });
         };
 
+				/**
+				 * Select an institution and load its data
+				 */
         searchInstCtrl.select = function (institution){
             InstitutionService.getInstitution(institution.id)
                 .then(institution => {
@@ -48,6 +59,9 @@
             });
         };
 
+				/**
+				 * Go to the create institution form state
+				 */
         searchInstCtrl.createInst = function createInst() {
             $state.go("create_institution_form");
         };

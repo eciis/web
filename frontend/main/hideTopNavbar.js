e5b6b7a63b2e88141d8e2bbb1903a3c5f8ca869b
MaianaB <maiana.brito@ccc.ufcg.edu.br>
2018-11-20 09:05:44 -0300

    change function

--- frontend/main/hideTopNavbar.js
+++ frontend/main/hideTopNavbar.js
@@ -10,17 +10,15 @@
                 const content = element[0];
                 const navbar = document.getElementById('main-toolbar');
 
-                if(Utils.isPhoneScreen()){
+                if(Utils.isMobileScreen(450)){
                     content.addEventListener('scroll', function() {
                         const screenPosition = content.scrollTop;
                         const limitScrol =  30;
                         if (screenPosition <= limitScrol) {
-                            navbar.style.animation='1.0s fade ease';
+                            navbar.style.animation='1.0s fadeNav ease';
                             navbar.style.animationDelay='0s';
                             navbar.style.display = 'block';
                         } else {
-                            navbar.style.animation='1.0s fadeOut ease';
-                            navbar.style.animationDelay='0s';
                             navbar.style.display = 'none';
                         }
                     });

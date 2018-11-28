7b1ef25ed86f39edd4a39c4188c28c06fe41258c
Ruan Eloy <ruan.silveira@ccc.ufcg.edu.br>
2018-11-28 14:29:03 -0300

    added jsdoc

--- frontend/utils/utilsService.js
+++ frontend/utils/utilsService.js
@@ -6,6 +6,10 @@
     .service('UtilsService', function ($state, $mdSidenav) {
         const service = this;
 
+        /**
+         * Select a sideNav option, go to the select state
+         * and call a callback function if it is present 
+         */
         service.selectNavOption = function (state, params, callback) {
             $state.go(state, params || {});
             $mdSidenav('leftNav').toggle();

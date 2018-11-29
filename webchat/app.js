862bb474f929de73a78f5ac595163b08e6fefad0
JuliePessoa <pessoajjulie@gmail.com>
2018-11-29 10:29:02 -0300

    creating webchat service

---
+++ webchat/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+(function() {
+    var app = angular.module('app', [
+    ]);
+
+    (function main() {
+        console.log("app running");
+    })();
+})();
---
+++ webchat/config.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var Config = {
+    BACKEND_URL: 'http://localhost:8082',
+    LANDINGPAGE_URL: 'http://localhost:8080',
+    SUPPORT_URL: 'http://localhost:8083',
+    FRONTEND_URL: 'http://localhost:8081',
+    WEBCHAT_URL: 'http://localhost:8084',
+    APP_VERSION: 'dev'
+}

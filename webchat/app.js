1b35f652eaf773b2f27b0f9ae4fb7e008a076dea
Pedro Espíndula <joaopedro.jpse@gmail.com>
2018-11-30 23:09:03 -0300

    Add ui-router support

--- webchat/app.js
+++ webchat/app.js
@@ -2,7 +2,57 @@
 
 (function() {
     var app = angular.module('app', [
+        'ui.router',
     ]);
+
+    app.config(function($stateProvider) {
+       $stateProvider
+           .state("webchat", {
+               abstract: true,
+               views: {
+                    main: {
+                        templateUrl: "app/webchat/webchat.html",
+                        controller: "WebchatController as controller",
+                    },
+               },
+           })
+           .state("webchat.home", {
+               url: "/",
+               views: {
+                   content: {
+                       templateUrl: "app/home/home.html",
+                       controller: "HomeController as controller",
+                   },
+               },
+           })
+           .state("webchat.chat", {
+               url: "/chat",
+               views: {
+                   content: {
+                       templateUrl: "app/chat/chat.html",
+                       controller: "ChatController as controller",
+                   },
+               },
+           })
+           .state("webchat.video", {
+               url: "/video",
+               views: {
+                   content: {
+                       templateUrl: "app/video/video.html",
+                       controller: "VideoController as controller",
+                   },
+               },
+           })
+           .state("login", {
+               url: "/login",
+               views: {
+                   main: {
+                       templateUrl: "app/auth/login.html",
+                       controller: "LoginController as controller",
+                   },
+               },
+           })
+    });
 
     (function main() {
         console.log("app running");

c4879cf2229292fc0c86abc37fc29a93cc0718c9
Rob <bruno.robsiq@gmail.com>
2019-01-08 17:35:27 -0300

    Remove unused functions on userService

--- webchat/utils/userService.js
+++ webchat/utils/userService.js
@@ -8,15 +8,6 @@
 
         const USER_URI = "/api/user";
 
-        service.deleteAccount = function deleteAccount() {
-            return HttpService.delete(USER_URI);
-        };
-
-        service.save = function save(patch) {
-            patch = JSON.parse(angular.toJson(patch));
-            return HttpService.patch(USER_URI, patch);
-        };
-
         service.getUser = function getUser(userKey) {
             return HttpService.get(USER_URI + "/" + userKey + "/profile");
         };

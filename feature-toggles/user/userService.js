805978e1cf7b0892faeff7ea3ea2b676b1103208
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2019-01-09 10:41:28 -0300

    Remove unnecessary methods

--- feature-toggles/user/userService.js
+++ feature-toggles/user/userService.js
@@ -5,12 +5,7 @@
 
     app.service("UserService", function UserService(HttpService) {
         var service = this;
-
         var USER_URI = "/api/user";
-
-        service.getUser = function getUser(userKey) {
-            return HttpService.get(USER_URI + "/" + userKey + "/profile");
-        };
 
         service.load = function load() {
             return HttpService.get(USER_URI);

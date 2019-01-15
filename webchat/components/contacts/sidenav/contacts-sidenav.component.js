3d93e0928f69c5cb30331cfea7024d7dd04ad243
Pedro Espíndula <joaopedro.jpse@gmail.com>
2019-01-15 11:25:04 -0300

    Add contacts-sidenav component

---
+++ webchat/components/contacts/sidenav/contacts-sidenav.component.js
@@ -0,0 +1,22 @@
+(function () {
+    'use strict';
+
+    angular.module("webchat").component("contactsSidenav", {
+        templateUrl: "app/components/contacts/sidenav/contacts-sidenav.html",
+        controller: contactsSidenavController,
+        controllerAs: "contactsSidenavCtrl",
+        bindings: {
+            className: "@",
+            style: "@",
+            ariaLabel: "@",
+            contacts: "<",
+        },
+    });
+
+    function contactsSidenavController() {
+        const contactsSidenavCtrl = this;
+        contactsSidenavCtrl.searchQuery = "";
+
+    }
+
+})();

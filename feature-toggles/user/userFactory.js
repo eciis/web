7f31013640c24ef0c649e63321f97b9b8482185c
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2019-01-16 10:35:14 -0300

    Remove unnecessary functions

--- feature-toggles/user/userFactory.js
+++ feature-toggles/user/userFactory.js
@@ -22,41 +22,11 @@
             }
         };
 
-        User.prototype.isAdmin = function isAdmin(keyInstitution) {
-            var managed_institution = _.find(this.institutions_admin, function(institution) {
-            return getKey(institution) == keyInstitution; });
-            return managed_institution;
-        };
-
-        User.prototype.isAdminOfCurrentInst = function isAdminOfCurrentInst() {
-            return this.institutions_admin.map(getKey).includes(this.current_institution.key);
-        };
-
-        User.prototype.isMember = function isMember(institutionKey){
-            return _.includes(_.map(this.institutions, getKeyObj), institutionKey);
-        };
-
-        User.prototype.isInactive = function isInactive() {
-            var notActive = this.state != 'active';
-            return notActive;
-        };
 
         User.prototype.hasPermission = function hasPermission(permissionType) {
             return permissionType in this.permissions;
         };
 
-        function getKeyObj(obj) {
-            if(obj.key){
-            return obj.key;
-            }
-        }
-
-        function getKey(obj){
-            var key = obj.split("/");
-            key = key[key.length -1];
-
-            return key;
-        }
 
         return {
             user: User

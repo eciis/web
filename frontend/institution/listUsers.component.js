46b74b20576d3752ee9eae5308cf54da2415f172
tiagolimpe <tiago.pereira@ccc.ufcg.edu.br>
2019-01-16 09:52:31 -0300

    Adding inline dependency annotation to controller

--- frontend/institution/listUsers.component.js
+++ frontend/institution/listUsers.component.js
@@ -1,32 +1,26 @@
 "use strict";
 
 (function() {
-    angular
-    .module("app")
-    .component("listUsers", {
+    const app = angular.module("app");
+
+    app.controller('ListUsersController', ['ProfileService', function ListUsersController(ProfileService) {
+        const listUsersCtrl = this;
+        listUsersCtrl.keyword = "";
+
+        listUsersCtrl.getInitialLetterOfName = (user) => Utils.getInitialLetterOfName(user);
+
+        listUsersCtrl.showUserProfile = (userKey, ev) => ProfileService.showProfile(userKey, ev);
+
+        listUsersCtrl.limitString = (string, limit) => Utils.limitString(string, limit);
+    }]);
+
+    app.component("listUsers", {
         templateUrl: 'app/institution/list_users.html',
-        controller: ListUsersCtrl,
+        controller: 'ListUsersController',
         controllerAs: 'listUsersCtrl',
         bindings: {
             pageLabel: '=',
             users: '='
         }
     });
-
-    function ListUsersCtrl(ProfileService) {
-        const listUsersCtrl = this;
-        listUsersCtrl.keyword = "";
-
-        listUsersCtrl.getInitialLetterOfName = (user) => {
-            return Utils.getInitialLetterOfName(user);
-        };
-
-        listUsersCtrl.showUserProfile = function showUserProfile(userKey, ev) {
-            ProfileService.showProfile(userKey, ev);
-        };
-
-        listUsersCtrl.limitString = (string, limit) => {
-            return Utils.limitString(string, limit);
-        };
-    }
 })();

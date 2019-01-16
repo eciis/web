60409aedee66efbbafd6b4ab5fb4835cef827729
tiagolimpe <tiago.pereira@ccc.ufcg.edu.br>
2019-01-16 10:04:59 -0300

    Updating inline dependency annotation

--- frontend/institution/listUsers.component.js
+++ frontend/institution/listUsers.component.js
@@ -3,8 +3,9 @@
 (function() {
     const app = angular.module("app");
 
-    app.controller('ListUsersController', ['ProfileService', function ListUsersController(ProfileService) {
+    function ListUsersController(ProfileService) {
         const listUsersCtrl = this;
+
         listUsersCtrl.keyword = "";
 
         listUsersCtrl.getInitialLetterOfName = (user) => Utils.getInitialLetterOfName(user);
@@ -12,11 +13,11 @@
         listUsersCtrl.showUserProfile = (userKey, ev) => ProfileService.showProfile(userKey, ev);
 
         listUsersCtrl.limitString = (string, limit) => Utils.limitString(string, limit);
-    }]);
+    };
 
     app.component("listUsers", {
         templateUrl: 'app/institution/list_users.html',
-        controller: 'ListUsersController',
+        controller: ['ProfileService', ListUsersController],
         controllerAs: 'listUsersCtrl',
         bindings: {
             pageLabel: '=',

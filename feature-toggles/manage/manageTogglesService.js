8812476ea3f112dbe4b74a0873fa2b696b62d162
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2019-01-22 16:53:14 -0300

    fix jsdoc

--- feature-toggles/manage/manageTogglesService.js
+++ feature-toggles/manage/manageTogglesService.js
@@ -20,7 +20,7 @@
 
         /**
          * Function to save feature.
-         * @param {Object} features - features to be save.
+         * @param {Object} feature - feature to be save.
          */
         service.saveFeature = function saveFeature(feature) {
             return HttpService.put(URI, feature);

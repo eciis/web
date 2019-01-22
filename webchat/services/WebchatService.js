b4d72b385cf40f3393fbc0f249b2c56fbd82d825
Pedro Espíndula <joaopedro.jpse@gmail.com>
2019-01-22 09:42:34 -0300

    Replace hardcoded mock for loop

--- webchat/services/WebchatService.js
+++ webchat/services/WebchatService.js
@@ -1,39 +1,24 @@
 (function () {
     'use strict';
 
-    angular.module("webchat").service('WebchatService',function WebchatService() {
-         const WebchatService = this;
-
-         WebchatService.getContacts = () => [
-             {name: "Name", description: "Description", avatar: "https://www.w3schools.com/howto/img_avatar.png"},
-             {name: "Name", description: "Description", avatar: "https://www.w3schools.com/howto/img_avatar.png"},
-             {name: "Name", description: "Description", avatar: "https://www.w3schools.com/howto/img_avatar.png"},
-             {name: "Name", description: "Description", avatar: "https://www.w3schools.com/howto/img_avatar.png"},
-             {name: "Name", description: "Description", avatar: "https://www.w3schools.com/howto/img_avatar.png"},
-             {name: "Name", description: "Description", avatar: "https://www.w3schools.com/howto/img_avatar.png"},
-             {name: "Name", description: "Description", avatar: "https://www.w3schools.com/howto/img_avatar.png"},
-             {name: "Name", description: "Description", avatar: "https://www.w3schools.com/howto/img_avatar.png"},
-             {name: "Name", description: "Description", avatar: "https://www.w3schools.com/howto/img_avatar.png"},
-             {name: "Name", description: "Description", avatar: "https://www.w3schools.com/howto/img_avatar.png"},
-             {name: "Name", description: "Description", avatar: "https://www.w3schools.com/howto/img_avatar.png"},
-             {name: "Name", description: "Description", avatar: "https://www.w3schools.com/howto/img_avatar.png"},
-             {name: "Name", description: "Description", avatar: "https://www.w3schools.com/howto/img_avatar.png"},
-             {name: "Name", description: "Description", avatar: "https://www.w3schools.com/howto/img_avatar.png"},
-             {name: "Name", description: "Description", avatar: "https://www.w3schools.com/howto/img_avatar.png"},
-             {name: "Name", description: "Description", avatar: "https://www.w3schools.com/howto/img_avatar.png"},
-             {name: "Name", description: "Description", avatar: "https://www.w3schools.com/howto/img_avatar.png"},
-             {name: "Name", description: "Description", avatar: "https://www.w3schools.com/howto/img_avatar.png"},
-             {name: "Name", description: "Description", avatar: "https://www.w3schools.com/howto/img_avatar.png"},
-             {name: "Name", description: "Description", avatar: "https://www.w3schools.com/howto/img_avatar.png"},
-             {name: "Name", description: "Description", avatar: "https://www.w3schools.com/howto/img_avatar.png"},
-             {name: "Name", description: "Description", avatar: "https://www.w3schools.com/howto/img_avatar.png"},
-             {name: "Name", description: "Description", avatar: "https://www.w3schools.com/howto/img_avatar.png"},
-             {name: "Name", description: "Description", avatar: "https://www.w3schools.com/howto/img_avatar.png"},
-         ];
+    angular.module("webchat").service('WebchatService', function WebchatService() {
+        const WebchatService = this;
 
         WebchatService.getCurrentUser = () => (
             {name: "Name", description: "Description", avatar: "https://www.w3schools.com/howto/img_avatar.png"}
         );
-        
+
+        WebchatService.getContacts = () => {
+            const contactsList = [];
+            const numContacts = 30;
+
+            for (let i = 0; i < numContacts; i++) {
+                contactsList.push(WebchatService.getCurrentUser());
+            }
+
+            return contactsList;
+        };
+
+
     });
 })();

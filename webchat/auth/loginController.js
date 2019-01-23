40eb38549e89bec80191c199c73a568ec838f49d
Rob <bruno.robsiq@gmail.com>
2019-01-23 12:35:40 -0300

    Add backButton behavior and tests

--- webchat/auth/loginController.js
+++ webchat/auth/loginController.js
@@ -11,6 +11,10 @@
           $state.go(STATES.home);
         }
 
+        controller.backButton = () => {
+          window.location.replace(Config.FRONTEND_URL);
+        }
+
         controller.$onInit = () => {
           if (AuthService.isLoggedIn()) {
             $state.go(STATES.home);

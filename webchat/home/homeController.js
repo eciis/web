a9498977758f85d51d581afeec223bcfcccae6ca
Rob <bruno.robsiq@gmail.com>
2019-01-29 14:30:23 -0300

    Remove testing files from PR branch

--- webchat/home/homeController.js
+++ webchat/home/homeController.js
@@ -3,97 +3,15 @@
 
     const webchat = angular.module('webchat');
 
-    webchat.controller('HomeController', function HomeController (AuthService, UserService) {
+    webchat.controller('HomeController', function HomeController ($scope, $state) {
         const controller = this;
 
-        controller.userList = {};
-        controller.client = {};
-        controller.chat = {};
-        controller.dest = '';
-        controller.msg = '';
-        controller.msgList = () => {
-          return controller.chat.currentMessages;
-        };
-
-        controller.sendMessage = () => {
-          controller.chat.sendMessage(controller.msg);
-          controller.msg = '';
-        }
-
-        controller.$onInit = () => {
+        const main = () => {
             console.log('HomeController running');
-            controller.client = AuthService.getChatClient();
-            controller.getUserList(controller.client.users);
-            controller.client.on('offer-received', callPrompt);
-            controller.client.on('user-list-update', controller.getUserList);
-            controller.client.on('chat-created', e => {
-              controller.chat = e.chat;
-
-              controller.chat.on('msg-list-updated', ev => {
-              })
-
-              controller.chat.on('track-received', ev => {
-                console.log('track received', ev)
-                document.getElementById('video-remote').srcObject = ev.streams[0];
-              })
-            })
         };
 
-        controller.updateUsers = () => {
-          controller.client.requestUsers();
-        }
+        main();
 
-        controller.getName = (id) => {
-          return this.userList[id];
-        }
-
-        controller.formatTime = (timestamp) => {
-          const date = new Date(timestamp);
-          return date.toLocaleTimeString();
-        }
-
-        controller.call = (id) => {
-          getMedia().then(stream => {
-            controller.client.call(id, stream);
-            // angular.element not working here
-            document.getElementById('video-selfie').srcObject = stream;
-          });
-        }
-
-      controller.getUserList = (users) => {
-        let parsedList = {};
-        if (users.forEach) {
-          users.forEach(userKey => {
-            UserService.getUser(userKey).then((res) => {
-              parsedList[userKey] = res.name;
-            });
-          });
-
-        }
-
-        controller.userList = parsedList;
-      }
-
-      function callPrompt(id) {
-        const answer = confirm(`${id} has called. accept?`);
-        if (answer) {
-          getMedia().then(stream => {
-            controller.client.acceptCall(id, stream);
-            document.getElementById('video-selfie').srcObject = stream;
-          });
-        }
-      }
-
-      function getMedia() {
-        return new Promise((resolve, reject) => {
-          let stream = new MediaStream();
-            navigator.mediaDevices.getUserMedia({video:true}).then(s => {
-              stream = s;
-              resolve(stream)
-            }).catch(_ => {
-              resolve(stream);
-            })
-        })
-      }
     });
+
 })();

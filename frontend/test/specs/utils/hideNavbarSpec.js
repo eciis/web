5f5456670a87f6cbefe78009c76afc926493f590
MaianaB <maiana.brito@ccc.ufcg.edu.br>
2019-01-30 17:01:03 -0300

    change tests

--- frontend/test/specs/utils/hideNavbarSpec.js
+++ frontend/test/specs/utils/hideNavbarSpec.js
@@ -47,13 +47,15 @@
             }};
             spyOn(hideDirective, 'hideElement').and.callThrough();
         })
-        it("should hide the bottom toolbar", function() {
+        it("When the current state is any of the states of the institution," +
+        "you should set the lower toolbar element of the property to None.", function() {
             state.current.name = STATES_CONST.INST_TIMELINE;
             scope.isStateAllowedTopMobile = scope.isTopToolbarAllowed();
             scope.isStateAllowedBottom  = scope.isBottomToolbarAllowed();
 
             scope.initialToolbarDisplayState();
             expect(hideDirective.hideElement).toHaveBeenCalledWith(hideDirective.bottomToolbar);
+            expect(hideDirective.bottomToolbar.style).toEqual({display: 'none'});
         })
         it("should not hide the bottom toolbar", function() {
             state.current.name = STATES_CONST.CREATE_EVENT;
@@ -62,6 +64,29 @@
             
             scope.initialToolbarDisplayState();
             expect(hideDirective.hideElement).toHaveBeenCalledWith(hideDirective.topTollbar);
+            expect(hideDirective.topTollbar.style).toEqual({display: 'none'});
+        })
+    })
+
+    describe('hideElement', function(){
+        beforeEach(function(){
+            hideDirective.topTollbar = {style:{
+                    display: 'grid'
+                }};
+            hideDirective.bottomToolbar = {style:{
+                display: 'flex'
+            }};
+            spyOn(hideDirective, 'hideElement').and.callThrough();
+        })
+        it("should change property of element to none", function() {
+            expect(hideDirective.topTollbar.style).toEqual({display: 'grid'});
+            scope.hideElement(hideDirective.topTollbar);
+            expect(hideDirective.topTollbar.style).toEqual({display: 'none'});
+        })
+        it("should change property of element to none", function() {
+            expect(hideDirective.bottomToolbar.style).toEqual({display: 'flex'});
+            scope.hideElement(hideDirective.bottomToolbar);
+            expect(hideDirective.bottomToolbar.style).toEqual({display: 'none'});
         })
     })
 }));

79fc36035908e1b78fc826e921c84a2b2a09cb7a
Pedro Espíndula <joaopedro.jpse@gmail.com>
2019-02-12 14:17:12 -0300

    Add jsdoc to is-empty-card component

--- frontend/utils/is-empty-card/is-empty-card.component.js
+++ frontend/utils/is-empty-card/is-empty-card.component.js
@@ -1,6 +1,13 @@
 (function () {
     'use strict';
 
+    /**
+     * Component that is shown when something is empty and in the error page.
+     * It's shows a default image and a text that it receives as a binding.
+     * @class isEmptyCard
+     * @example
+     * <is-empty-card text="Card is empty" ng-if="conditionToBeEmpty"></is-empty-card>
+     */
     angular.module("app").component("isEmptyCard", {
         templateUrl: "app/utils/is-empty-card/is-empty-card.html",
         controller: isEmptyCardController,

5714696748372caab5954ed8468188a69fd14659
ruanres <ruan.silveira@ccc.ufcg.edu.br>
2019-02-12 15:10:52 -0300

    fixed tests for editProfileController

--- frontend/test/specs/user/editProfileControllerSpec.js
+++ frontend/test/specs/user/editProfileControllerSpec.js
@@ -2,69 +2,141 @@
 
 (describe('Test EditProfileController', function() {
 
-    var httpBackend, scope, mdDialog, authService, profileService, editProfileCtrl, createCrtl, userToEdit;
+    let scope, mdDialog, authService, profileService, messageService,
+        editProfileCtrl, observerRecorderService, userToEdit, deferred;
 
-    var institution = {
-        name: 'INSTITUTION',
-        key: '987654321'
-    };
+    let institution, user, profile;
 
-    var user = {
-        name: 'User',
-        email: 'teste@gmail',
-        institutions: [institution],
-        institution_profiles: [{
-                office: 'developer',
-                phone: '(99) 99999-9999',
-                email: 'teste@gmail.com',
-                institution_key: institution.key
-            }],
-        state: 'active'
-    };
+    const setupModels = () => {
+        institution = {
+            name: 'INSTITUTION',
+            key: '987654321'
+        };
+    
+        user = {
+            name: 'User',
+            email: 'teste@gmail',
+            institutions: [institution],
+            institution_profiles: [{
+                    office: 'developer',
+                    phone: '(99) 99999-9999',
+                    email: 'teste@gmail.com',
+                    institution_key: institution.key
+                }],
+            state: 'active'
+        };
+
+        profile = {
+            office: 'member',
+            email: user.email,
+            institution: institution
+        };
+    }
 
     beforeEach(module('app'));
 
-    beforeEach(inject(function($controller, $httpBackend, $rootScope, ProfileService,
-        $mdDialog, AuthService, MessageService) {
+    beforeEach(inject(function($controller, $rootScope, ProfileService,
+        $mdDialog, AuthService, MessageService, ObserverRecorderService, $q) {
 
-        httpBackend = $httpBackend;
-        httpBackend.when('GET', 'main/main.html').respond(200);
-        httpBackend.when('GET', 'home/home.html').respond(200);
-        httpBackend.when('GET', 'user/edit_profile.html').respond(200);
-        httpBackend.when('GET', 'auth/login.html').respond(200);
         profileService = ProfileService;
         scope = $rootScope.$new();
         mdDialog = $mdDialog;
+        deferred = $q.defer();
         authService = AuthService;
+        observerRecorderService = ObserverRecorderService;
+        messageService = MessageService;
 
+        setupModels();
         authService.login(user);
-
         userToEdit = authService.getCurrentUser();
 
-        createCrtl = function() {
-            return $controller('EditProfileController', {
-                    scope: scope,
-                    institution: institution,
-                    user: userToEdit,
-                    profileService: profileService,
-                    authService: authService,
-                    mdDialog: mdDialog,
-                    MessageService: MessageService
-                });
-        };
-        editProfileCtrl = createCrtl();
+        editProfileCtrl = $controller('EditProfileController', {
+            scope: scope,
+            profile: profile,
+            institution: institution,
+            user: userToEdit,
+            profileService: profileService,
+            authService: authService,
+            mdDialog: mdDialog,
+            MessageService: MessageService
+        });
+        editProfileCtrl.$onInit();
     }));
+
+    describe('onInit()', () => {
+        it('should set user, profile and observer', () => {
+            spyOn(authService, 'getCurrentUser').and.returnValue(user);
+            spyOn(observerRecorderService, 'register');
+            
+            editProfileCtrl.$onInit();
+
+            expect(editProfileCtrl.user).toBe(user);
+            expect(editProfileCtrl.profile).toBe(profile);
+            expect(observerRecorderService.register).toHaveBeenCalledWith(editProfileCtrl.user);
+        });
+    });
+
+    describe('getIntName()', () => {
+        it('should call limitString', () => {
+            spyOn(Utils, 'limitString');
+            editProfileCtrl.getInstName();
+            expect(Utils.limitString).toHaveBeenCalledWith(profile.institution.name, 67);
+        });
+    });
 
     describe('edit', function() {
         it('should call editProfile() and save()', function() {
-            spyOn(profileService, 'editProfile').and.callThrough();
-            spyOn(authService, 'save').and.callThrough();
-            httpBackend.expect('PATCH', '/api/user').respond(userToEdit);
-            userToEdit.institution_profiles[0].office = 'Developer';
+            const patch = {...profile, office: 'developer'};
+            spyOn(observerRecorderService, 'generate').and.returnValue(patch);
+            spyOn(profileService, 'editProfile').and.returnValue(deferred.promise);
+            spyOn(authService, 'save');
+            spyOn(messageService, 'showToast');
+            spyOn(mdDialog, 'hide');
+            deferred.resolve();
+
             editProfileCtrl.edit();
-            httpBackend.flush();
-            expect(profileService.editProfile).toHaveBeenCalled();
+            scope.$apply();
+
+            expect(observerRecorderService.generate).toHaveBeenCalled();
+            expect(profileService.editProfile).toHaveBeenCalledWith(patch);
+            expect(messageService.showToast).toHaveBeenCalledWith('Perfil editado com sucesso');
             expect(authService.save).toHaveBeenCalled();
+            expect(mdDialog.hide).toHaveBeenCalled();
+        });
+
+        it('should not call editProfile() if there is no alterations', function() {
+            const patch = {};
+            spyOn(observerRecorderService, 'generate').and.returnValue(patch);
+            spyOn(profileService, 'editProfile');
+            spyOn(mdDialog, 'hide');
+
+            editProfileCtrl.edit();
+
+            expect(observerRecorderService.generate).toHaveBeenCalled();
+            expect(profileService.editProfile).not.toHaveBeenCalled();
+            expect(mdDialog.hide).toHaveBeenCalled();
+        });
+
+        it('should show a message when the edited profile is invalid', () => {
+            editProfileCtrl.profile.office = undefined;
+            spyOn(observerRecorderService, 'generate');
+            spyOn(profileService, 'editProfile');
+            spyOn(messageService, 'showToast');
+
+            editProfileCtrl.edit();
+
+            expect(observerRecorderService.generate).not.toHaveBeenCalled();
+            expect(profileService.editProfile).not.toHaveBeenCalled();
+            expect(messageService.showToast).toHaveBeenCalledWith('O cargo é obrigatório.');
+        });
+    });
+
+    describe('removeProfile()', () => {
+        it('should call the removeProfile function from ProfileService', () => {
+            spyOn(profileService, 'removeProfile');
+            const event = {};
+            editProfileCtrl.removeProfile(event);
+            expect(profileService.removeProfile).toHaveBeenCalledWith(event, editProfileCtrl.profile.institution);
         });
     });
 

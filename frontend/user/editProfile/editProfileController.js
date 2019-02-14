5e6f9a5731cc62d0dfb1e82177807e0748020b4b
ruanres <ruan.silveira@ccc.ufcg.edu.br>
2019-02-14 15:30:41 -0300

    editProfile jsDoc

--- frontend/user/editProfile/editProfileController.js
+++ frontend/user/editProfile/editProfileController.js
@@ -15,6 +15,10 @@
         let profileObserver = {};
         let oldProfile;
         
+        /**
+         * Sets up the current user, the profile that is going to be edited
+         * and the abserver that will capture the changes to create a patch object 
+         */
         editProfileCtrl.$onInit = () => {
             editProfileCtrl.user = AuthService.getCurrentUser();
             editProfileCtrl.profile = profile;
@@ -22,10 +26,18 @@
             profileObserver = ObserverRecorderService.register(editProfileCtrl.user);
         };
 
+        /**
+         * Gets the institution name and abreviates it 
+         * if is longer than 67 characters
+         */
         editProfileCtrl.getInstName = () => {
             return Utils.limitString(editProfileCtrl.profile.institution.name, 67);
         };
 
+        /**
+         * Save the changes done in the current profile
+         * if all of its fields are corrected filled
+         */
         editProfileCtrl.edit = function edit() {
             if (isValidProfile()) {
                 var patch = ObserverRecorderService.generate(profileObserver);
@@ -42,16 +54,27 @@
             }
         };
 
+        /**
+         * Calls the removeProfile function to remove the current profile
+         * @param {object} event - javascript event 
+         */
         editProfileCtrl.removeProfile = (event) => {
             ProfileService.removeProfile(event, profile.institution);
         };
 
+        /**
+         * Closes the profile and restores the profile to its orinal state 
+         * if some changes were done, but not saved
+         */
         editProfileCtrl.closeDialog = function closeDialog() {
             const indexOfProfile = editProfileCtrl.user.institution_profiles.indexOf(editProfileCtrl.profile);
             editProfileCtrl.user.institution_profiles[indexOfProfile] = oldProfile;
             $mdDialog.hide();
         };
 
+        /**
+         * Check if the profile has the field office filled
+         */
         function isValidProfile() {
             return !_.isEmpty(editProfileCtrl.profile.office);
         }

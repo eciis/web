900a4230586b46c9231ed2dba96590c1275474de
Pedro Espíndula <joaopedro.jpse@gmail.com>
2019-02-18 13:24:34 -0300

    Add generical dialog jsdoc

--- frontend/utils/generic-dialog/gen-dialog.component.js
+++ frontend/utils/generic-dialog/gen-dialog.component.js
@@ -1,6 +1,20 @@
 (function () {
     'use strict';
 
+    /**
+     * An generical dialog component that has a title, a subtitle, a cancel button
+     * and a confirm button. It receives as a binding the title, the subtitle and
+     * the confirm action.
+     * @class genDialog
+     * @example
+     * <md-dialog>
+     *     <gen-dialog title="anTitle" subtitle="anSubtitle" confirm-action="anAction">
+     *         <h1>The dialog content</h1>
+     *         <p>It could be any HTML</p>
+     *         <gen-component>Including a component</gen-component>
+     *     </gen-dialog>
+     * </md-dialog>
+     */
     angular.module("app").component("genDialog", {
         templateUrl: "app/utils/generic-dialog/gen-dialog.html",
         controller: [

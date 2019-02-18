1bc01bcd179e346931d7c616662e450fe6985ac9
Raoni Smaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2019-02-18 14:38:38 -0300

    Extracting style to css' file

--- frontend/test/specs/event/eventDetailsControllerSpec.js
+++ frontend/test/specs/event/eventDetailsControllerSpec.js
@@ -169,7 +169,7 @@
 
             var startTime = new Date('2018-01-13');
             var endTime = new Date('2018-02-13');
-            event.sta9rt_time = startTime;
+            event.start_time = startTime;
             event.end_time = endTime;
             eventCtrl.event = event;
             var result = eventCtrl.endInTheSameDay();

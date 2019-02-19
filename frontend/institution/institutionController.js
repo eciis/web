ce114e979501401f54a96954c74a47e9e3461beb
Pedro Espíndula <joaopedro.jpse@gmail.com>
2019-02-19 17:02:27 -0300

    Add event institution page redirection by query params

--- frontend/institution/institutionController.js
+++ frontend/institution/institutionController.js
@@ -209,9 +209,17 @@
         };
 
         institutionCtrl.goToEvents = function goToEvents(institutionKey) {
-            UtilsService.selectNavOption(STATES.INST_EVENTS, 
-                {institutionKey: institutionKey, posts: institutionCtrl.posts});
+            Utils.isMobileScreen() ? goToEventsMobile(institutionKey) : goToEventsDesktop(institutionKey);
         };
+
+        function goToEventsMobile(institutionKey) {
+            UtilsService.selectNavOption(STATES.EVENTS,{institutionKey: institutionKey});
+        }
+
+        function goToEventsDesktop(institutionKey) {
+            UtilsService.selectNavOption(STATES.INST_EVENTS,
+                {institutionKey: institutionKey, posts: institutionCtrl.posts});
+        }
 
         institutionCtrl.goToLinks = function goToLinks(institutionKey) {
             UtilsService.selectNavOption(STATES.INST_LINKS, {institutionKey: institutionKey});

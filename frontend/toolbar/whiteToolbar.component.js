9c4104e8eae8cb040d453be20053f6f7f75e9b7f
Raoni Smaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2019-02-20 14:14:49 -0300

    adding some jsdocs

--- frontend/toolbar/whiteToolbar.component.js
+++ frontend/toolbar/whiteToolbar.component.js
@@ -19,10 +19,16 @@
     function WhiteToolbarController($window, SCREEN_SIZES) {
         const whiteToolbarCtrl = this;
 
+        /**
+         * Redirect the user to the previous page.
+         */
         whiteToolbarCtrl.goBack = () => {
             return $window.history.back();
         };
 
+        /**
+        * Returns true if the application is being used by a mobile
+        */
         whiteToolbarCtrl.isMobileScreen = () => {
             return Utils.isMobileScreen(SCREEN_SIZES.SMARTPHONE);
         };

ec1c6cb3f2eb234387f92ac2c1ed2b3facca5e23
Rob <bruno.robsiq@gmail.com>
2019-02-21 15:25:22 -0300

    Finish institution form

--- frontend/institution/forms/institutionForm.component.js
+++ frontend/institution/forms/institutionForm.component.js
@@ -1,21 +1,50 @@
 'use strict';
 
 (function () {
-  function controller() {
+  function InstitutionFormController(CropImageService, ImageService, MessageService, $scope, InstitutionService) {
     const ctrl = this;
+    ctrl.institution = {};
+    ctrl.pictureFile = {};
+    ctrl.actuationAreas = {};
+    ctrl.legalNatures = {};
+    ctrl.numberRegex = /\d+$/
+    ctrl.phoneRegex = /\d{2}\s\d{4,5}\-\d{4,5}/
+
+    ctrl.cropThenSetImage = (event) => {
+      CropImageService.crop(ctrl.pictureFile, event).then((cropped) => {
+        const size = 800;
+        ImageService.compress(cropped, size).then((resized) => {
+          ctrl.onNewPhoto({photoSrc: resized});
+          ImageService.readFile(resized, (img) => {
+            ctrl.institution.photo_url = img.src;
+            ctrl.pictureFile = null;
+            $scope.$apply();
+          });
+        });
+      }).catch(e => {
+        MessageService.showToast(e);
+        ctrl.pictureFile = null;
+      });
+    }
 
     ctrl.$onInit = () => {
-      ctrl.configInstCtrl = ctrl.parentCtrl;
+      InstitutionService.getLegalNatures().then(res => {
+        ctrl.legalNatures = res;
+      });
+      InstitutionService.getActuationAreas().then(res => {
+        ctrl.actuationAreas = res;
+      });
     }
   }
 
   const app = angular.module('app');
   app.component('institutionForm', {
-    controller: controller,
+    controller: InstitutionFormController,
     controllerAs: 'ctrl',
     templateUrl: 'app/institution/forms/institution_form.html',
     bindings: {
-      parentCtrl: '=',
+      institution: '=',
+      onNewPhoto: '&',
     }
   });
 })();

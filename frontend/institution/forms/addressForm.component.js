b79f787e6d3adf140abd2e4a1f3ffbc0a4952cf7
Rob <bruno.robsiq@gmail.com>
2019-02-21 10:54:52 -0300

    Add address form for new invite inst screen

---
+++ frontend/institution/forms/addressForm.component.js
@@ -0,0 +1,48 @@
+'use strict';
+
+(function () {
+  function controller(brCidadesEstados, $http) {
+    const ctrl = this;
+    ctrl.states = {};
+    ctrl.countries = {};
+    ctrl.cities = {};
+
+    ctrl.numberRegex = /\d+$/;
+    ctrl.cepRegex = /\d{5}\-\d{3}/;
+
+    Object.defineProperty(ctrl, 'isAnotherCountry', {
+      get: function() {
+        return ctrl.address && ctrl.address.country !== 'Brasil';
+      }
+    });
+
+    Object.defineProperty(ctrl, 'cities', {
+      get: function() {
+        if (ctrl.address && !ctrl.isAnotherCountry) {
+          const currentState = _.find(brCidadesEstados.estados,
+            e => _.isEqual(e.nome, ctrl.address.federal_state))
+          if (currentState) {
+            return currentState.cidades;
+          }
+        }
+      }
+    });
+
+    ctrl.$onInit = () => {
+      ctrl.states = brCidadesEstados.estados;
+      $http.get('app/institution/countries.json').then(res => {
+        ctrl.countries = res.data;
+      });
+    }
+  }
+
+  const app = angular.module('app');
+  app.component('addressForm', {
+    controller: controller,
+    controllerAs: 'ctrl',
+    templateUrl: 'app/institution/forms/address_form.html',
+    bindings: {
+      address: '=',
+    }
+  });
+})();

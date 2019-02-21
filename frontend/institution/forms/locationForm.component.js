da119eff13c119608073b48a15dbb1d90314b736
Rob <bruno.robsiq@gmail.com>
2019-02-21 09:53:34 -0300

    Update location form

--- frontend/institution/forms/locationForm.component.js
+++ frontend/institution/forms/locationForm.component.js
@@ -1,11 +1,38 @@
 'use strict';
 
 (function () {
-  function controller() {
+  function controller(brCidadesEstados, $http) {
     const ctrl = this;
+    ctrl.states = {};
+    ctrl.countries = {};
+    ctrl.cities = {};
+
+    ctrl.numberRegex = /\d+$/;
+    ctrl.cepRegex = /\d{5}\-\d{3}/;
+
+    Object.defineProperty(ctrl, 'isAnotherCountry', {
+      get: function() {
+        return ctrl.address && ctrl.address.country !== 'Brasil';
+      }
+    });
+
+    Object.defineProperty(ctrl, 'cities', {
+      get: function() {
+        if (ctrl.address && !ctrl.isAnotherCountry) {
+          const currentState = _.find(brCidadesEstados.estados,
+            e => _.isEqual(e.nome, ctrl.address.federal_state))
+          if (currentState) {
+            return currentState.cidades;
+          }
+        }
+      }
+    });
 
     ctrl.$onInit = () => {
-      ctrl.configInstCtrl = ctrl.parentCtrl;
+      ctrl.states = brCidadesEstados.estados;
+      $http.get('app/institution/countries.json').then(res => {
+        ctrl.countries = res.data;
+      });
     }
   }
 
@@ -15,7 +42,7 @@
     controllerAs: 'ctrl',
     templateUrl: 'app/institution/forms/location_form.html',
     bindings: {
-      parentCtrl: '=',
+      address: '=',
     }
   });
 })();

1361e3ba6ead12e3ee101575ba0851cdeab8c211
Pedro Espíndula <joaopedro.jpse@gmail.com>
2019-02-25 09:52:53 -0300

    Add entityShowcase jsdoc

--- frontend/utils/entityShowcase/entityShowcase.component.js
+++ frontend/utils/entityShowcase/entityShowcase.component.js
@@ -1,6 +1,32 @@
 (function () {
     'use strict';
 
+    /**
+     * Component that shows an entity (user or institution). It receives a title (entity name),
+     * an subtitle (an entity information) and a avatar (the image to be shown). Also it could
+     * receive an icon in case the entity doesn't have an avatar url. It could receive an left
+     * action that will be triggered when the user clicks on the avatar or the icon and a array
+     * of rightIconBtns objects that has a icon, a icon-color and a action. You should use one
+     * icon or one avatar, and not both of them.
+     * rightIconBtns example:
+     * [
+     *      {
+     *          icon: aIcon,
+     *          iconColor: aIconColor,
+     *          action: aAction,
+     *      },
+     * ]
+     * @class entityShowcase
+     * @example
+     * <entityShowcase
+     *      avatar="aPhoto"
+     *      icon="aIcon"
+     *      title="aTitle"
+     *      subtitle="aSubtitle"
+     *      leftAction="aAction"
+     *      rightIconBtns="rightIconBtnsArray">
+     * </entityShowcase>
+     */
     angular.module("app").component("entityShowcase", {
         templateUrl: "app/utils/entityShowcase/entityShowcase.html",
         controller: entityShowcaseController,

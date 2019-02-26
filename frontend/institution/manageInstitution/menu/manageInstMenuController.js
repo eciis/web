ee836185beec893d04c8c16664897fbf00be840b
ruanres <ruan.silveira@ccc.ufcg.edu.br>
2019-02-26 14:11:14 -0300

    refact manageInstMenu controller

--- frontend/institution/manageInstitution/menu/manageInstMenuController.js
+++ frontend/institution/manageInstitution/menu/manageInstMenuController.js
@@ -1,11 +1,13 @@
 "use strict";
 
-(() => {
+(function() {
     angular
     .module("app")
-    .controller("ManageInstMenuController", ManageInstMenuController);
+    .controller("ManageInstMenuController", [
+        'AuthService', 'ManageInstItemsFactory',
+        ManageInstMenuController]);
 
-    function ManageInstMenuController(AuthService, InstitutionService, ManageInstItemsFactory) {
+    function ManageInstMenuController(AuthService, ManageInstItemsFactory) {
         const manageInstMenuCtrl = this;
 
         manageInstMenuCtrl.$onInit = () => {
@@ -18,16 +20,16 @@
         };
 
         manageInstMenuCtrl._loadInstitution = () => {
-            InstitutionService.getInstitution(manageInstMenuCtrl.user.current_institution.key)
-                .then(institution => {
-                    manageInstMenuCtrl.institution = institution;
-                    manageInstMenuCtrl._loadMenuOptions(institution);
-                });
+            const currentProfile = getProfilesAdmin()
+                .find(prof => prof.institution_key === manageInstMenuCtrl.user.current_institution.key);
+                
+            manageInstMenuCtrl.institution = currentProfile.institution;
+            manageInstMenuCtrl._loadMenuOptions();
         };
 
         manageInstMenuCtrl._loadMenuOptions = (institution) => {
             // the slice was used to get just the first four items
-            manageInstMenuCtrl.options = ManageInstItemsFactory.getItems(institution).slice(0,4);
+            manageInstMenuCtrl.options = ManageInstItemsFactory.getItems(manageInstMenuCtrl.institution).slice(0,4);
         }
 
         manageInstMenuCtrl.getActionButtons = {
@@ -41,20 +43,23 @@
                 return isInstSelected ? "radio_button_checked" : "radio_button_unchecked";
             };
             
-            manageInstMenuCtrl.switchInstOptions = manageInstMenuCtrl.user.institution_profiles
-                .filter(prof => manageInstMenuCtrl.user.isAdmin(prof.institution_key))
-                .map(prof => {
-                    return {
-                        getIcon: () => getIcon(prof.institution_key),
-                        title: prof.institution.name,
-                        action: () => switchInstitution(prof.institution)
-                    };
-                });
+            manageInstMenuCtrl.switchInstOptions = getProfilesAdmin().map(prof => {
+                return {
+                    getIcon: () => getIcon(prof.institution_key),
+                    title: prof.institution.name,
+                    action: () => switchInstitution(prof.institution)
+                };
+            });
         };
 
         const switchInstitution = institution => {
             manageInstMenuCtrl.user.changeInstitution(institution);
             manageInstMenuCtrl._loadInstitution();
+        };
+
+        const getProfilesAdmin = () => {
+            return manageInstMenuCtrl.user.institution_profiles
+                .filter(prof => manageInstMenuCtrl.user.isAdmin(prof.institution_key));
         };
     }
 })();

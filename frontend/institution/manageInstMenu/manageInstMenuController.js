8c979498003484636abb8b1d111809adf0328f93
ruanres <ruan.silveira@ccc.ufcg.edu.br>
2019-02-27 11:35:31 -0300

    created ManageInstMenuController tests

--- frontend/institution/manageInstMenu/manageInstMenuController.js
+++ frontend/institution/manageInstMenu/manageInstMenuController.js
@@ -20,14 +20,14 @@
         };
 
         manageInstMenuCtrl._loadInstitution = () => {
-            const currentProfile = getProfilesAdmin()
+            const currentProfile = manageInstMenuCtrl._getProfilesAdmin()
                 .find(prof => prof.institution_key === manageInstMenuCtrl.user.current_institution.key);
                 
             manageInstMenuCtrl.institution = currentProfile.institution;
             manageInstMenuCtrl._loadMenuOptions();
         };
 
-        manageInstMenuCtrl._loadMenuOptions = (institution) => {
+        manageInstMenuCtrl._loadMenuOptions = () => {
             // the slice was used to get just the first four items
             manageInstMenuCtrl.options = ManageInstItemsFactory.getItems(manageInstMenuCtrl.institution).slice(0,4);
         }
@@ -38,26 +38,27 @@
         };
 
         manageInstMenuCtrl._loadSwitchInstOptions = () => {
-            const getIcon = (instKey) => {
-                const isInstSelected = manageInstMenuCtrl.user.current_institution.key === instKey;
-                return isInstSelected ? "radio_button_checked" : "radio_button_unchecked";
-            };
-            
-            manageInstMenuCtrl.switchInstOptions = getProfilesAdmin().map(prof => {
+            manageInstMenuCtrl.switchInstOptions = manageInstMenuCtrl._getProfilesAdmin()
+            .map(prof => {
                 return {
-                    getIcon: () => getIcon(prof.institution_key),
+                    getIcon: () => manageInstMenuCtrl._getIcon(prof.institution_key),
                     title: prof.institution.name,
-                    action: () => switchInstitution(prof.institution)
+                    action: () => manageInstMenuCtrl._switchInstitution(prof.institution)
                 };
             });
         };
 
-        const switchInstitution = institution => {
+        manageInstMenuCtrl._getIcon = (instKey) => {
+            const isInstSelected = manageInstMenuCtrl.user.current_institution.key === instKey;
+            return isInstSelected ? "radio_button_checked" : "radio_button_unchecked";
+        };
+
+        manageInstMenuCtrl._switchInstitution = institution => {
             manageInstMenuCtrl.user.changeInstitution(institution);
             manageInstMenuCtrl._loadInstitution();
         };
 
-        const getProfilesAdmin = () => {
+        manageInstMenuCtrl._getProfilesAdmin = () => {
             return manageInstMenuCtrl.user.institution_profiles
                 .filter(prof => manageInstMenuCtrl.user.isAdmin(prof.institution_key));
         };

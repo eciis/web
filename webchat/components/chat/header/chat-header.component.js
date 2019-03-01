33470526108c8f4d4032aafcd98f139bd8c8da77
Pedro Espíndula <joaopedro.jpse@gmail.com>
2019-03-01 13:16:40 -0300

    Add chat-header component

---
+++ webchat/components/chat/header/chat-header.component.js
@@ -0,0 +1,19 @@
+(function () {
+    'use strict';
+
+    angular.module("webchat").component("chatHeader", {
+        templateUrl: "app/components/chat/header/chat-header.html",
+        controller: chatHeaderController,
+        controllerAs: "chatHeaderCtrl",
+        bindings: {
+            user: "<",
+            chat: "<",
+            callFunc: "<",
+        },
+    });
+
+    function chatHeaderController() {
+        const chatHeaderCtrl = this;
+    }
+
+})();

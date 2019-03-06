32dd200c731c5d50ec475c6b35f97f640054b863
ruanres <ruan.silveira@ccc.ufcg.edu.br>
2019-03-06 11:02:21 -0300

    added jsDoc to components

--- frontend/utils/clickableCard/clickableCard.component.js
+++ frontend/utils/clickableCard/clickableCard.component.js
@@ -1,6 +1,19 @@
-"use strict";
-
 (function () {
+    "use strict";
+
+    /**
+     * Component used to render a card that opens or closes when clicked.
+     * It receives an icon and title to be showed at the card header,
+     * and the flag isOpen, that changes the initial state of the card
+     * to open or closed, its default value is 'false'. Implicitly, 
+     * it also receives any template that will be rendered inside the card.
+     * 
+     * @class clickableCard
+     * @example
+     * <clickable-card icon="someIcon" title="someTitle" is-open="true">
+     *  <any-template></any-template>
+     * </clickable-card>
+     */
     angular
     .module('app')
     .component("clickableCard", {

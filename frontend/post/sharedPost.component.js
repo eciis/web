50d6bb058938035a33f6052825a28f605fcf2c7f
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2019-03-12 08:20:45 -0300

    Create shared_post_desktop.html

--- frontend/post/sharedPost.component.js
+++ frontend/post/sharedPost.component.js
@@ -3,7 +3,7 @@
     const app = angular.module('app');
 
     app.component('sharedPost', {
-        templateUrl: 'app/post/shared_post_mobile.html',
+        templateUrl: Utils.selectFieldBasedOnScreenSize('app/post/shared_post_desktop.html', 'app/post/shared_post_mobile.html', 600),
         controller: 'PostDetailsController',
         controllerAs: 'postDetailsCtrl',
         bindings: {

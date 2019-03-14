fb9a9d5ea6fd752807a34e8c4528c45e963a417f
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2019-03-14 17:24:27 -0300

    fix issues

--- frontend/event/sharedEvent.component.js
+++ frontend/event/sharedEvent.component.js
@@ -3,6 +3,12 @@
 
     const app = angular.module('app');
 
+    /**
+     * Shared event mobile component
+     * 
+     * @example
+     * <shared-event event="..."></shared-event>
+     */
     app.component('sharedEvent', {
         templateUrl: 'app/event/shared_event_mobile.html',
         controller: 'EventDetailsController',
